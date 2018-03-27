4694327875be9557fe1a7f805f23112bf223541a
Ryan Julian <rjulian@usc.edu>
2018-03-27 14:52:23 -0700

    [travisci] Correct changed file detection

--- scripts/travisci/check_yapf.sh
+++ scripts/travisci/check_yapf.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-CHANGED=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep "\.py$")
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+  CHANGED=$(git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep "\.py$")
+else
+  git remote set-branches --add origin integration
+  git fetch
+  CHANGED=$(git diff --name-only origin/integration | grep "\.py$")
+fi
+
 if [[ ! -z $CHANGED ]]; then
+  echo "Only checking changes files: ${CHANGED}"
   yapf -dpr --style=pep8 $CHANGED
 fi
