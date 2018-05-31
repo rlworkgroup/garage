edd1cb24faf51b4b8960d57037427f48028008b2
Chang <changsu.cs.work@gmail.com>
2018-05-31 15:47:44 -0700

    Add features and remove variables in RandomizedEnv
    Add error handling in constructor and reset().
    Remove variables that doesn't depend on self._wrapped_env.sim in
    reset().
    Reuse MODEL_DIR in mujoco_env.py
    Alphabetize imports.

--- rllab/dynamics_randomization/randomize_env.py
+++ rllab/dynamics_randomization/randomize_env.py
@@ -1,15 +1,15 @@
-from lxml import etree
-from rllab.envs import Env
 import os.path as osp
-import numpy as np
-from rllab.dynamics_randomization import VariationMethods
-from rllab.dynamics_randomization.variation import VariationDistributions
+
+from lxml import etree
 from mujoco_py import load_model_from_xml
 from mujoco_py import MjSim
-from rllab.core import Serializable
+import numpy as np
 
-MODEL_DIR = osp.abspath(
-    osp.join(osp.dirname(__file__), '../../vendor/mujoco_models'))
+from rllab.envs import Env
+from rllab.envs.mujoco.mujoco_env import MODEL_DIR
+from rllab.core import Serializable
+from rllab.dynamics_randomization import VariationMethods
+from rllab.dynamics_randomization.variation import VariationDistributions
 
 
 class RandomizedEnv(Env, Serializable):
@@ -22,9 +22,12 @@ class RandomizedEnv(Env, Serializable):
 
         for v in variations.get_list():
             e = self._model.find(v.xpath)
+            if not e:
+                raise AttributeError("Can't find node in xml")
             v.elem = e
 
-            # todo: handle AttributeError
+            if v.attrib not in e.attrib:
+                raise KeyError("Attribute doesn't exist")
             val = e.attrib[v.attrib].split(' ')
             if len(val) == 1:
                 v.default = float(e.attrib[v.attrib])
@@ -39,6 +42,8 @@ class RandomizedEnv(Env, Serializable):
                 c = np.random.normal(loc=v.var_range[0], scale=v.var_range[1])
             elif v.distribution == VariationDistributions.UNIFORM:
                 c = np.random.uniform(low=v.var_range[0], high=v.var_range[1])
+            else:
+                raise NotImplementedError("Unkown distribution")
             if v.method == VariationMethods.COEFFICIENT:
                 e.attrib[v.attrib] = str(c * v.default)
             elif v.method == VariationMethods.ABSOLUTE:
@@ -50,17 +55,10 @@ class RandomizedEnv(Env, Serializable):
         self._wrapped_env.model = load_model_from_xml(model_xml)
         self._wrapped_env.sim = MjSim(self._wrapped_env.model)
         self._wrapped_env.data = self._wrapped_env.sim.data
-        self._wrapped_env.viewer = None
         self._wrapped_env.init_qpos = self._wrapped_env.sim.data.qpos
         self._wrapped_env.init_qvel = self._wrapped_env.sim.data.qvel
         self._wrapped_env.init_qacc = self._wrapped_env.sim.data.qacc
         self._wrapped_env.init_ctrl = self._wrapped_env.sim.data.ctrl
-        self._wrapped_env.qpos_dim = self._wrapped_env.init_qpos.size
-        self._wrapped_env.qvel_dim = self._wrapped_env.init_qvel.size
-        self._wrapped_env.ctrl_dim = self._wrapped_env.init_ctrl.size
-        self._wrapped_env.frame_skip = 1
-        self._wrapped_env.dcom = None
-        self._wrapped_env.current_com = None
         return self._wrapped_env.reset()
 
     def step(self, action):
@@ -96,6 +94,3 @@ class RandomizedEnv(Env, Serializable):
     @property
     def horizon(self):
         return self._wrapped_env.horizon
-
-
-randomize = RandomizedEnv
