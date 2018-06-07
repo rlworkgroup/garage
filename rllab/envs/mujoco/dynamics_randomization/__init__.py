761f06587855805b9277af8d8fab4c6607c18a79
Chang <changsu.cs.work@gmail.com>
2018-06-07 10:18:32 -0700

    Rename mujoco_model_gen to mujoco_model_generator
    Package names should follow class names.

--- rllab/envs/mujoco/dynamics_randomization/__init__.py
+++ rllab/envs/mujoco/dynamics_randomization/__init__.py
@@ -1,5 +1,5 @@
 from rllab.envs.mujoco.dynamics_randomization.variation import Variations
 from rllab.envs.mujoco.dynamics_randomization.variation import Method
 from rllab.envs.mujoco.dynamics_randomization.variation import Distribution
-from rllab.envs.mujoco.dynamics_randomization.mujoco_model_gen import MujocoModelGenerator
+from rllab.envs.mujoco.dynamics_randomization.mujoco_model_generator import MujocoModelGenerator
 from rllab.envs.mujoco.dynamics_randomization.randomized_env import randomize
