d01009de08dda45424eae902d4d14aaaf9d40bed
Angel Gonzalez <aigonzal@usc.edu>
2018-06-11 16:07:48 -0700

    Return error status in check_flake8
    Give an error status 1 if either the absolute or incremental
    verification find style errors.

--- scripts/travisci/check_flake8.sh
+++ scripts/travisci/check_flake8.sh
@@ -1,13 +1,19 @@
+exit_status=0
 # Absolute verification
 # W191 - Indentation contains tabs
 # E101 - Indentation contains mixed spaces and tabs
 find -iname "*.py" | flake8 --select=E101,W191
-#flake8 --select=E101,W191 $(git ls-tree --full-tree --name-only -r HEAD | grep \.py)
+if [ $? -ne 0 ]; then
+  exit_status=1
+fi
 
 # Incremental verification
 # E501 - Line too long
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-  echo "Commit range: "
-  echo ${TRAVIS_COMMIT_RANGE}
   git diff ${TRAVIS_COMMIT_RANGE} | flake8 --diff --select=E501
+  if [ $? -ne 0 ]; then
+    exit_status=1
+  fi
 fi
+
+exit $exit_status
