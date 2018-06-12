6399e12ed39fa9ad99978a8205ebb18812b92d56
Angel Gonzalez <aigonzal@usc.edu>
2018-06-12 16:34:03 -0700

    Add errors to the absolute and incremental checks
    Error codes have been added to both the absolute and incremental checks
    in file check_flake8.sh.
    An option was added to the former mentioned script to run the
    incremental verification locally so developers can use it to look for
    errors before pushing the code for verification in Travis.
    The order of select and ignore in flake8 was swapped to allow filtering
    specific error codes, specifically, in for naming conventions (N) and
    docstrings (D).
    The package flake8-quotes was replaced for flake8-docstrings, since both
    can check that docstrings use triple double quotes. Also the package
    pep8-naming was added to check style rules for naming conventions.
    Finally, the test files for PEP8 were removed.

--- scripts/travisci/check_flake8.sh
+++ scripts/travisci/check_flake8.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 # Exit status of this script
 exit_status=0
-# Absolute verification:
+
+# Add an option to run the incremental verification locally to reduce the time
+# spent in travis for error verification. It's required to rebase the feature
+# branch with the master branch to avoid checking on unknown changes.
+while getopts ":l" opt; do
+  case ${opt} in
+    l)
+      TRAVIS_PULL_REQUEST="false"
+      ;;
+    \?)
+      echo "usage: check_flake8 [-l]"
+      echo "    -l    run incremental verification locally"
+      exit 1
+      ;;
+  esac
+done
+
+# ABSOLUTE VERIFICATION:
 # The following errors are analyzed in all python files across the repository.
 #
 # TABS
-# W191 - Indentation contains tabs
-# E101 - Indentation contains mixed spaces and tabs
+# W191 indentation contains tabs
+# E101 indentation contains mixed spaces and tabs
 errors="E101,W191,"
 
 # WHITESPACE IN EXPRESSION AND STATEMENTS
@@ -23,6 +40,11 @@ errors+="E221,"
 errors+="W291"
 # E702 multiple statements on one line (semicolon)
 errors+="E702"
+
+# PROGRAMMING RECOMMENDATIONS
+# E714 use is not operator rather than not ... is.
+errors+="E714"
+
 find -iname "*.py" | flake8 --select=${errors}
 if [[ $? -ne 0 ]]; then
   exit_status=1
@@ -52,6 +74,12 @@ errors+="W503,"
 # E303 too many blank lines (2)
 errors+="E301,E302,E303,"
 
+# IMPORTS
+# E100 import statements are in the wrong order
+# E401 multiple imports on one line
+# I201 missing newline between import groups
+errors+="E100,E401,I201,"
+
 # WHITESPACE IN EXPRESSION AND STATEMENTS
 # E203 whitespace before ':', ';' or ','
 errors+="E203,"
@@ -65,26 +93,51 @@ errors+="E231,"
 # E701 multiple statements on one line (colon)
 errors+="E701,"
 
-# STRING QUOTES
-# Use double quote characters to be consistent with the docstring convention
-# in PEP 257.
-quotes="--docstring-quote "'double'
-# We prefer double-quoted strings (`"foo"`) over single-quoted strings
-# (`'foo'`), unless there is a compelling escape or formatting reason for using
-# single quotes. Therefore, ignore those rules to avoid conflicts.
-# Q000 Remove bad quotes
-# Q001 Remove bad quotes from multiline string
-ignore+="Q000,Q001"
+# COMMENTS
+# E261 at least two spaces before inline comment
+errors+="E261,"
+# E262 inline comment should start with '# '
+errors+="E262,"
+
+# NAMING CONVENTIONS
+# Names to avoid
+# E741 do not use variables named ‘l’, ‘O’, or ‘I’
+# E742 do not define classes named ‘l’, ‘O’, or ‘I’
+# E743 do not define functions named ‘l’, ‘O’, or ‘I’
+errors+="E741,E742,E743,"
+# The following error code enables all the error codes for naming conventions
+# defined here:
+# https://github.com/PyCQA/pep8-naming
+# If one of the errors needs to be ignored, just add it to the ignore variable.
+errors+="N,"
+
+# DOCSTRING
+# The following error code enables all the error codes for docstring defined
+# here:
+# http://pep257.readthedocs.io/en/latest/error_codes.html
+# If one of the errors needs to be ignored, just add it to the ignore variable.
+errors+="D,"
+
+# PROGRAMMING RECOMMENDATIONS
+# E711 comparisons to None should always be done with is or is not, never the
+# equality operators.
+errors+="E711,"
+# E714 use is not operator rather than not ... is.
+errors+="E714,"
+# E731 do not assign a lambda expression, use a def.
+errors+="E731,"
+# E722 do not use bare except, specify exception instead.
+errors+="E722"
 
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-  git diff ${TRAVIS_COMMIT_RANGE} | flake8 --diff ${quotes} --ignore=${ignore} --select=${errors}
+  git diff ${TRAVIS_COMMIT_RANGE} | flake8 --diff ${quotes} --select=${errors} --ignore=${ignore}
   if [[ $? -ne 0 ]]; then
     exit_status=1
   fi
 else
   git remote set-branches --add origin master
   git fetch
-  git diff origin/master | flake8 --diff ${quotes} --ignore=${ignore} --select=${errors}
+  git diff origin/master | flake8 --diff ${quotes} --select=${errors} --ignore=${ignore}
   if [[ $? -ne 0 ]]; then
     exit_status=1
   fi
