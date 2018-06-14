869aba94ff6a948c6a22d342fd4b99ee4e8fd773
Angel Gonzalez <aigonzal@usc.edu>
2018-06-14 12:28:01 -0700

    Add missing inverted slash in check_pylint.sh

--- scripts/travisci/check_pylint.sh
+++ scripts/travisci/check_pylint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" && "${TRAVIS}" == "true" ]]; then
-  files_changed=$(git diff "${TRAVIS_COMMIT_RANGE}" --name-only
+  files_changed=$(git diff "${TRAVIS_COMMIT_RANGE}" --name-only \
     | grep "*.py")
   if [[ ! -z "${files_changed}" ]]; then
     pylint "${files_changed}" --rcfile=setup.cfg
