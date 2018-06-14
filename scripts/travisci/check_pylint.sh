e771db955612f275dc5c082063c8adff8c21b0d2
Angel Gonzalez <aigonzal@usc.edu>
2018-06-14 11:53:45 -0700

    Filter the files passed to pylint
    All files found in differences were passed to pylint in check_pylint.sh.
    Grep now filter the files to only be python files. Also, it's verified
    if any python files are found in the modified files to execute pylint.
    Otherwise, pylint considers zero input files an error.

--- scripts/travisci/check_pylint.sh
+++ scripts/travisci/check_pylint.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" && "${TRAVIS}" == "true" ]]; then
-  git diff "${TRAVIS_COMMIT_RANGE}" --name-only \
-    | xargs pylint --rcfile=setup.cfg
+  files_changed=$(git diff "${TRAVIS_COMMIT_RANGE}" --name-only
+    | grep "*.py")
+  if [[ ! -z "${files_changed}" ]]; then
+    pylint "${files_changed}" --rcfile=setup.cfg
+  fi
 else
   git remote set-branches --add origin master
   git fetch
-  git diff origin/master --name-only | xargs pylint --rcfile=setup.cfg
+  files_changed=$(git diff origin/master --name-only | grep "*.py")
+  if [[ ! -z "${files_changed}" ]]; then
+    pylint "${files_changed}" --rcfile=setup.cfg
+  fi
 fi
