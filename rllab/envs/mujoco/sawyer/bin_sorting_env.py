6fcc5506d5151fd414a7686de504ced22bcd1529
zhanpenghe <zhanpenh@usc.edu>
2018-06-13 18:28:33 -0700

    Add mujoco sawyer envs

---
+++ rllab/envs/mujoco/sawyer/bin_sorting_env.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+from rllab.envs.base import Step
+from rllab.envs.mujoco.mujoco_env import MujocoEnv
+from rllab.core.serializable import Serializable
+from rllab.misc.overrides import overrides
+
+
+class BinSortingEnv(MujocoEnv, Serializable):
+
+    FILE = 'bin_sorting.xml'
+
+    def __init__(self,
+                 green_bin_center=(0.85, 0.25),
+                 red_bin_center=(0.85, 0.55),
+                 blue_bin_center=(0.85, -0.05),
+                 *args,
+                 **kwargs):
+        Serializable.__init__(self, *args, **kwargs)
+        self._green_bin_center = green_bin_center
+        self._red_bin_center = red_bin_center
+        self._blue_bin_center = blue_bin_center
+        self._bin_radius = 0.08
+
+        self._green_done = False
+        self._red_done = False
+        self._blue_done = False
+
+        super(BinSortingEnv, self).__init__(*args, **kwargs)
+
+    @overrides
+    def step(self, action):
+        self.forward_dynamics(action)
+        obs = self.get_current_obs()
+        reward = self.compute_reward()
+        done = (self._green_done and self._red_done and self._blue_done)
+        return Step(obs, reward, done)
+
+    @overrides
+    def get_current_obs(self):
+        grip_pos = self.sim.data.get_site_xpos('grip')
+        dt = self.sim.nsubsteps * self.sim.model.opt.timestep
+        grip_velp = self.sim.data.get_site_xvelp('grip') * dt
+
+        qpos = self.sim.data.qpos
+        qvel = self.sim.data.qvel
+
+        green_object_pos = self.sim.data.get_site_xpos('object0')
+        red_object_pos = self.sim.data.get_site_xpos('object1')
+        blue_object_pos = self.sim.data.get_site_xpos('object2')
+
+        obs = np.concatenate([
+            grip_pos,
+            green_object_pos,
+            red_object_pos,
+            blue_object_pos,
+            grip_velp,
+            qpos,
+            qvel,
+        ])
+
+        return obs
+
+    def compute_reward(self):
+        green_object_pos = self.sim.data.get_site_xpos('object0')
+        red_object_pos = self.sim.data.get_site_xpos('object1')
+        blue_object_pos = self.sim.data.get_site_xpos('object2')
+
+        if self.in_circle(self._green_bin_center, green_object_pos):
+            self._green_done = True
+        if self.in_circle(self._red_bin_center, red_object_pos):
+            self._red_done = True
+        if self.in_circle(self._blue_bin_center, blue_object_pos):
+            self._blue_done = True
+
+        if self._green_done and self._blue_done and self._red_done:
+            return 100
+        else:
+            return -1
+
+    def in_circle(self, center, obj_pos):
+        distance = np.sqrt((obj_pos[0] - center[0])**2 +
+                           (obj_pos[1] - center[1])**2)
+        return distance < self._bin_radius
+
+    @overrides
+    def reset(self, init_state=None):
+        self._green_done = False
+        self._blue_done = False
+        self._red_done = False
+        return super(BinSortingEnv, self).reset(init_state)
---
+++ rllab/envs/mujoco/sawyer/block_stacking_env.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+from rllab.envs.base import Step
+from rllab.envs.mujoco.mujoco_env import MujocoEnv
+from rllab.core.serializable import Serializable
+from rllab.misc.overrides import overrides
+
+
+class BlockStackingEnv(MujocoEnv, Serializable):
+
+    FILE = 'block_stacking.xml'
+
+    def __init__(self, block_size=0.025, *args, **kwargs):
+        Serializable.__init__(self, *args, **kwargs)
+        super(BlockStackingEnv, self).__init__(*args, **kwargs)
+        self._distance_threshold = block_size / 2.
+        self._done = False
+
+    @overrides
+    def step(self, action):
+        self.forward_dynamics(action)
+        obs = self.get_current_obs()
+        reward = self.compute_reward()
+        done = self._done
+
+        return Step(obs, reward, done)
+
+    def compute_reward(self):
+        green_pos = self.sim.data.get_site_xpos('object0')
+        red_pos = self.sim.data.get_site_xpos('object1')
+        blue_pos = self.sim.data.get_site_xpos('object2')
+
+        if self._is_stacked(green_pos, red_pos) and self._is_stacked(
+                green_pos, blue_pos) and self._is_stacked(red_pos, blue_pos):
+            self._done = True
+            return 100
+        else:
+            return -1
+
+    @overrides
+    def get_current_obs(self):
+        grip_pos = self.sim.data.get_site_xpos('grip')
+        dt = self.sim.nsubsteps * self.sim.model.opt.timestep
+        grip_velp = self.sim.data.get_site_xvelp('grip') * dt
+
+        qpos = self.sim.data.qpos
+        qvel = self.sim.data.qvel
+
+        green_object_pos = self.sim.data.get_site_xpos('object0')
+        red_object_pos = self.sim.data.get_site_xpos('object1')
+        blue_object_pos = self.sim.data.get_site_xpos('object2')
+
+        obs = np.concatenate([
+            grip_pos,
+            green_object_pos,
+            red_object_pos,
+            blue_object_pos,
+            grip_velp,
+            qpos,
+            qvel,
+        ])
+
+        return obs
+
+    def _is_stacked(self, com_1, com_2):
+        distance = np.sqrt((com_1[0] - com_2[0])**2 + (com_1[1] - com_2[1])**2)
+        return distance < self._distance_threshold
+
+    @overrides
+    def reset(self, init_state=None):
+        self._done = False
+        return super(BlockStackingEnv, self).reset(init_state)
