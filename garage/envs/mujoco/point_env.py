e2286fd597e20b11e2a15d62181d79ad51d0c903
Jonathon Shen <jshen100@gmail.com>
2018-06-14 16:21:49 -0700

    Fixed PointEnv for test_env

--- garage/envs/mujoco/point_env.py
+++ garage/envs/mujoco/point_env.py
@@ -1,7 +1,6 @@
 import math
 
 import glfw
-import mujoco_py
 import numpy as np
 
 from garage.core import Serializable
@@ -23,28 +22,28 @@ class PointEnv(MujocoEnv, Serializable):
 
     def step(self, action):
         qpos = np.copy(self.sim.data.qpos)
-        qpos[2, 0] += action[1]
-        ori = qpos[2, 0]
+        qpos[2] += action[1]
+        ori = qpos[2]
         # compute increment in each direction
         dx = math.cos(ori) * action[0]
         dy = math.sin(ori) * action[0]
         # ensure that the robot is within reasonable range
-        qpos[0, 0] = np.clip(qpos[0, 0] + dx, -7, 7)
-        qpos[1, 0] = np.clip(qpos[1, 0] + dy, -7, 7)
-        self.sim.data.qpos = qpos
+        qpos[0] = np.clip(qpos[0] + dx, -7, 7)
+        qpos[1] = np.clip(qpos[1] + dy, -7, 7)
+        self.sim.data.qpos[:] = qpos
         self.sim.forward()
         next_obs = self.get_current_obs()
         return Step(next_obs, 0, False)
 
     def get_xy(self):
         qpos = self.sim.data.qpos
-        return qpos[0, 0], qpos[1, 0]
+        return qpos[0], qpos[1]
 
     def set_xy(self, xy):
         qpos = np.copy(self.sim.data.qpos)
-        qpos[0, 0] = xy[0]
-        qpos[1, 0] = xy[1]
-        self.sim.data.qpos = qpos
+        qpos[0] = xy[0]
+        qpos[1] = xy[1]
+        self.sim.data.qpos[:] = qpos
         self.sim.forward()
 
     @overrides
