cb32efbc90188c17814b434deb10ff5c0f74eac3
Angel Gonzalez <aigonzal@usc.edu>
2018-06-29 18:29:54 -0700

    Set up mujoco in setup_linux.sh and setup_osx.sh
    The script setup_mujoco.sh is called now from the scripts to install
    garage in Linux and OS X, since it's required by the packages installed
    by conda.
    Also, the three scripts were formatted with the style defined by Google.

--- scripts/setup_osx.sh
+++ scripts/setup_osx.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # Make sure that pip is available
 hash brew 2>/dev/null || {
-    echo "Please install homebrew before continuing. You can use the following command to install:"
-    echo "/usr/bin/ruby -e \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
-    exit 0
+  echo "Please install homebrew before continuing. You can use the" \
+    "following command to install:"
+  echo "/usr/bin/ruby -e \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
+  exit 0
 }
 
 hash conda 2>/dev/null || {
-    echo "Please install anaconda before continuing. You can download it at https://www.continuum.io/downloads. Please use the Python 2.7 installer."
-    exit 0
+  echo "Please install anaconda before continuing. You can download" \
+    "it at https://www.continuum.io/downloads. Please use the Python 2.7" \
+    " installer."
+  exit 0
 }
 
 
@@ -19,8 +22,17 @@ brew install swig sdl sdl_image sdl_mixer sdl_ttf portmidi
 
 # Make sure that we're under the directory of the project
 cd "$(dirname "$0")/.."
+
+echo "Setting up MuJoCo..."
+/bin/bash ./scripts/setup_mujoco.sh
+if [[ "${?}" -ne 0 ]]; then
+  echo -e "\e[0;31mError: mujoco couldn't be set up\e[0m"
+  exit 1
+fi
+
 echo "Creating conda environment..."
 conda env create -f environment.yml
 conda env update
 
-echo "Conda environment created! Make sure to run \`source activate garage\` whenever you open a new terminal and want to run programs under garage."
+echo "Conda environment created! Make sure to run \`source activate garage\`" \
+  "whenever you open a new terminal and want to run programs under garage."
