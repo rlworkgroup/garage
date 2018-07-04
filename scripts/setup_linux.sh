28615c539674b39af590dae973cf253909770322
Angel Ivan Gonzalez <35857569+gonzaiva@users.noreply.github.com>
2018-07-03 18:12:19 -0700

    Set up mujoco in setup_linux.sh and setup_osx.sh
    The script setup_mujoco.sh is called now from the scripts to install
    garage in Linux and OS X, since it's required by the packages installed
    by conda.
    Also, the three scripts were formatted with the style defined by Google.

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -2,48 +2,60 @@
 # Make sure that conda is available
 
 hash conda 2>/dev/null || {
-    echo "Please install anaconda before continuing. You can download it at https://www.continuum.io/downloads. Please use the Python 2.7 installer."
-    exit 0
+  echo "Please install anaconda before continuing. You can download it at" \
+    "https://www.continuum.io/downloads. Please use the Python 2.7" \
+    "installer."
+  exit 0
 }
 
 echo "Installing system dependencies"
 echo "You will probably be asked for your sudo password."
 sudo apt-get update
-sudo apt-get install -y python-pip python-dev swig cmake build-essential zlib1g-dev
+sudo apt-get install -y python-pip python-dev swig cmake build-essential \
+  zlib1g-dev
 sudo apt-get build-dep -y python-pygame
 sudo apt-get build-dep -y python-scipy
 
 # Make sure that we're under the directory of the project
 cd "$(dirname "$0")/.."
 
+echo "Setting up MuJoCo..."
+/bin/bash ./scripts/setup_mujoco.sh
+if [[ "${?}" -ne 0 ]]; then
+  echo -e "\e[0;31mError: MuJoCo couldn't be set up\e[0m"
+  exit 1
+fi
+
 echo "Creating conda environment..."
 conda env create -f environment.yml
-if [ $? -ne 0 ]; then
-   echo -e "\e[0;31mError: conda environment could not be created\e[0m"
-   exit 1
+if [[ "${?}" -ne 0 ]]; then
+  echo -e "\e[0;31mError: conda environment could not be created\e[0m"
+  exit 1
 fi
 
 env_name="garage"
 tf_version="1.8"
-read -r -p "Install tensorflow-gpu instead of regular tensorflow [y/N]: " response
-case "$response" in
+read -r -p "Install tensorflow-gpu instead of regular tensorflow [y/N]: " \
+  response
+case "${response}" in
   [yY])
-     source activate $env_name
-     pip install tensorflow-gpu==$tf_version
-     source deactivate
-     ;;
+    source activate "${env_name}"
+    pip install tensorflow-gpu=="${tf_version}"
+    source deactivate
+    ;;
   *)
-     source activate $env_name
-     pip install tensorflow==$tf_version
-     source deactivate
-     ;;
+    source activate "${env_name}"
+    pip install tensorflow=="${tf_version}"
+    source deactivate
+    ;;
 esac
 
 echo "Updating conda environment..."
 conda env update
-if [ $? -ne 0 ]; then
-   echo -e "\e[0;31mError: conda environment could not be updated\e[0m"
-   exit 1
+if [[ "${?}" -ne 0 ]]; then
+  echo -e "\e[0;31mError: conda environment could not be updated\e[0m"
+  exit 1
 fi
 
-echo "Conda environment created! Make sure to run \`source activate garage\` whenever you open a new terminal and want to run programs under garage."
+echo "Conda environment created! Make sure to run \`source activate garage\`" \
+  "whenever you open a new terminal and want to run programs under garage."
