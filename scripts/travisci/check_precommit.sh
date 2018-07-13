b76729bce9375036ad5099acfccadafe138c9e9f
Ryan Julian <ryanjulian@users.noreply.github.com>
2018-07-12 21:58:28 -0700

    Check commit messages in the CI (#190)

--- scripts/travisci/check_precommit.sh
+++ scripts/travisci/check_precommit.sh
@@ -1,8 +1,47 @@
 #!/usr/bin/env bash
+status=0
+
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" && "${TRAVIS}" == "true" ]]; then
-  pre-commit run --source ${TRAVIS_COMMIT_RANGE%...*} --origin ${TRAVIS_COMMIT_RANGE#*...}
+  pre-commit run --source "${TRAVIS_COMMIT_RANGE%...*}" \
+                 --origin "${TRAVIS_COMMIT_RANGE#*...}"
+  status="$((${status} | ${?}))"
+
+  # Check commit messages
+  while read commit; do
+    commit_msg="$(mktemp)"
+    git log --format=%B -n 1 "${commit}" > "${commit_msg}"
+    pre-commit run --hook-stage commit-msg --commit-msg-file="${commit_msg}"
+    pass=$?
+    status="$((${status} | ${pass}))"
+
+    # Print message if it fails
+    if [[ "${pass}" -ne 0 ]]; then
+      echo "Failing commit message:"
+      cat "${commit_msg}"
+    fi
+
+  done < <(git rev-list "${TRAVIS_COMMIT_RANGE}")
 else
   git remote set-branches --add origin master
   git fetch
   pre-commit run --source origin/master --origin ${TRAVIS_BRANCH}
+  status="$((${status} | ${?}))"
+
+  # Check commit messages
+  while read commit; do
+    commit_msg="$(mktemp)"
+    git log --format=%B -n 1 "${commit}" > "${commit_msg}"
+    pre-commit run --hook-stage commit-msg --commit-msg-file="${commit_msg}"
+    pass=$?
+    status="$((${status} | ${pass}))"
+
+    # Print message if it fails
+    if [[ "${pass}" -ne 0 ]]; then
+      echo "Failing commit message:"
+      cat "${commit_msg}"
+    fi
+
+  done < <(git rev-list origin/master..."${TRAVIS_BRANCH}")
 fi
+
+exit "${status}"
