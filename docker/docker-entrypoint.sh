3dc86719a9779a04c7ed6d4290595b3d9fcac87d
Ryan Julian <rjulian@usc.edu>
2018-07-18 09:27:40 -0700

    Use Docker for TravisCI
    * Adds headless Docker container with all garage dependencies
    * Changes some deps in environment.yml to fix build problems
    * Updates .travis.yml to produce and used cached Docker images
      from Docker Hub
    * Deletes old Dockerfiles

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Dockerfile entrypoint
+set -e
+
+# Get MuJoCo key from the environment
+echo "${MJKEY}" > /root/.mujoco/mjkey.txt
+
+# Setup dummy X server display
+display_num=0
+export DISPLAY=:"${display_num}"
+Xvfb "${DISPLAY}" -screen 0 1024x768x24 &
+pulseaudio -D --exit-idle-time=-1
+
+# Wait for X to come up
+file="/tmp/.X11-unix/X${display_num}"
+for i in $(seq 1 10); do
+    if [ -e "$file" ]; then
+      break
+    fi
+    echo "Waiting for X to start (i.e. $file to be created) (attempt $i/10)"
+    sleep "$i"
+done
+if ! [ -e "$file" ]; then
+    echo "Timed out waiting for X to start: $file was not created"
+    exit 1
+fi
+
+# Activate conda environment
+source activate garage
+
+exec "$@"
