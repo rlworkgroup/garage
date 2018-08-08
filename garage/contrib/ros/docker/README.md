96ac10340715291ade1a8c8c74c8b9fe79dc4484
hjzh <hjzh578@gmail.com>
2018-08-08 02:14:26 -0700

    nvidia should work

--- garage/contrib/ros/docker/README.md
+++ garage/contrib/ros/docker/README.md
@@ -9,7 +9,7 @@
 - build
     ```bash
     # specify gpu if you are using NVIDIA graphic cards.
-    $ ./garage/contrib/ros/docker/build.sh robot_name [cpu | gpu ]
+    $ ./garage/contrib/ros/docker/build.sh [ robot_name ] [cpu | gpu ]
     ```
 - run
     ```bash
@@ -18,3 +18,47 @@
     # gpu
     $ ./garage/contrib/ros/docker/run_gpu.sh robot_name
     ```
+- copy the intera.sh script
+    The intera.sh file already exists in intera_sdk repo, copy the file into your ros workspace.
+
+    ```bash
+    $ cd /home/$USER/ros_ws/
+    $ cp src/intera_sdk/intera.sh .
+    ```
+- customize the intera.sh script
+    ```bash
+    $ cd /home/$USER/ros_ws/
+    $ vim intera.sh
+    # Edit the 'robot_hostname' field
+    # Edit the 'your_ip' field
+    # Verify 'ros_version' field
+    # update: ros_version='kinetic'
+    ```
+- start gazebo sawyer
+    ```bash
+
+    $ cd $ROS_WS
+    $ ./intera.sh sim
+    $ export PYTHONPATH=/opt/ros/kinetic/lib/python2.7/dist-packages:$ROS_WS/devel/lib/python3/dist-packages
+    $ roslaunch sawyer_gazebo sawyer_learning.launch
+    ```
+- start sawyer moveit trajectory server
+    ```bash
+    $ cd $ROS_WS
+    $ ./intera.sh sim
+    $ source activate garage
+    $ rosrun intera_interface enable_robot.py -e
+    $ rosrun intera_interface joint_trajectory_action_server.py
+    ```
+- start sawyer moveit 
+    ```bash
+    $ cd $ROS_WS
+    $ ./intera.sh sim
+    $ source activate garage
+    # with gripper
+    $ roslaunch sawyer_moveit_config sawyer_moveit.launch electric_gripper:=true
+    # without gripper
+    $ roslaunch sawyer_moveit_config sawyer_moveit.launch
+    ```    
+## Trouble Shooting
+- Notice moveit sawyer collision definition.
--- garage/contrib/ros/docker/sawyer/docker-entrypoint.sh
+++ garage/contrib/ros/docker/sawyer/docker-entrypoint.sh
@@ -18,6 +18,12 @@ echo "export HOME=/home/$USER" >> "/home/$USER/.bashrc"
 export PATH=$PATH:/opt/conda/bin
 echo 'export PATH=$PATH:/opt/conda/bin' >> "/home/$USER/.bashrc"
 
+export GARAGE_CODE_ROOT=/root/code/garage
+echo 'export GARAGE_CODE_ROOT=/root/code/garage' >> "/home/$USER/.bashrc"
+
+export ROS_WS=/home/$USER/ros_ws
+echo 'export ROS_WS=/home/$USER/ros_ws' >> "/home/$USER/.bashrc"
+
 export HOME=/home/$USER
 
 if [ ! -d "/home/$USER/ros_ws/src" ]; then
@@ -101,75 +107,110 @@ install_from_git "octomap" "Octomap" "https://github.com/OctoMap/octomap.git"
 
 # Install fcl
 if [ ! -d "/home/$USER/dev/fcl/build" ] && [ ! -f "/home/$USER/dev/fcl/build" ]; then
-		echo -e "${ORANGE}Building and installing fcl.${WHITE}"
-		git clone https://github.com/flexible-collision-library/fcl.git \
-			&& cd fcl && git checkout fcl-0.5 \
-			&& mkdir build && cd build \
-			&& cmake .. && make -j4 && make install
-		popd
-		pushd .
-	else
-		echo -e "${ORANGE}fcl already exists, just need to install.${WHITE}"
-		cd "/home/$USER/dev/fcl/build" && make install
-		popd
-		pushd .
+	echo -e "${ORANGE}Building and installing fcl.${WHITE}"
+	git clone https://github.com/flexible-collision-library/fcl.git \
+		&& cd fcl && git checkout fcl-0.5 \
+		&& mkdir build && cd build \
+		&& cmake .. && make -j4 && make install
+	popd
+	pushd .
+else
+	echo -e "${ORANGE}fcl already exists, just need to install.${WHITE}"
+	cd "/home/$USER/dev/fcl/build" && make install
+	popd
+	pushd .
 fi
 
 # Install OMPL
 install_from_targz "ompl-1.3.1" "OMPL 1.3.1" "https://github.com/ompl/ompl/archive/1.3.1.tar.gz"
 
 # Prepare Intera SDK installation
-cd "/home/$USER/ros_ws/src"
-echo -e "Retrieving Instera SDK sources."
-wstool init .
-git clone https://github.com/RethinkRobotics/sawyer_robot.git
-wstool merge sawyer_robot/sawyer_robot.rosinstall
-wstool update
-cd "/home/$USER/ros_ws/src"
-git clone https://github.com/ros/geometry.git
-cd geometry
-git checkout indigo-devel
-cd "/home/$USER/ros_ws/src"
-git clone https://github.com/ros/geometry2.git
-cd geometry2
-git checkout indigo-devel
+if [ ! -d "$ROS_WS/src/sawyer_robot" ] && [ ! -f "$ROS_WS/src/sawyer_robot" ]; then
+  cd "/home/$USER/ros_ws/src"
+  echo -e "Retrieving Instera SDK sources."
+  wstool init .
+  git clone https://github.com/RethinkRobotics/sawyer_robot.git
+  wstool merge sawyer_robot/sawyer_robot.rosinstall
+  wstool update
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/ros/geometry.git
+  cd geometry
+  git checkout indigo-devel
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/ros/geometry2.git
+  cd geometry2
+  git checkout indigo-devel
+fi
 
 # Prepare sawyer gazebo simulator installation
-cd "/home/$USER/ros_ws/src"
-git clone https://github.com/RethinkRobotics/sawyer_simulator.git
-wstool merge sawyer_simulator/sawyer_simulator.rosinstall
-wstool update
+if [ ! -d "$ROS_WS/src/sawyer_simulator" ] && [ ! -f "$ROS_WS/src/sawyer_simulator" ]; then
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/RethinkRobotics/sawyer_simulator.git
+  wstool merge sawyer_simulator/sawyer_simulator.rosinstall
+  wstool update
+fi
+
+# Prepare sawyer moveit installation
+if [ ! -d "$ROS_WS/src/sawyer_moveit" ] && [ ! -f "$ROS_WS/src/sawyer_moveit" ]; then
+  cd "/home/$USER/ros_ws/src"
+  wstool merge https://raw.githubusercontent.com/RethinkRobotics/sawyer_moveit/master/sawyer_moveit.rosinstall
+  wstool update
+fi
 
 # Prepare Moveit! installation
 # Get source
-cd "/home/$USER/ros_ws/src"
-git clone https://github.com/wg-perception/object_recognition_msgs.git
-git clone https://github.com/OctoMap/octomap_msgs.git
-cd octomap_msgs
-git checkout indigo-devel
-cd "/home/$USER/ros_ws/src"
-git clone https://github.com/ros/urdf_parser_py.git
-cd urdf_parser_py
-git checkout indigo-devel
-cd "/home/$USER/ros_ws/src"
-git clone https://github.com/ros-planning/warehouse_ros.git
-cd warehouse_ros
-git checkout kinetic-devel
-cd "/home/$USER/ros_ws/src"
-git clone https://github.com/PickNikRobotics/rviz_visual_tools.git
-cd rviz_visual_tools
-git checkout kinetic-devel
-cd "/home/$USER/ros_ws/src"
-git clone https://github.com/davetcoleman/graph_msgs.git
-cd graph_msgs
-git checkout indigo-devel
+if [ ! -d "$ROS_WS/src/object_recognition_msgs" ] && [ ! -f "$ROS_WS/src/object_recognition_msgs" ]; then
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/wg-perception/object_recognition_msgs.git
+fi
+
+if [ ! -d "$ROS_WS/src/octomap_msgs" ] && [ ! -f "$ROS_WS/src/octomap_msgs" ]; then
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/OctoMap/octomap_msgs.git
+  cd octomap_msgs
+  git checkout indigo-devel
+fi
+
+if [ ! -d "$ROS_WS/src/urdf_parser_py" ] && [ ! -f "$ROS_WS/src/urdf_parser_py" ]; then
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/ros/urdf_parser_py.git
+  cd urdf_parser_py
+  git checkout indigo-devel
+fi
+
+if [ ! -d "$ROS_WS/src/warehouse_ros" ] && [ ! -f "$ROS_WS/src/warehouse_ros" ]; then
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/ros-planning/warehouse_ros.git
+  cd warehouse_ros
+  git checkout kinetic-devel
+fi
+
+if [ ! -d "$ROS_WS/src/rviz_visual_tools" ] && [ ! -f "$ROS_WS/src/rviz_visual_tools" ]; then
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/PickNikRobotics/rviz_visual_tools.git
+  cd rviz_visual_tools
+  git checkout kinetic-devel
+fi
+
+if [ ! -d "$ROS_WS/src/graph_msgs" ] && [ ! -f "$ROS_WS/src/graph_msgs" ]; then
+  cd "/home/$USER/ros_ws/src"
+  git clone https://github.com/davetcoleman/graph_msgs.git
+  cd graph_msgs
+  git checkout indigo-devel
+fi
+
+if [ ! -d "$ROS_WS/src/moveit" ] && [ ! -f "$ROS_WS/src/moveit" ]; then
+ cd "/home/$USER/ros_ws"
+ wstool merge -t src https://raw.githubusercontent.com/ros-planning/moveit/kinetic-devel/moveit.rosinstall
+ wstool update -t src
+ rosdep install -y --from-paths src --ignore-src --rosdistro kinetic
+ # Have to exclude some targets
+  # See https://github.com/ros-planning/moveit/issues/697
+  sed -i '/demo/s/^/# /g' "/home/$USER/ros_ws/src/moveit/moveit_ros/planning_interface/move_group_interface/CMakeLists.txt"
+fi
+
 cd "/home/$USER/ros_ws"
-wstool merge -t src https://raw.githubusercontent.com/ros-planning/moveit/kinetic-devel/moveit.rosinstall
-wstool update -t src
 rosdep install -y --from-paths src --ignore-src --rosdistro kinetic
-# Have to exclude some targets
-# See https://github.com/ros-planning/moveit/issues/697
-sed -i '/demo/s/^/# /g' "/home/$USER/ros_ws/src/moveit/moveit_ros/planning_interface/move_group_interface/CMakeLists.txt"
 
 # Build ROS_WS
 # convert python2 code to python3
@@ -180,7 +221,21 @@ cd "/home/$USER/ros_ws"
 
 export CMAKE_PREFIX_PATH=/usr/local:$CMAKE_PREFIX_PATH
 
+# See https://github.com/osmcode/pyosmium/issues/52
+# We need boost_python-py35
+rm /usr/lib/x86_64-linux-gnu/libboost_python.so
+ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py35.so /usr/lib/x86_64-linux-gnu/libboost_python.so
+
 catkin_make -DPYTHON_EXECUTABLE:FILEPATH=$GARAGE_PYTHON -DCATKIN_BLACKLIST_PACKAGES='moveit_setup_assistant'
+
+# Copy the sawyer simulation launch file and simulation world file
+cp $GARAGE_CODE_ROOT/garage/contrib/ros/envs/sawyer/sawyer_learning.launch $ROS_WS/src/sawyer_simulator/sawyer_gazebo/launch
+cp $GARAGE_CODE_ROOT/garage/contrib/ros/envs/sawyer/sawyer_world_learning.launch $ROS_WS/src/sawyer_simulator/sawyer_gazebo/launch
+cp $GARAGE_CODE_ROOT/garage/contrib/ros/envs/sawyer/sawyer_learning.world $ROS_WS/src/sawyer_simulator/sawyer_gazebo/worlds/
+
+if [ -f /opt/ros/kinetic/lib/python2.7/dist-packages/cv2.so ] ; then
+  rm /opt/ros/kinetic/lib/python2.7/dist-packages/cv2.so
+fi
 
 echo "
 function run_gazebo() {
