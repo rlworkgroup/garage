76cb274d25bf0888f758a0a206a5226eea3e2395
hjzh <hjzh578@gmail.com>
2018-08-08 11:56:55 -0700

    Have Dockerfile.cpu and Dockerfile.gpu

--- garage/contrib/ros/docker/build.sh
+++ garage/contrib/ros/docker/build.sh
@@ -7,14 +7,12 @@ DEVICE=${2:-"cpu"}
 if [ "$ROBOT" = "sawyer" ] ; then
   if [ "$DEVICE" = "cpu" ] ; then
     echo "Building sawyer-ros-docker with cpu..." ;
-    docker build -f ./garage/contrib/ros/docker/sawyer/Dockerfile \
-      -t sawyer-ros-docker:cpu \
-      --build-arg DOCKER_FROM="ubuntu:16.04" . ;
+    docker build -f ./garage/contrib/ros/docker/sawyer/Dockerfile.cpu \
+      -t sawyer-ros-docker:cpu . ;
   else
     echo "Building sawyer-ros-docker with gpu..." ;
-    docker build -f ./garage/contrib/ros/docker/sawyer/Dockerfile \
-      -t sawyer-ros-docker:gpu \
-      --build-arg DOCKER_FROM="tensorflow/tensorflow:1.8.0-gpu-py3" . ;
+    docker build -f ./garage/contrib/ros/docker/sawyer/Dockerfile.gpu \
+      -t sawyer-ros-docker:gpu . ;
   fi
 else
   echo "The robot "$ROBOT" is not supported by us!" ;
