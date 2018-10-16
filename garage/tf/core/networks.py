7578cd95c83a3c77f289592dc430d2249ba938c2
Anson Wong <ahtsans@gmail.com>
2018-10-15 17:15:00 -0700

    MLP as function, added batch normalization layer

--- garage/tf/core/networks.py
+++ garage/tf/core/networks.py
@@ -1,71 +1,52 @@
 import tensorflow as tf
-from garage.core import Serializable
 
 
-class MLPs(Serializable):
-    def __init__(
-            self,
-            output_dim,
-            hidden_sizes,
-            hidden_nonlinearity,
-            output_nonlinearity,
-            name=None,
-            hidden_w_init=tf.contrib.layers.xavier_initializer(),
-            hidden_b_init=tf.zeros_initializer(),
-            output_w_init=tf.contrib.layers.xavier_initializer(),
-            output_b_init=tf.zeros_initializer(),
-            input_var=None,
-            input_shape=None,
-            batch_normalization=False,
-            weight_normalization=False,
-    ):
+def mlp(input_var,
+        output_dim,
+        hidden_sizes,
+        hidden_nonlinearity=tf.nn.relu,
+        output_nonlinearity=None,
+        name="mlp",
+        hidden_w_init=tf.contrib.layers.xavier_initializer(),
+        hidden_b_init=tf.zeros_initializer(),
+        output_w_init=tf.contrib.layers.xavier_initializer(),
+        output_b_init=tf.zeros_initializer(),
+        batch_normalization=False,
+        weight_normalization=False,
+        is_training=True,
+        debug=False):
 
-        Serializable.quick_init(self, locals())
+    _layers = []
 
-        with tf.variable_scope(name):
-            if input_var is None:
-                l_in = tf.placeholder(
-                    tf.float32,
-                    shape=(None, ) + input_shape,  # dynamic batch dimension
-                    name="input")
-            else:
-                l_in = input_var
-            self._layers = [l_in]
-            l_hid = l_in
-            for idx, hidden_size in enumerate(hidden_sizes):
-                l_hid = tf.layers.dense(
-                    inputs=l_hid,
-                    units=hidden_size,
-                    activation=hidden_nonlinearity,
-                    kernel_initializer=hidden_w_init,
-                    bias_initializer=hidden_b_init,
-                    name="hidden_%d" % idx)
-
-                self._layers.append(l_hid)
-            l_out = tf.layers.dense(
+    with tf.variable_scope(name):
+        l_hid = input_var
+        _layers = [l_hid]
+        if batch_normalization:
+            l_hid = tf.layers.batch_normalization(l_hid, training=is_training)
+        for idx, hidden_size in enumerate(hidden_sizes):
+            l_hid = tf.layers.dense(
                 inputs=l_hid,
-                units=output_dim,
-                activation=output_nonlinearity,
-                kernel_initializer=output_w_init,
-                bias_initializer=output_b_init,
-                name="output")
+                units=hidden_size,
+                activation=hidden_nonlinearity,
+                kernel_initializer=hidden_w_init,
+                bias_initializer=hidden_b_init,
+                name="hidden_%d" % idx,
+            )
 
-            self._layers.append(l_out)
-            self._l_in = l_in
-            self._l_out = l_out
+            _layers.append(l_hid)
+        l_out = tf.layers.dense(
+            inputs=l_hid,
+            units=output_dim,
+            activation=output_nonlinearity,
+            kernel_initializer=output_w_init,
+            bias_initializer=output_b_init,
+            name="output")
+        if batch_normalization:
+            l_out = tf.layers.batch_normalization(l_out, training=is_training)
+        _layers.append(l_out)
+        _l_out = l_out
 
-    @property
-    def input_layer(self):
-        return self._l_in
-
-    @property
-    def output_layer(self):
-        return self._l_out
-
-    @property
-    def input_var(self):
-        return self._l_in
-
-    @property
-    def layers(self):
-        return self._layers
+    if debug:
+        return _l_out, _layers
+    else:
+        return _l_out
--- tests/garage/tf/core/test_networks.py
+++ tests/garage/tf/core/test_networks.py
@@ -1,68 +1,75 @@
-import unittest
-
 import tensorflow as tf
-from garage.tf.core.networks import MLPs
+from garage.tf.core.networks import mlp
 import numpy as np
+from tests.fixtures import TfGraphTestCase
 
 
-class TestNetworks(unittest.TestCase):
-    def test_networks(self):
-        obs_input = np.array([[1, 2, 3, 4]])
-        obs_dim = obs_input.shape[1]
-        action_dim = 2
-        activation_f = tf.nn.relu
-        hidden_sizes = (32, 32)
-        bias = np.random.rand(1)
+class TestNetworks(TfGraphTestCase):
+    def setUp(self):
+        super(TestNetworks, self).setUp()
+        self.obs_input = np.array([[1, 2, 3, 4]])
+        input_shape = self.obs_input.shape[1]  # 4
 
-        with tf.variable_scope("Test"):
-            obs_t_ph = tf.placeholder(
-                tf.float32, [None, obs_dim], name="input")
+        self._input = tf.placeholder(
+            tf.float32, shape=(None, input_shape), name="input")
 
-            mlps = MLPs(
-                name="mlps",
-                input_shape=(obs_dim, ),
-                output_dim=action_dim,
-                input_var=obs_t_ph,
-                hidden_sizes=hidden_sizes,
-                hidden_nonlinearity=activation_f,
-                output_nonlinearity=None,
-                hidden_b_init=tf.constant_initializer(value=bias))
+        self._output_shape = 2
 
-            with tf.Session() as sess:
-                sess.run(tf.global_variables_initializer())
+        self.mlp_f, self.mlp_all_layers_f = mlp(
+            input_var=self._input,
+            output_dim=self._output_shape,
+            hidden_sizes=(32, 32),
+            hidden_b_init=tf.constant_initializer(value=np.random.rand(1)),
+            name="mlp",
+            debug=True)
 
-                with tf.variable_scope("mlps", reuse=True):
-                    w = tf.get_variable("hidden_0/kernel")
-                    h1_w = sess.run(w)
-                    b = tf.get_variable("hidden_0/bias")
-                    h1_b = sess.run(b)
-                    w = tf.get_variable("hidden_1/kernel")
-                    h2_w = sess.run(w)
-                    b = tf.get_variable("hidden_1/bias")
-                    h2_b = sess.run(b)
-                    w = tf.get_variable("output/kernel")
-                    out_w = sess.run(w)
-                    b = tf.get_variable("output/bias")
-                    out_b = sess.run(b)
+        self.sess.run(tf.global_variables_initializer())
 
-                # get output from session.run
-                layer_outputs = sess.run(
-                    mlps.layers, feed_dict={obs_t_ph: obs_input})
+    def test_input_shape(self):
+        mlp_outputs = self.sess.run(
+            [self.mlp_all_layers_f], feed_dict={self._input: self.obs_input})
+        # first sample, first layer
+        assert self._input.get_shape().as_list()[1] == mlp_outputs[0][0].shape[
+            1]
 
-                h1_out = layer_outputs[1]
-                h2_out = layer_outputs[2]
-                out = layer_outputs[3]
+    def test_output_shape(self):
+        mlp_output = self.sess.run(
+            [self.mlp_f], feed_dict={self._input: self.obs_input})
+        # first sample
+        assert mlp_output[0].shape[1] == self._output_shape
 
-                # check first layer
-                h2_in = np.dot(obs_input, h1_w) + h1_b
-                h2_in = sess.run(activation_f(h2_in))
-                np.testing.assert_array_almost_equal(h2_in, h1_out)
+    def test_output_value(self):
+        with tf.variable_scope("mlp", reuse=True):
+            w = tf.get_variable("hidden_0/kernel")
+            h1_w = self.sess.run(w)
+            b = tf.get_variable("hidden_0/bias")
+            h1_b = self.sess.run(b)
+            w = tf.get_variable("hidden_1/kernel")
+            h2_w = self.sess.run(w)
+            b = tf.get_variable("hidden_1/bias")
+            h2_b = self.sess.run(b)
+            w = tf.get_variable("output/kernel")
+            out_w = self.sess.run(w)
+            b = tf.get_variable("output/bias")
+            out_b = self.sess.run(b)
 
-                # second layer
-                h3_in = np.dot(h2_in, h2_w) + h2_b
-                h3_in = sess.run(activation_f(h3_in))
-                np.testing.assert_array_almost_equal(h3_in, h2_out)
+        mlp_output, mlp_outputs = self.sess.run(
+            [self.mlp_f, self.mlp_all_layers_f],
+            feed_dict={self._input: self.obs_input})
 
-                # output layer
-                h3_out = np.dot(h3_in, out_w) + out_b
-                np.testing.assert_array_almost_equal(h3_out, out)
+        h1_out = mlp_outputs[1]
+        h2_out = mlp_outputs[2]
+        out = mlp_outputs[3]
+
+        # check first layer
+        h2_in = np.dot(self.obs_input, h1_w) + h1_b
+        h2_in = self.sess.run(tf.nn.relu(h2_in))
+        np.testing.assert_array_almost_equal(h2_in, h1_out)
+        # second layer
+        h3_in = np.dot(h2_in, h2_w) + h2_b
+        h3_in = self.sess.run(tf.nn.relu(h3_in))
+        np.testing.assert_array_almost_equal(h3_in, h2_out)
+        # output layer
+        h3_out = np.dot(h3_in, out_w) + out_b
+        np.testing.assert_array_almost_equal(h3_out, out)
+        np.testing.assert_array_almost_equal(h3_out, mlp_output)
