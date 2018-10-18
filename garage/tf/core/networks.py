f4408281afe6ff70a433c76ea6b576c00afd6c10
Anson Wong <ahtsans@gmail.com>
2018-10-18 11:09:24 -0700

    Multiple fix to MLP function
    * Include docstring
    * Layer normalization instead of weight normalization
    * Inherit reuse flag from parent scope
    * Add test for layer normalization

--- garage/tf/core/networks.py
+++ garage/tf/core/networks.py
@@ -1,24 +1,51 @@
+"""
+Multilayer Perceptrons (MLP) with tensorflow as the only dependency.
+
+The module contains MLP which serves as the base of all networks.
+It aims to replace existing implementation of MLP class
+(garage.tf.core.network), which is under development.
+"""
 import tensorflow as tf
 
 
 def mlp(input_var,
         output_dim,
         hidden_sizes,
+        name,
         hidden_nonlinearity=tf.nn.relu,
-        output_nonlinearity=None,
-        name="mlp",
         hidden_w_init=tf.contrib.layers.xavier_initializer(),
         hidden_b_init=tf.zeros_initializer(),
+        output_nonlinearity=None,
         output_w_init=tf.contrib.layers.xavier_initializer(),
         output_b_init=tf.zeros_initializer(),
-        batch_normalization=False,
-        weight_normalization=False,
-        is_training=True):
+        layer_normalization=False):
+    """
+    MLP function.
 
+    Args:
+        input_var: Input tf.Tensor to the MLP.
+        output_dim: Dimension of the network output.
+        hidden_sizes: Output dimension of dense layer(s).
+        name: variable scope of the mlp.
+        hidden_nonlinearity: Activation function for
+                    intermediate dense layer(s).
+        hidden_w_init: Initializer function for the weight
+                    of intermediate dense layer(s).
+        hidden_b_init: Initializer function for the bias
+                    of intermediate dense layer(s).
+        output_nonlinearity: Activation function for
+                    output dense layer.
+        output_w_init: Initializer function for the weight
+                    of output dense layer(s).
+        output_b_init: Initializer function for the bias
+                    of output dense layer(s).
+        layer_normalization: Bool for using layer normalization or not.
+
+    Return:
+        The output tf.Tensor of the MLP
+    """
     with tf.variable_scope(name):
         l_hid = input_var
-        if batch_normalization:
-            l_hid = tf.layers.batch_normalization(l_hid, training=is_training)
         for idx, hidden_size in enumerate(hidden_sizes):
             l_hid = tf.layers.dense(
                 inputs=l_hid,
@@ -26,12 +53,8 @@ def mlp(input_var,
                 activation=hidden_nonlinearity,
                 kernel_initializer=hidden_w_init,
                 bias_initializer=hidden_b_init,
-                name="hidden_%d" % idx,
-            )
-            if batch_normalization:
-                l_hid = tf.layers.batch_normalization(
-                    l_hid, training=is_training)
-            if weight_normalization:
+                name="hidden_{}".format(idx))
+            if layer_normalization:
                 l_hid = tf.contrib.layers.layer_norm(l_hid)
         l_out = tf.layers.dense(
             inputs=l_hid,
@@ -40,9 +63,4 @@ def mlp(input_var,
             kernel_initializer=output_w_init,
             bias_initializer=output_b_init,
             name="output")
-        if batch_normalization:
-            l_out = tf.layers.batch_normalization(l_out, training=is_training)
-        if weight_normalization:
-            l_out = tf.contrib.layers.layer_norm(l_out)
-
     return l_out
