e89ce7ae9ba99a109da9ee58cca9f464988ce62f
Jonathon Shen <jonashen@users.noreply.github.com>
2018-10-30 14:17:48 -0700

    Disable flaky tests (#373)
    Disable SIGINT test so the CI can pass consistently.

--- scripts/travisci/check_tests.sh
+++ scripts/travisci/check_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./cc-test-reporter before-build
-coverage run -m nose2 -c setup.cfg -E 'not cron_job and not huge'
+coverage run -m nose2 -c setup.cfg -E 'not cron_job and not huge and not flaky'
 TEST_EXIT_CODE="$?"
 coverage xml
 bash <(curl -s https://codecov.io/bash)
