74eec28f8747a96471f6c069c256a7655691a27d
Zhanpeng He <31218416+zhanpenghe@users.noreply.github.com>
2018-11-01 18:27:59 -0700

    Add parameter without layers library (#378)
    This implements similar function as `garage.tf.core.layers.ParamLayer` but without garage layers library. This function will create variables of length `length` and broadcast it to the shape of `input_var.shape[:-1] + [length, ]`.

--- tests/garage/tf/core/test_networks.py
+++ tests/garage/tf/core/test_networks.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-from garage.tf.core.networks import mlp
+from garage.tf.core.networks import mlp, parameter
 from tests.fixtures import TfGraphTestCase
 
 
@@ -156,3 +156,26 @@ class TestNetworks(TfGraphTestCase):
             self.mlp_f_w_n, feed_dict={self._input: self.obs_input})
 
         np.testing.assert_array_almost_equal(out, mlp_output)
+
+    def test_param(self):
+        input_vars = tf.placeholder(shape=[None, 2, 3, 4], dtype=tf.float32)
+        initial_params = np.array([48, 21, 33])
+
+        init = tf.constant_initializer(initial_params)
+
+        params = parameter(
+            input_var=input_vars,
+            length=3,
+            initializer=init,
+        )
+
+        data = np.zeros(shape=[5, 2, 3, 4])
+        feed_dict = {
+            input_vars: data,
+        }
+
+        self.sess.run(tf.global_variables_initializer())
+        p = self.sess.run(params, feed_dict=feed_dict)
+
+        assert p.shape[:-1] == data.shape[:-1]
+        assert np.all(p[0, 0, 0, :] == initial_params)
