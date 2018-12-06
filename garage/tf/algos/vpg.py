f2c1b942592c5467ef4f485c6db64f821dfb1f5b
Chang Su <CatherineSue@users.noreply.github.com>
2018-12-06 14:05:12 -0800

    Fix bugs in tf/VPG (#418)
    1. Fix typo in pg_loss.
    2. The default batch_size of the optimizer in VPG should be None.
    3. The default max_epochs of the optimizer in VPG should be 1.

--- garage/tf/algos/vpg.py
+++ garage/tf/algos/vpg.py
@@ -10,11 +10,17 @@ class VPG(NPO):
 
     def __init__(self, optimizer=None, optimizer_args=None, **kwargs):
         if optimizer is None:
+            default_args = dict(
+                batch_size=None,
+                max_epochs=1,
+            )
             optimizer = FirstOrderOptimizer
             if optimizer_args is None:
-                optimizer_args = dict()
+                optimizer_args = default_args
+            else:
+                optimizer_args = dict(default_args, **optimizer_args)
         super(VPG, self).__init__(
-            pg_loss=PGLoss.Vanilla,
+            pg_loss=PGLoss.VANILLA,
             optimizer=optimizer,
             optimizer_args=optimizer_args,
             name="VPG",
