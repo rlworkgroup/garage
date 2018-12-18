3ced0d0e856d830a96a08f0a358b868d73e0d955
CatherineSue <changsu.cs.work@gmail.com>
2018-12-18 12:08:48 -0800

    Add tf/TNPG
    1. Add TNPG to TF. TNPG is NPO with ConjucateGradientOptimizer.
    2. Add example and test.

---
+++ garage/tf/algos/tnpg.py
@@ -0,0 +1,26 @@
+"""Truncated Natural Policy Gradient."""
+from garage.tf.algos.npo import NPO
+from garage.tf.optimizers import ConjugateGradientOptimizer
+
+
+class TNPG(NPO):
+    """
+    Truncated Natural Policy Gradient.
+
+    TNPG uses Conjugate Gradient to compute the policy gradient.
+
+    """
+
+    def __init__(self, optimizer=None, optimizer_args=None, **kwargs):
+        if optimizer is None:
+            optimizer = ConjugateGradientOptimizer
+            default_args = dict(max_backtracks=1)
+            if optimizer_args is None:
+                optimizer_args = default_args
+            else:
+                optimizer_args = dict(default_args, **optimizer_args)
+        super().__init__(
+            optimizer=optimizer,
+            optimizer_args=optimizer_args,
+            name="TNPG",
+            **kwargs)
