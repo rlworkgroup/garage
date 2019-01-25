bc12631f060af63011fd7d0ba514dbc51247d65b
Peter Lillian <plillian@usc.edu>
2019-01-24 17:00:48 -0800

    More docstrings

--- garage/logger/logger.py
+++ garage/logger/logger.py
@@ -2,6 +2,22 @@
 
 It takes in many different types of input and directs them to the correct
 output.
+
+The logger has 4 major steps:
+
+    1. Inputs, such as a simple string or something more complicated like
+    TabularInput, are passed to the log() method of an instantiated Logger.
+
+    2. The Logger class checks for any outputs that have been added to it, and
+    calls the record() method of any outputs that accept the type of input.
+
+    3. The output (a subclass of LogOutput) receives the input via its record()
+    method and handles it in whatever way is expected.
+
+    4. (only in some cases) The dump method is used to dump the output to file.
+    Some LogOutput subclasses, like StdOutput, do not instantiate this method,
+    but it is necessary for the TensorBoardLogger.
+
 """
 from contextlib import contextmanager
 from warnings import warn
@@ -18,8 +34,11 @@ class Logger:
     def log(self, data):
         """Magic method that takes in all different types of input.
 
+        This method is the main API for the logger. Any data to be logged goes
+        through this method.
+
         Any data sent to this method is sent to all outputs that accept its
-        type.
+        type (defined in the types_accepted property).
 
         :param data: Data to be logged. This can be any type specified in the
          types_accepted property of any of the logger outputs.
@@ -41,6 +60,8 @@ class Logger:
         """Add a new output to the logger.
 
         All data that is compatible with this output will be sent there.
+
+        :param output: An instantiation of a LogOutput subclass to be added.
         """
         self._outputs.append(output)
 
@@ -49,21 +70,27 @@ class Logger:
         self._outputs.clear()
 
     def remove_output(self, output_type):
-        """Remove all outputs of a given type."""
+        """Remove all outputs of a given type.
+
+        :param output_type: A LogOutput subclass type to be removed.
+        """
         self._outputs = [
             output for output in self._outputs
             if not isinstance(output, output_type)
         ]
 
     def reset_output(self, output):
-        """Removes, then re-adds a given output to the logger."""
+        """Removes, then re-adds a given output to the logger.
+
+        :param output: An instantiation of a LogOutput subclass to be added.
+        """
         self.remove_output(type(output))
         self.add_output(output)
 
     def has_output(self, output_type):
         """Checks to see if a given logger output is attached to the logger.
 
-        :param output_type: The type of output to check for.
+        :param output_type: A LogOutput subclass type to be checked for.
         """
         for output in self._outputs:
             if isinstance(output, output_type):
@@ -73,8 +100,8 @@ class Logger:
     def dump_output(self, output_type, step=None):
         """Dumps all outputs of the given type.
 
-        :param output_type: The type of output to check for.
-        :param step: he
+        :param output_type: A LogOutput subclass type to be dumped.
+        :param step: The current run step.
         """
         for output in self._outputs:
             if isinstance(output, output_type):
