b417578ae6f31fff8144db0f85722e5d4b6d0801
Ryan Julian <rjulian@usc.edu>
2019-01-31 18:39:43 -0800

    Move dm_control tests into the unit test tree
    This PR moves the dm_control admittance test into the test/garage unit
    test tree. It also rewrites how the tests are parameterized to be more
    idiomatic for nose2, and deletes a fixture in the process.

--- tests/fixtures/fixtures.py
+++ tests/fixtures/fixtures.py
@@ -1,10 +1,8 @@
 import gc
 import unittest
 
-from dm_control.suite import ALL_TASKS
 import tensorflow as tf
 
-from garage.envs.dm_control import DmControlEnv
 from garage.misc import ext
 import garage.misc.logger as logger
 
@@ -35,18 +33,3 @@ class TfGraphTestCase(unittest.TestCase):
         del self.graph
         del self.sess
         gc.collect()
-
-
-class DmParameterizedTestCase(unittest.TestCase):
-    def __init__(self, method_name='runTest', param=ALL_TASKS[0]):
-        super().__init__(method_name)
-        self.env = DmControlEnv(domain_name=param[0], task_name=param[1])
-
-    @staticmethod
-    def parameterize(child_class, param=None):
-        testloader = unittest.TestLoader()
-        testnames = testloader.getTestCaseNames(child_class)
-        suite = unittest.TestSuite()
-        for name in testnames:
-            suite.addTest(child_class(name, param=param))
-        return suite
