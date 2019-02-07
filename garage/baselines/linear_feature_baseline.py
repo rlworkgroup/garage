59df18712c9d2930688de240f309075b203ce96c
Peter Lillian <plillian@usc.edu>
2019-02-07 12:04:28 -0800

    Applying yapf

--- garage/baselines/linear_feature_baseline.py
+++ garage/baselines/linear_feature_baseline.py
@@ -33,8 +33,8 @@ class LinearFeatureBaseline(Baseline):
         reg_coeff = self._reg_coeff
         for _ in range(5):
             self._coeffs = np.linalg.lstsq(
-                featmat.T.dot(featmat)
-                + reg_coeff * np.identity(featmat.shape[1]),
+                featmat.T.dot(featmat) +
+                reg_coeff * np.identity(featmat.shape[1]),
                 featmat.T.dot(returns),
                 rcond=-1)[0]
             if not np.any(np.isnan(self._coeffs)):
