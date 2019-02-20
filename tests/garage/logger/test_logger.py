7636edb475f749045366de60ad8f811b2543747b
Peter Lillian <plillian@usc.edu>
2019-02-20 14:38:03 -0800

    Second TextOutput test

--- tests/garage/logger/test_logger.py
+++ tests/garage/logger/test_logger.py
@@ -15,12 +15,22 @@ class TestLogger(unittest.TestCase):
         try:
             text_output = TextOutput(log_file)
             logger.add_output(text_output)
+
             text = "TESTING 123 GARAGE"
             logger.log(text)
             with open(log_file, 'r') as file:
                 read = file.read().split(text_output._delimiter, 1)[1]
                 read = read.rstrip('\n')
                 assert read == text
+
+            more_text = "MORE TESTING"
+            logger.log(more_text)
+            with open(log_file, 'r') as file:
+                read = file.read().split('\n')
+                for i in range(len(read) - 1):
+                    read[i] = read[i].split(text_output._delimiter, 1)[1]
+                assert read[0] == text
+                assert read[1] == more_text
         finally:
             os.remove(log_file)
 
