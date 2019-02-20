fc940125839c248a74d6e30964a5103c97c3de3a
Peter Lillian <plillian@usc.edu>
2019-02-20 12:46:35 -0800

    TestLogger changes

--- tests/garage/test_logger.py
+++ tests/garage/test_logger.py
@@ -3,11 +3,11 @@ Auxiliary coverage test for logger.
 
 This test covers snapshot and parameter logging.
 """
-import os
 from types import SimpleNamespace
 import unittest
 
 from garage.logger import logger, snapshotter, StdOutput, tabular
+from garage.misc.console import mkdir_p
 
 
 class TestLogger(unittest.TestCase):
@@ -15,10 +15,13 @@ class TestLogger(unittest.TestCase):
         args = SimpleNamespace()
         args.args_data = ""
 
-        logger.add_output(StdOutput())
-        snapshotter.set_snapshot_dir("exp")
+        snap_dir = "exp"
+        mkdir_p(snap_dir)
+
+        snapshotter.snapshot_dir = snap_dir
         snapshotter.save_itr_params(1, {})
+
+        logger.add_output(StdOutput())
         tabular.record_misc_stat("key", 1)
-        if os.path.isfile("exp-log"):
-            os.remove("exp-log")
+
         logger.log(tabular)
