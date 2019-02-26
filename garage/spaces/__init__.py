edcba959bacc7b01745c95a37ae659f342fd8dff
K.R. Zentner <krzentner@gmail.com>
2019-02-25 16:53:57 -0800

    Delete garage.spaces

--- garage/spaces/__init__.py
+++
@@ -1,7 +0,0 @@
-from garage.spaces.base import Space
-from garage.spaces.box import Box
-from garage.spaces.dict import Dict
-from garage.spaces.discrete import Discrete
-from garage.spaces.tuple import Tuple
-
-__all__ = ["Space", "Box", "Dict", "Discrete", "Tuple"]
--- garage/tf/spaces/__init__.py
+++
@@ -1,8 +0,0 @@
-"""TensorFlow extension of garage.spaces."""
-
-from garage.tf.spaces.box import Box
-from garage.tf.spaces.dict import Dict
-from garage.tf.spaces.discrete import Discrete
-from garage.tf.spaces.tuple import Tuple
-
-__all__ = ["Box", "Dict", "Discrete", "Tuple"]
