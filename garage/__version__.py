07ca082a57b9f6fc9f0df69814118fd2ad238af7
Ryan Julian <ryanjulian@users.noreply.github.com>
2019-03-05 19:14:44 -0800

    Update next version
    The next major version is v2019.06.0

--- garage/__version__.py
+++ garage/__version__.py
@@ -2,6 +2,6 @@
 import os
 
 # Update this every release to be 'v{next release number}'
-NEXT_VERSION = 'v2019.02.0'
+NEXT_VERSION = 'v2019.06.0'
 
 __version__ = os.environ.get('TRAVIS_TAG', '{}-dev'.format(NEXT_VERSION))
