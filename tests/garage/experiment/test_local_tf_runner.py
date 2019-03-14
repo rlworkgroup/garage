3aab2a1025ccf29b98aa72932beda0e506f6754a
Keren Zhu <naeioi@hotmail.com>
2019-03-13 20:41:31 -0700

    Fix CI

--- tests/garage/experiment/test_local_tf_runner.py
+++ tests/garage/experiment/test_local_tf_runner.py
@@ -12,9 +12,6 @@ from garage.tf.samplers import BatchSampler
 
 
 class TestLocalRunner(unittest.TestCase):
-    def tearDown(self):
-        tf.reset_default_graph()
-
     def test_session(self):
         with LocalRunner():
             self.assertIsNotNone(
