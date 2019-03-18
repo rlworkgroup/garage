28606a534782606fcea4c767a549624f0e17a027
Keren Zhu <naeioi@hotmail.com>
2019-03-18 12:29:33 -0700

    Format

--- tests/garage/tf/algos/test_ddpg.py
+++ tests/garage/tf/algos/test_ddpg.py
@@ -51,9 +51,7 @@ class TestDDPG(TfGraphTestCase):
             )
             runner.setup(algo, env)
             last_avg_ret = runner.train(
-                n_epochs=10,
-                n_epoch_cycles=20,
-                batch_size=100)
+                n_epochs=10, n_epoch_cycles=20, batch_size=100)
             assert last_avg_ret > 60
 
             env.close()
