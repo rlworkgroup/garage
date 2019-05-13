ce411977e42dec974343226cf3f7ce78354ed278
Nishanth Hegde <hegde.nishanth@gmail.com>
2019-05-13 16:25:30 -0700

    Fix test

--- garage/tf/policies/deterministic_mlp_policy_with_model.py
+++ garage/tf/policies/deterministic_mlp_policy_with_model.py
@@ -96,7 +96,6 @@ class DeterministicMLPPolicyWithModel(Policy2):
         """Return action sym according to obs_var."""
         with tf.variable_scope(self._variable_scope):
             action = self.model.build(obs_var, name=name)
-            action = tf.reshape(action, self.action_space.shape)
             return action
 
     def get_params(self, trainable=True):
