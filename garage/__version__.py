e2035fd5afc6005ea810f85364e5d1cc5b0df9c6
Ryan Julian <ryanjulian@users.noreply.github.com>
2019-05-15 13:53:41 -0700

    Rewrite automatic versioning (#659)
    The previous automatic versioning script was flawed. It produced the
    correct package version for documentation builds and building PyPI
    distributions, but produced an incorrect version when you run setup.py
    from the downloaded package. Unfortunately, Python environment managers
    (e.g. Pipenv, conda) resolve package version by evaluating setup.py,
    not using the PyPI version.
    
    This PR makes version generation simpler by reading the version string
    from a simple file. Automatic versioning from tags is handled by
    clobbering the version file from within the CI, rather than looking for
    a CI environment variable on every usage.

--- garage/__version__.py
+++
@@ -1,7 +0,0 @@
-"""Automatically resolve the version of this package."""
-import os
-
-# Update this every release to be 'v{next release number}'
-NEXT_VERSION = 'v2019.06.0'
-
-__version__ = os.environ.get('TRAVIS_TAG', '{}-dev'.format(NEXT_VERSION))
