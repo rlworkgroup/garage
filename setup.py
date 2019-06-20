58970f03d6855afce5ca254d887cf1582a5c61e5
Linda Wong <43280382+lywong92@users.noreply.github.com>
2019-06-20 14:01:32 -0700

    Pinned akro and fixed torch mlp module (#733)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 # Required dependencies
 required = [
     # Please keep alphabetized
-    'akro',
+    'akro==0.0.2',
     'boto3',
     'cached_property',
     'click',
