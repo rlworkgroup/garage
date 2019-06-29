9c8f137d850abb97c8c021427a8c8b719e9c2f84
Gitanshu Sardana <gitanshusardana@gmail.com>
2019-06-29 08:42:39 -0700

    Fix nightly tests broken due to switch to pytest (#760)

--- tests/garage/tf/envs/test_base.py
+++ tests/garage/tf/envs/test_base.py
@@ -10,11 +10,11 @@ class TestTfEnv:
     @pytest.mark.parametrize('spec', list(gym.envs.registry.all()))
     def test_all_gym_envs(self, spec):
         env = TfEnv(spec.make())
-        step_env_with_gym_quirks(self, env, spec)
+        step_env_with_gym_quirks(env, spec)
 
     @pytest.mark.nightly
     @pytest.mark.parametrize('spec', list(gym.envs.registry.all()))
     def test_all_gym_envs_pickleable(self, spec):
         env = TfEnv(env_name=spec.id)
         step_env_with_gym_quirks(
-            self, env, spec, n=1, render=True, serialize_env=True)
+            env, spec, n=1, render=True, serialize_env=True)
