a5a8e18277c68269e052652208f78d0081dd02c6
Zequn Yu <zequnyu@usc.edu>
2019-07-02 14:28:08 -0700

    Unify resume via run_experiment API (#87)
    - Unified resume and train usage in experiment_wrapper
    - Updated command line resume and code example

--- examples/resume_training.py
+++ examples/resume_training.py
@@ -1,37 +1,19 @@
 #!/usr/bin/env python3
+"""
+This is an example to resume training programmatically.
+"""
+from garage.experiment import LocalRunner, run_experiment
 
-import argparse
-import os
-import subprocess
 
-from garage.experiment import to_local_command
+def run_task(*_):
+    with LocalRunner() as runner:
+        runner.restore(from_dir='dir/', from_epoch=2)
+        runner.resume()
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--snapshot_dir',
-        type=str,
-        default=None,
-        help='Directory of the pickle file to resume experiment from.')
-    parser.add_argument(
-        '--resume_epoch',
-        type=str,
-        default=None,
-        help='Index of epoch to restore from. '
-        'Can be "first", "last" or a number. '
-        'Not applicable when snapshot_mode="last"')
 
-    args = parser.parse_args()
-    params = dict()
-    params['resume_from_dir'] = args.snapshot_dir
-    if args.resume_epoch is not None:
-        params['resume_epoch'] = args.resume_epoch
-    command = to_local_command(
-        params, script='garage.experiment.experiment_wrapper')
-    print(command)
-    try:
-        subprocess.call(command, shell=True, env=os.environ)
-    except Exception as e:
-        print(e)
-        if isinstance(e, KeyboardInterrupt):
-            raise
+run_experiment(
+    run_task,
+    log_dir='new_dir/',
+    snapshot_mode='last',
+    seed=1,
+)
--- tests/garage/experiment/test_experiment_wrapper.py
+++ tests/garage/experiment/test_experiment_wrapper.py
@@ -6,19 +6,30 @@ from garage.experiment import SnapshotConfig
 from garage.experiment.experiment_wrapper import run_experiment
 
 
-def method_call(snapshot_config, variant_data):
+def method_call(snapshot_config, variant_data, from_dir, from_epoch):
     assert isinstance(snapshot_config, SnapshotConfig)
     assert snapshot_config.snapshot_dir == 'data/'
     assert snapshot_config.snapshot_mode == 'last'
     assert snapshot_config.snapshot_gap == 1
     assert variant_data is None
+    assert from_dir == 'resume_dir/'
+    assert from_epoch == 'first'
 
 
 class TestExperimentWrapper:
     def test_experiment_wrapper_method_call(self):
         data = base64.b64encode(cloudpickle.dumps(method_call)).decode('utf-8')
         args = [
-            '', '--args_data', data, '--use_cloudpickle', 'True', '--log_dir',
-            'data/'
+            '',
+            '--args_data',
+            data,
+            '--use_cloudpickle',
+            'True',
+            '--log_dir',
+            'data/',
+            '--resume_from_dir',
+            'resume_dir/',
+            '--resume_from_epoch',
+            'first',
         ]
         run_experiment(args)
