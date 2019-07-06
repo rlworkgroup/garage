d591d1c24124a72c6328335c4fa1e8a5af40d2c8
Ryan Julian <ryanjulian@users.noreply.github.com>
2019-07-05 20:18:56 -0700

    Remove f-expression from CEM
    This is not Python 3.5 compatible.

--- src/garage/np/algos/cem.py
+++ src/garage/np/algos/cem.py
@@ -68,7 +68,7 @@ class CEM(BatchPolopt):
         # fixed
         self.n_best = int(n_samples * best_frac)
         assert self.n_best >= 1, (
-            f'n_samples is too low. Make sure that n_samples * best_frac >= 1')
+            'n_samples is too low. Make sure that n_samples * best_frac >= 1')
         self.n_params = len(self.cur_mean)
 
     def sample_params(self, epoch):
