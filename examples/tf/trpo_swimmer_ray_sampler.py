eb5d1101464ebd78167668d9d097a893e3e1a5e6
Avnish Narayan <avnishna@usc.edu>
2019-07-09 11:10:01 -0700

    Add example of ray sampler ussage

---
+++ examples/tf/trpo_swimmer_ray_sampler.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+This is an example to train a task with TRPO algorithm.
+
+Uses Ray_batched sampler instead of on_policy vectorized
+sampler.
+Here it runs Swimmer-v2 environment with 40 iterations.
+"""
+import gym
+
+from garage.experiment import LocalRunner, run_experiment
+from garage.np.baselines import LinearFeatureBaseline
+from garage.tf.algos import TRPO
+from garage.tf.envs import TfEnv
+from garage.tf.policies import GaussianMLPPolicyWithModel
+from garage.tf.samplers.ray_batched_sampler import RaySamplerTF
+
+
+def run_task(*_):
+    """."""
+    with LocalRunner() as runner:
+        env = TfEnv(gym.make('Swimmer-v2'))
+
+        policy = GaussianMLPPolicyWithModel(
+            env_spec=env.spec, hidden_sizes=(32, 32))
+
+        baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+        algo = TRPO(
+            env_spec=env.spec,
+            policy=policy,
+            baseline=baseline,
+            max_path_length=500,
+            discount=0.99,
+            max_kl_step=0.01)
+
+        # runner.setup(algo, env)
+        runner.setup(algo, env, sampler_cls=RaySamplerTF)
+        runner.train(n_epochs=40, batch_size=4000)
+
+
+run_experiment(
+    run_task,
+    snapshot_mode='last',
+    seed=1,
+)
