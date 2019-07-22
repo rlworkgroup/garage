98789a1e79290d0c2b84448833e368036142a439
Zequn Yu <zequnyu@usc.edu>
2019-07-22 14:44:24 -0700

    Fix incorrect usage of snapshot_config

--- examples/resume_training.py
+++ examples/resume_training.py
@@ -5,8 +5,8 @@ This is an example to resume training programmatically.
 from garage.experiment import LocalRunner, run_experiment
 
 
-def run_task(*_):
-    with LocalRunner() as runner:
+def run_task(snapshot_config, *_):
+    with LocalRunner(snapshot_config=snapshot_config) as runner:
         runner.restore(from_dir='dir/', from_epoch=2)
         runner.resume()
 
