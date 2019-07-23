b6355ece673401e17136619de5f1c9516d780874
Linda Wong <y.wong92@gmail.com>
2019-07-23 14:59:00 -0700

    Moved OffPolicyRLAlgorithm to np.algos

--- src/garage/np/algos/__init__.py
+++ src/garage/np/algos/__init__.py
@@ -4,5 +4,8 @@ from garage.np.algos.batch_polopt import BatchPolopt
 from garage.np.algos.cem import CEM
 from garage.np.algos.cma_es import CMAES
 from garage.np.algos.nop import NOP
+from garage.np.algos.off_policy_rl_algorithm import OffPolicyRLAlgorithm
 
-__all__ = ['RLAlgorithm', 'BatchPolopt', 'CEM', 'CMAES', 'NOP']
+__all__ = [
+    'RLAlgorithm', 'BatchPolopt', 'CEM', 'CMAES', 'NOP', 'OffPolicyRLAlgorithm'
+]
--- src/garage/tf/algos/ddpg.py
+++ src/garage/tf/algos/ddpg.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import tensorflow.contrib as tc
 
 from garage.misc.overrides import overrides
-from garage.tf.algos.off_policy_rl_algorithm import OffPolicyRLAlgorithm
+from garage.np.algos.off_policy_rl_algorithm import OffPolicyRLAlgorithm
 from garage.tf.misc import tensor_utils
 
 
