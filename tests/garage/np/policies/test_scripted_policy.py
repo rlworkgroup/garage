edb5809648e2fef708b3969a701f722aaf8a9825
Avnish Narayan <avnishna@usc.edu>
2019-07-26 12:53:34 -0700

    Repush

--- tests/garage/np/policies/test_scripted_policy.py
+++ tests/garage/np/policies/test_scripted_policy.py
@@ -5,6 +5,10 @@ class TestScriptedPolicy:
     def setup_method(self):
         self.sp = ScriptedPolicy(scripted_actions=[1], agent_env_infos={0: 1})
 
+    """
+    potentially add more tests down the line
+    """
+
     def test_pass_codecov(self):
         self.sp.get_action(0)
         self.sp.get_actions([0])
