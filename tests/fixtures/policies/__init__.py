e32e29c29fbaf3ea42f77dfd747e088db2ea3771
Avnish Narayan <avnishna@usc.edu>
2019-07-25 22:00:42 -0700

    Fix __init__ fixtures

--- tests/fixtures/policies/__init__.py
+++ tests/fixtures/policies/__init__.py
@@ -1,6 +1,5 @@
 from tests.fixtures.policies.dummy_policy import DummyPolicy
 from tests.fixtures.policies.dummy_recurrent_policy import (
     DummyRecurrentPolicy)
-from tests.fixtures.policies.scripted_policy import ScriptedPolicy
 
-__all__ = ['DummyPolicy', 'DummyRecurrentPolicy', 'ScriptedPolicy']
+__all__ = ['DummyPolicy', 'DummyRecurrentPolicy']
