5c7f39f8b40d36c6a7ab0939f189ec9d920ad755
K.R. Zentner <krzentner@gmail.com>
2019-08-30 15:22:29 -0700

    Add the classic_control gym extra

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ required = [
     'cloudpickle',
     'cma==1.1.06',
     'dowel==0.0.2',
-    'gym[box2d,atari]==0.12.4',
+    'gym[atari,box2d,classic_control]==0.12.4',
     'joblib<0.13,>=0.12',
     'matplotlib',
     'numpy==1.14.5',
