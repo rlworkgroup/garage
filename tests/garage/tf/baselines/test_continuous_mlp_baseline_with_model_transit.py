cf1ce8798b4aa17cc208fdf5cd7907b318e0359b
Avnish Narayan <avnishna@usc.edu>
2019-09-25 17:06:21 -0700

    Add transition test
    will delete later

--- tests/garage/tf/baselines/test_continuous_mlp_baseline_with_model_transit.py
+++ tests/garage/tf/baselines/test_continuous_mlp_baseline_with_model_transit.py
@@ -12,38 +12,38 @@ ContinuousMLPBaselineWithModel.
 from unittest import mock
 
 import numpy as np
-import pytest
 import tensorflow as tf
 
-from garage.tf.envs import TfEnv
 from garage.tf.baselines import ContinuousMLPBaseline
 from garage.tf.baselines import ContinuousMLPBaselineWithModel
+from garage.tf.envs import TfEnv
 from tests.fixtures import TfGraphTestCase
 from tests.fixtures.envs.dummy import DummyBoxEnv
 
 
 class TestContinuousMLPPolicyWithModelTransit(TfGraphTestCase):
+
     def setup_method(self):
         with mock.patch('tensorflow.random.normal') as mock_rand:
             mock_rand.return_value = 0.5
             super().setup_method()
             self.box_env = TfEnv(DummyBoxEnv())
             self.baseline1 = ContinuousMLPBaseline(
-                                    self.box_env.spec,
-                                    regressor_args=dict(hidden_sizes=(32, 32)),
-                                    name="CMB_1")
+                self.box_env.spec,
+                regressor_args=dict(hidden_sizes=(32, 32)),
+                name='CMB_1')
             self.baseline2 = ContinuousMLPBaseline(
-                                    self.box_env.spec,
-                                    regressor_args=dict(hidden_sizes=(64, 64)),
-                                    name="CMB_2")
+                self.box_env.spec,
+                regressor_args=dict(hidden_sizes=(64, 64)),
+                name='CMB_2')
             self.baseline3 = ContinuousMLPBaselineWithModel(
-                                    self.box_env.spec,
-                                    regressor_args=dict(hidden_sizes=(32, 32)),
-                                    name="CMB_WM_1")
+                self.box_env.spec,
+                regressor_args=dict(hidden_sizes=(32, 32)),
+                name='CMB_WM_1')
             self.baseline4 = ContinuousMLPBaselineWithModel(
-                                    self.box_env.spec,
-                                    regressor_args=dict(hidden_sizes=(64, 64)),
-                                    name="CMB_WM_2")
+                self.box_env.spec,
+                regressor_args=dict(hidden_sizes=(64, 64)),
+                name='CMB_WM_2')
 
             self.sess.run(tf.compat.v1.global_variables_initializer())
             for a, b in zip(self.baseline1.get_params_internal(),
@@ -53,8 +53,10 @@ class TestContinuousMLPPolicyWithModelTransit(TfGraphTestCase):
             for a, b in zip(self.baseline4.get_params_internal(),
                             self.baseline2.get_params_internal()):
                 self.sess.run(a.assign(b))
-            assert(np.array_equal(self.baseline4.get_param_values(), self.baseline2.get_param_values()))
-            assert(np.array_equal(self.baseline1.get_param_values(), self.baseline3.get_param_values()))
+            assert (np.array_equal(self.baseline4.get_param_values(),
+                                   self.baseline2.get_param_values()))
+            assert (np.array_equal(self.baseline1.get_param_values(),
+                                   self.baseline3.get_param_values()))
 
     def test_transition(self):
         """ Test to see that CMLPBase with and without model have the same outputs.
@@ -64,12 +66,14 @@ class TestContinuousMLPPolicyWithModelTransit(TfGraphTestCase):
         obs_dim = self.box_env.observation_space.shape
         # with mock.patch('numpy.random.normal') as mock_rand:
         #     mock_rand.return_value = 0.5
-        paths = [{'observations': [np.full(shape=obs_dim, fill_value=1)],
-                    'returns': [1]}, 
-                    {'observations': [np.full(shape=obs_dim, fill_value=2)],
-                    'returns': [2]}
-                ]
-        
+        paths = [{
+            'observations': [np.full(shape=obs_dim, fill_value=1)],
+            'returns': [1]
+        }, {
+            'observations': [np.full(shape=obs_dim, fill_value=2)],
+            'returns': [2]
+        }]
+
         obs = {'observations': [np.full(obs_dim, 1), np.full(obs_dim, 2)]}
         prediction1 = self.baseline1.predict(obs)
         prediction2 = self.baseline2.predict(obs)
@@ -81,7 +85,6 @@ class TestContinuousMLPPolicyWithModelTransit(TfGraphTestCase):
 
         # assert np.allclose(prediction1, prediction3, rtol=1e-6)
         # assert np.allclose(prediction2, prediction4, rtol=1e-6)
-        import ipdb; ipdb.set_trace()
 
         self.baseline1.fit(paths)
         self.baseline2.fit(paths)
@@ -96,7 +99,6 @@ class TestContinuousMLPPolicyWithModelTransit(TfGraphTestCase):
 
         assert np.array_equal(prediction1, prediction3)
         assert np.array_equal(prediction2, prediction4)
-
 
     # def test_get_action_sym(self):
     #     obs_dim = self.box_env.spec.observation_space.flat_dim
