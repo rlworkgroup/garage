221c9f0b5a9754c0ef007aa2d1934afd633e4d52
Avnish Narayan <avnishna@usc.edu>
2019-10-15 14:10:54 -0700

    Add custom tanh normal distribution.

--- src/garage/torch/modules/__init__.py
+++ src/garage/torch/modules/__init__.py
@@ -1,12 +1,13 @@
 """Pytorch modules."""
 
-from garage.torch.modules.gaussian_mlp_module import \
-    GaussianMLPIndependentStdModule, GaussianMLPModule, \
-    GaussianMLPTwoHeadedModule
+from garage.torch.modules.gaussian_mlp_module import (GaussianMLPBaseModule,
+    GaussianMLPIndependentStdModule, GaussianMLPModule, 
+    GaussianMLPTwoHeadedModule)
 from garage.torch.modules.mlp_module import MLPModule
 from garage.torch.modules.multi_headed_mlp_module import MultiHeadedMLPModule
 
 __all__ = [
     'MLPModule', 'MultiHeadedMLPModule', 'GaussianMLPModule',
-    'GaussianMLPIndependentStdModule', 'GaussianMLPTwoHeadedModule'
+    'GaussianMLPIndependentStdModule', 'GaussianMLPTwoHeadedModule',
+    'GaussianMLPBaseModule'
 ]
