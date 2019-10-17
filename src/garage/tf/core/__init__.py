238fb08073db1e9a993c1566f22b12a102a00561
Ryan Julian <rjulian@usc.edu>
2019-10-16 17:19:57 -0700

    Remove TF layers library
    * Remove `garage.tf.core.layers`
    * Remove `garage.tf.core.layers_powered`
    * Remove `garage.tf.core.networks`

--- src/garage/tf/core/__init__.py
+++ src/garage/tf/core/__init__.py
@@ -1,7 +1,6 @@
+"""Core interfaces for TensorFlow-based algorithms."""
 from garage.tf.core.parameterized import Parameterized
-from garage.tf.core.layers_powered import LayersPowered  # noqa: I100
 
 __all__ = [
     'Parameterized',
-    'LayersPowered',
 ]
