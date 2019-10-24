4c4b3d2a010beb84d9d81f12c4c05f580df563b8
Ryan Julian <ryanjulian@users.noreply.github.com>
2019-10-24 15:39:52 -0700

    Enable all pylint rules (#958)
    We will decide as-we-go which ones to disable

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ TF_VERSION = '<1.15,>=1.14.0'
 GYM_VERSION = '==0.12.4'
 
 # Required dependencies
-required = [
+REQUIRED = [
     # Please keep alphabetized
     'akro==0.0.6',
     'cached_property',
@@ -35,30 +35,30 @@ required = [
 ]
 
 # Dependencies for optional features
-extras = {}
+EXTRAS = {}
 
-extras['mujoco'] = [
+EXTRAS['mujoco'] = [
     'mujoco-py<2.1,>=2.0',
     'gym[all]' + GYM_VERSION,
 ]
 
-extras['dm_control'] = [
+EXTRAS['dm_control'] = [
     # dm_control throws an error during install about not being able to
     # find a build dependency (absl-py). Later pip executes the `install`
     # command again and the install succeeds because absl-py has been
     # installed. This is stupid, but harmless.
-    'dm_control @ https://api.github.com/repos/deepmind/dm_control/tarball/7a36377879c57777e5d5b4da5aae2cd2a29b607a',  # noqa: E501
+    'dm_control @ https://api.github.com/repos/deepmind/dm_control/tarball/7a36377879c57777e5d5b4da5aae2cd2a29b607a',  # pylint: disable=line-too-long; # noqa: E501
 ]
 
-extras['all'] = list(set(sum(extras.values(), [])))
+EXTRAS['all'] = list(set(sum(EXTRAS.values(), [])))
 
-# dependencies for using gpu, not included in all
-extras['gpu'] = ['tensorflow-gpu' + TF_VERSION]
+# dependencies for using gpu, not included in 'all'
+EXTRAS['gpu'] = ['tensorflow-gpu' + TF_VERSION]
 
-# Development dependencies (*not* included in "all")
-extras['dev'] = [
+# Development dependencies (*not* included in 'all')
+EXTRAS['dev'] = [
     # Please keep alphabetized
-    'baselines @ https://api.github.com/repos/openai/baselines/tarball/f2729693253c0ef4d4086231d36e0a4307ec1cb3',  # noqa: E501
+    'baselines @ https://api.github.com/repos/openai/baselines/tarball/f2729693253c0ef4d4086231d36e0a4307ec1cb3',  # pylint: disable=line-too-long; # noqa: E501
     'flake8',
     'flake8-docstrings>=1.5.0',
     'flake8-import-order',
@@ -73,7 +73,7 @@ extras['dev'] = [
     'pytest-cov',
     'pytest-xdist',
     'recommonmark',
-    'rlkit @ git+https://github.com/vitchyr/rlkit/@1d469a509b797ca04a39b8734c1816ca7d108fc8',  # noqa: E501
+    'rlkit @ git+https://github.com/vitchyr/rlkit/@1d469a509b797ca04a39b8734c1816ca7d108fc8',  # pylint: disable=line-too-long; # noqa: E501
     'seaborn',
     'sphinx',
     'sphinx_rtd_theme',
@@ -81,26 +81,26 @@ extras['dev'] = [
 ]
 
 with open('README.md') as f:
-    readme = f.read()
+    README = f.read()
 
 # Get the package version dynamically
 with open('VERSION') as v:
-    version = v.read().strip()
+    VERSION = v.read().strip()
 
 setup(
     name='garage',
-    version=version,
+    version=VERSION,
     author='Reinforcement Learning Working Group',
-    description='A framework for reproducible reinforcement learning research',
+    description='A toolkit for reproducible reinforcement learning research',
     url='https://github.com/rlworkgroup/garage',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     scripts=['scripts/garage'],
     python_requires='>=3.5',
-    install_requires=required,
-    extras_require=extras,
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
     license='MIT',
-    long_description=readme,
+    long_description=README,
     long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 4 - Beta',
