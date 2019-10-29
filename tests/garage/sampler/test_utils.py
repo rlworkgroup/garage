c9c80e0cd522c96634dd1a2f63552026c006fbd8
tsan <ahtsans@gmail.com>
2019-10-28 22:01:29 -0700

    Make API consistent

---
+++ tests/garage/sampler/test_utils.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pytest
+
+from garage.envs import GarageEnv
+from garage.sampler import utils
+from tests.fixtures.envs.dummy import DummyBoxEnv
+from tests.fixtures.policies import DummyPolicy
+
+
+class TestRollout:
+
+    def setup_method(self):
+        self.env = GarageEnv(DummyBoxEnv(obs_dim=(4, 4), action_dim=(2, 2)))
+        self.policy = DummyPolicy(self.env.spec)
+
+    def test_max_path_length(self):
+        # pylint: disable=unsubscriptable-object
+        path = utils.rollout(self.env, self.policy, max_path_length=3)
+        assert path['observations'].shape[0] == 3
+        assert path['actions'].shape[0] == 3
+        assert path['rewards'].shape[0] == 3
+        assert len(path['agent_infos']) == 3
+        assert len(path['env_infos']) == 3
+
+    def test_does_not_flatten(self):
+        path = utils.rollout(self.env, self.policy, max_path_length=5)
+        assert path['observations'][0].shape == (4, 4)
+        assert path['actions'][0].shape == (2, 2)
+
+
+class TestTruncatePaths:
+
+    def setup_method(self):
+        self.paths = [
+            dict(
+                observations=np.zeros((100, 1)),
+                actions=np.zeros((100, 1)),
+                rewards=np.zeros(100),
+                env_infos=dict(),
+                agent_infos=dict(lala=np.zeros(100)),
+            ),
+            dict(
+                observations=np.zeros((50, 1)),
+                actions=np.zeros((50, 1)),
+                rewards=np.zeros(50),
+                env_infos=dict(),
+                agent_infos=dict(lala=np.zeros(50)),
+            ),
+        ]
+
+    def test_truncates(self):
+        truncated = utils.truncate_paths(self.paths, 130)
+        assert len(truncated) == 2
+        assert len(truncated[-1]['observations']) == 30
+        assert len(truncated[0]['observations']) == 100
+        # make sure not to change the original one
+        assert len(self.paths) == 2
+        assert len(self.paths[-1]['observations']) == 50
+
+    def test_invalid_path(self):
+        self.paths[0]['invalid'] = None
+        with pytest.raises(ValueError):
+            utils.truncate_paths(self.paths, 3)
