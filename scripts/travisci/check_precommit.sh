721ca584ae48ef20cc37995dd1adf474688cd3af
Ryan Julian <rjulian@usc.edu>
2019-10-30 16:29:44 -0700

    Simpify check_precommit.sh

--- scripts/travisci/check_precommit.sh
+++ scripts/travisci/check_precommit.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 status=0
 
-if [[ "${TRAVIS_PULL_REQUEST}" != "false" && "${TRAVIS}" == "true" ]]; then
-  pre-commit run --source "${TRAVIS_COMMIT_RANGE%...*}" \
-                 --origin "${TRAVIS_COMMIT_RANGE#*...}"
-  status="$((${status} | ${?}))"
+echo "Checking commit range ${TRAVIS_COMMIT_RANGE}"
+SOURCE="${TRAVIS_COMMIT_RANGE%...*}"
+ORIGIN="${TRAVIS_COMMIT_RANGE#*...}"
+pre-commit run --source "${SOURCE}" --origin "${ORIGIN}"
+status="$((${status} | ${?}))"
 
-  # Check commit messages
+# Check commit messages for pull requests only
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
   while read commit; do
+    echo "Checking commit message for ${commit}"
     commit_msg="$(mktemp)"
     git log --format=%B -n 1 "${commit}" > "${commit_msg}"
-    pre-commit run --hook-stage commit-msg --commit-msg-file="${commit_msg}"
+    scripts/check_commit_message "${commit_msg}"
     pass=$?
     status="$((${status} | ${pass}))"
 
@@ -20,31 +23,8 @@ if [[ "${TRAVIS_PULL_REQUEST}" != "false" && "${TRAVIS}" == "true" ]]; then
       cat "${commit_msg}"
     fi
 
-  done < <(git rev-list "${TRAVIS_COMMIT_RANGE}")
-else
-  git remote set-branches --add origin master
-  git fetch
-  merge_base=$(git merge-base origin/master ${TRAVIS_BRANCH})
-  pre-commit run --source ${merge_base} --origin ${TRAVIS_BRANCH}
-  status="$((${status} | ${?}))"
-
-  if [[ "${TRAVIS}" != "true" ]]; then
-  # Check commit messages
-    while read commit; do
-      commit_msg="$(mktemp)"
-      git log --format=%B -n 1 "${commit}" > "${commit_msg}"
-      pre-commit run --hook-stage commit-msg --commit-msg-file="${commit_msg}"
-      pass=$?
-      status="$((${status} | ${pass}))"
-
-      # Print message if it fails
-      if [[ "${pass}" -ne 0 ]]; then
-        echo "Failing commit message:"
-        cat "${commit_msg}"
-      fi
-
-    done < <(git rev-list ^origin/master "${TRAVIS_BRANCH}")
-  fi
+  done < <(git log --cherry-pick --left-only --pretty="%H" \
+                   "${ORIGIN}...${SOURCE}")
 fi
 
 exit "${status}"
