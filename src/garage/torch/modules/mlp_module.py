7e93bcfe4f2f90dab2f9af72bee47460f2b88944
yonghyuc <43084978+yonghyuc@users.noreply.github.com>
2019-11-18 22:30:22 -0800

    Support non-linearity function and module (#928)
    (cherry picked from commit f58b4bd588658f5e02f8dfd8da5fe9ac16dcdfd3)

--- src/garage/torch/modules/mlp_module.py
+++ src/garage/torch/modules/mlp_module.py
@@ -1,17 +1,16 @@
-"""
-MLP Module.
+"""MLP Module."""
 
-A Pytorch module composed only of a multi-layer perceptron (MLP), which maps
-real-valued inputs to real-valued outputs.
-"""
+from torch import nn
+from torch.nn import functional as F
 
-from torch import nn as nn
-from torch.nn import functional as F  # NOQA
+from garage.torch.modules.multi_headed_mlp_module import MultiHeadedMLPModule
 
 
-class MLPModule(nn.Module):
-    """
-    MLP Model.
+class MLPModule(MultiHeadedMLPModule):
+    """MLP Model.
+
+    A Pytorch module composed only of a multi-layer perceptron (MLP), which
+    maps real-valued inputs to real-valued outputs.
 
     Args:
         input_dim (int) : Dimension of the network input.
@@ -19,18 +18,18 @@ class MLPModule(nn.Module):
         hidden_sizes (list[int]): Output dimension of dense layer(s).
             For example, (32, 32) means this MLP consists of two
             hidden layers, each with 32 hidden units.
-        hidden_nonlinearity (callable): Activation function for intermediate
-            dense layer(s). It should return a torch.Tensor. Set it to
-            None to maintain a linear activation.
+        hidden_nonlinearity (callable or torch.nn.Module): Activation function
+            for intermediate dense layer(s). It should return a torch.Tensor.
+            Set it to None to maintain a linear activation.
         hidden_w_init (callable): Initializer function for the weight
             of intermediate dense layer(s). The function should return a
             torch.Tensor.
         hidden_b_init (callable): Initializer function for the bias
             of intermediate dense layer(s). The function should return a
             torch.Tensor.
-        output_nonlinearity (callable): Activation function for output dense
-            layer. It should return a torch.Tensor. Set it to None to
-            maintain a linear activation.
+        output_nonlinearity (callable or torch.nn.Module): Activation function
+            for output dense layer. It should return a torch.Tensor.
+            Set it to None to maintain a linear activation.
         output_w_init (callable): Initializer function for the weight
             of output dense layer(s). The function should return a
             torch.Tensor.
@@ -39,8 +38,6 @@ class MLPModule(nn.Module):
             torch.Tensor.
         layer_normalization (bool): Bool for using layer normalization or not.
 
-    Return:
-        The output torch.Tensor of the MLP
     """
 
     def __init__(self,
@@ -54,41 +51,21 @@ class MLPModule(nn.Module):
                  output_w_init=nn.init.xavier_normal_,
                  output_b_init=nn.init.zeros_,
                  layer_normalization=False):
-        super().__init__()
+        super().__init__(1, input_dim, output_dim, hidden_sizes,
+                         hidden_nonlinearity, hidden_w_init, hidden_b_init,
+                         output_nonlinearity, output_w_init, output_b_init,
+                         layer_normalization)
 
-        self._input_dim = input_dim
-        self._output_dim = output_dim
-        self._hidden_nonlinearity = hidden_nonlinearity
-        self._output_nonlinearity = output_nonlinearity
-        self._layer_normalization = layer_normalization
-        self._layers = nn.ModuleList()
+    # pylint: disable=arguments-differ
+    def forward(self, input_value):
+        """Forward method.
 
-        prev_size = input_dim
+        Args:
+            input_value (torch.Tensor): Input values with (N, *, input_dim)
+                shape.
 
-        for size in hidden_sizes:
-            layer = nn.Linear(prev_size, size)
-            hidden_w_init(layer.weight)
-            hidden_b_init(layer.bias)
-            self._layers.append(layer)
-            prev_size = size
+        Returns:
+            torch.Tensor: Output value
 
-        layer = nn.Linear(prev_size, output_dim)
-        output_w_init(layer.weight)
-        output_b_init(layer.bias)
-        self._layers.append(layer)
-
-    def forward(self, input_val):
-        """Forward method."""
-        x = input_val
-        for layer in self._layers[:-1]:
-            x = layer(x)
-            if self._hidden_nonlinearity is not None:
-                x = self._hidden_nonlinearity(x)
-            if self._layer_normalization:
-                x = nn.LayerNorm(x.shape[1])(x)
-
-        x = self._layers[-1](x)
-        if self._output_nonlinearity is not None:
-            x = self._output_nonlinearity(x)
-
-        return x
+        """
+        return super().forward(input_value)[0]
