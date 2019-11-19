f58b4bd588658f5e02f8dfd8da5fe9ac16dcdfd3
yonghyuc <43084978+yonghyuc@users.noreply.github.com>
2019-11-18 22:30:22 -0800

    Support non-linearity function and module (#928)

--- tests/garage/torch/modules/test_mlp_module.py
+++ tests/garage/torch/modules/test_mlp_module.py
@@ -1,3 +1,5 @@
+"""Test MLPModule."""
+
 import pickle
 
 import numpy as np
@@ -9,6 +11,7 @@ from garage.torch.modules import MLPModule
 
 
 class TestMLPModel:
+    """Test MLPModule."""
     # yapf: disable
     @pytest.mark.parametrize('input_dim, output_dim, hidden_sizes', [
         (5, 1, (1, )),
@@ -19,21 +22,42 @@ class TestMLPModel:
     ])
     # yapf: enable
     def test_output_values(self, input_dim, output_dim, hidden_sizes):
-        input_val = torch.ones([1, 5], dtype=torch.float32)
-        module = MLPModule(
+        """Test output values from MLPModule.
+
+        Args:
+            input_dim (int): Input dimension.
+            output_dim (int): Ouput dimension.
+            hidden_sizes (list[int]): Size of hidden layers.
+
+        """
+        input_val = torch.ones([1, input_dim], dtype=torch.float32)
+        module_with_nonlinear_function_and_module = MLPModule(
             input_dim=input_dim,
             output_dim=output_dim,
-            hidden_nonlinearity=None,
+            hidden_nonlinearity=torch.relu,
             hidden_sizes=hidden_sizes,
             hidden_w_init=nn.init.ones_,
-            output_w_init=nn.init.ones_)
-        output = module(input_val)
+            output_w_init=nn.init.ones_,
+            output_nonlinearity=torch.nn.ReLU)
+
+        module_with_nonlinear_module_instance_and_function = MLPModule(
+            input_dim=input_dim,
+            output_dim=output_dim,
+            hidden_nonlinearity=torch.nn.ReLU(),
+            hidden_sizes=hidden_sizes,
+            hidden_w_init=nn.init.ones_,
+            output_w_init=nn.init.ones_,
+            output_nonlinearity=torch.relu)
+
+        output1 = module_with_nonlinear_function_and_module(input_val)
+        output2 = module_with_nonlinear_module_instance_and_function(input_val)
 
         expected_output = torch.full([1, output_dim],
                                      fill_value=5 * np.prod(hidden_sizes),
                                      dtype=torch.float32)
 
-        assert torch.all(torch.eq(output, expected_output))
+        assert torch.all(torch.eq(expected_output, output1))
+        assert torch.all(torch.eq(expected_output, output2))
 
     # yapf: disable
     @pytest.mark.parametrize('input_dim, output_dim, hidden_sizes', [
@@ -45,14 +69,23 @@ class TestMLPModel:
     ])
     # yapf: enable
     def test_is_pickleable(self, input_dim, output_dim, hidden_sizes):
-        input_val = torch.ones([1, 5], dtype=torch.float32)
-        module = MLPModule(
-            input_dim=input_dim,
-            output_dim=output_dim,
-            hidden_nonlinearity=None,
-            hidden_sizes=hidden_sizes,
-            hidden_w_init=nn.init.ones_,
-            output_w_init=nn.init.ones_)
+        """Check MLPModule is pickeable.
+
+        Args:
+            input_dim (int): Input dimension.
+            output_dim (int): Ouput dimension.
+            hidden_sizes (list[int]): Size of hidden layers.
+
+        """
+        input_val = torch.ones([1, input_dim], dtype=torch.float32)
+        module = MLPModule(input_dim=input_dim,
+                           output_dim=output_dim,
+                           hidden_nonlinearity=torch.relu,
+                           hidden_sizes=hidden_sizes,
+                           hidden_w_init=nn.init.ones_,
+                           output_w_init=nn.init.ones_,
+                           output_nonlinearity=torch.nn.ReLU)
+
         output1 = module(input_val)
 
         h = pickle.dumps(module)
@@ -60,3 +93,49 @@ class TestMLPModel:
         output2 = model_pickled(input_val)
 
         assert np.array_equal(torch.all(torch.eq(output1, output2)), True)
+
+    # yapf: disable
+    @pytest.mark.parametrize('hidden_nonlinear, output_nonlinear', [
+        (torch.nn.ReLU, 'test'),
+        ('test', torch.relu),
+        (object(), torch.tanh),
+        (torch.tanh, object())
+    ])
+    # yapf: enable
+    def test_no_head_invalid_settings(self, hidden_nonlinear,
+                                      output_nonlinear):
+        """Check MLPModule throws exception with invalid non-linear functions.
+
+        Args:
+            hidden_nonlinear (callable or torch.nn.Module): Non-linear
+                functions for hidden layers.
+            output_nonlinear (callable or torch.nn.Module): Non-linear
+                functions for output layer.
+
+        """
+        expected_msg = 'Non linear function .* is not supported'
+        with pytest.raises(ValueError, match=expected_msg):
+            MLPModule(input_dim=3,
+                      output_dim=5,
+                      hidden_sizes=(2, 3),
+                      hidden_nonlinearity=hidden_nonlinear,
+                      output_nonlinearity=output_nonlinear)
+
+    def test_mlp_with_learnable_non_linear_function(self):
+        """Test MLPModule with learnable non-linear functions."""
+        input_dim, output_dim, hidden_sizes = 1, 1, (3, 2)
+
+        input_val = -torch.ones([1, input_dim], dtype=torch.float32)
+        module = MLPModule(input_dim=input_dim,
+                           output_dim=output_dim,
+                           hidden_nonlinearity=torch.nn.PReLU(init=10.),
+                           hidden_sizes=hidden_sizes,
+                           hidden_w_init=nn.init.ones_,
+                           output_w_init=nn.init.ones_,
+                           output_nonlinearity=torch.nn.PReLU(init=1.))
+
+        output = module(input_val)
+        output.sum().backward()
+
+        for tt in module.parameters():
+            assert torch.all(torch.ne(tt.grad, 0))
