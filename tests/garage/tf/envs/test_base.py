cfb562ac764a68a7a6381f413aebac51dba15640
Anson Wong <ahtsans@gmail.com>
2019-11-20 17:56:57 -0800

    Fix garage.envs.GarageEnv (#1029)
    * Fix garage.envs.GarageEnv pickling logic
    * Add unit test to make sure garage.envs.GarageEnv is pickleable

--- tests/garage/tf/envs/test_base.py
+++ tests/garage/tf/envs/test_base.py
@@ -1,3 +1,5 @@
+import pickle
+
 import gym
 import pytest
 
@@ -6,6 +8,12 @@ from tests.helpers import step_env_with_gym_quirks
 
 
 class TestTfEnv:
+
+    def test_is_pickleable(self):
+        env = TfEnv(env_name='CartPole-v1')
+        round_trip = pickle.loads(pickle.dumps(env))
+        assert round_trip.env.spec == env.env.spec
+
     @pytest.mark.nightly
     @pytest.mark.parametrize('spec', list(gym.envs.registry.all()))
     def test_all_gym_envs(self, spec):
@@ -16,5 +24,8 @@ class TestTfEnv:
     @pytest.mark.parametrize('spec', list(gym.envs.registry.all()))
     def test_all_gym_envs_pickleable(self, spec):
         env = TfEnv(env_name=spec.id)
-        step_env_with_gym_quirks(
-            env, spec, n=1, render=True, serialize_env=True)
+        step_env_with_gym_quirks(env,
+                                 spec,
+                                 n=1,
+                                 render=True,
+                                 serialize_env=True)
