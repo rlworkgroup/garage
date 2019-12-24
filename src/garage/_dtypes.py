b0c3cf6a9b001ece816ce8003bd2f17aea7bddba
Avnish Narayan <avnishna@usc.edu>
2019-12-24 10:07:51 -0800

    Needs review

--- src/garage/_dtypes.py
+++ src/garage/_dtypes.py
@@ -167,3 +167,199 @@ class TrajectoryBatch(
         return super().__new__(TrajectoryBatch, env_spec, observations,
                                actions, rewards, terminals, env_infos,
                                agent_infos, lengths)
+
+class SamplesBatch(
+        collections.namedtuple('SamplesBatch', [
+            'env_spec',
+            'observations',
+            'actions',
+            'rewards',
+            'next_observations',
+            'terminals',
+            'env_infos',
+            'agent_infos',
+        ])):
+    # pylint: disable=missing-return-doc, missing-return-type-doc, missing-param-doc, missing-type-doc  # noqa: E501
+    r"""A tuple representing a batch of whole trajectories.
+
+    A :class:`SamplesBatch` represents a batch of samples
+    produced when one or more agents interacts with one or more environments.
+
+    Attributes:
+        env_spec (garage.envs.EnvSpec): Specification for the environment from
+            which this data was sampled.
+        observations (numpy.ndarray): A numpy array of shape
+            :math:`(N \bullet [T], O^*)` containing the (possibly
+            multi-dimensional) observations for all time steps in this batch.
+            These must conform to :obj:`env_spec.observation_space`.
+        actions (numpy.ndarray): A  numpy array of shape
+            :math:`(N \bullet [T], A^*)` containing the (possibly
+            multi-dimensional) actions for all time steps in this batch. These
+            must conform to :obj:`env_spec.action_space`.
+        rewards (numpy.ndarray): A numpy array of shape
+            :math:`(N \bullet [T])` containing the rewards for all time steps
+            in this batch.
+        terminals (numpy.ndarray): A boolean numpy array of shape
+            :math:`(N \bullet [T])` containing the termination signals for all
+            time steps in this batch.
+        env_infos (dict): A dict of numpy arrays arbitrary environment state
+            information. Each value of this dict should be a numpy array of
+            shape :math:`(N \bullet [T])` or :math:`(N \bullet [T], S^*)`.
+        agent_infos (numpy.ndarray): A dict of numpy arrays arbitrary agent
+            state information. Each value of this dict should be a numpy array
+            of shape :math:`(N \bullet [T])` or :math:`(N \bullet [T], S^*)`.
+            For example, this may contain the hidden states from an RNN policy.
+
+    Raises:
+        ValueError: If any of the above attributes do not conform to their
+            prescribed types and shapes.
+
+    """
+    __slots__ = ()
+
+    def __new__(cls, env_spec, observations, actions, rewards, next_observations,
+                 terminals, env_infos, agent_infos):  # noqa: D102
+        # pylint: disable=too-many-branches
+
+        first_observation = observations[0]
+        first_next_observation =  next_observation[0]
+        first_action = actions[0]
+
+        # observations
+        if not env_spec.observation_space.contains(first_observation):
+            raise ValueError(
+                'observations must conform to observation_space {}, but got '
+                'data with shape {} instead.'.format(
+                    env_spec.observation_space, first_observation))
+
+        if not env_spec.observation_space.contains(first_next_observation):
+            raise ValueError(
+                'next_observations must conform to observation_space {}, but got '
+                'data with shape {} instead.'.format(
+                    env_spec.observation_space, first_next_observation))
+
+        # actions
+        # TODO: Shape check can be removed for gym>=0.15.4 # pylint: disable=fixme # noqa: E501
+        if not (env_spec.action_space.contains(first_action)
+                and first_action.shape == env_spec.action_space.shape):
+            raise ValueError(
+                'actions must conform to action_space {}, but got data with '
+                'shape {} instead.'.format(env_spec.action_space,
+                                           first_action))
+
+        if terminals.dtype != np.bool:
+            raise ValueError(
+                'Terminals tensor must be dtype np.bool, but got tensor '
+                'of dtype {} instead.'.format(terminals.dtype))
+
+        # env_infos
+        for key, val in env_infos.items():
+            if not isinstance(val, np.ndarray):
+                raise ValueError(
+                    'Each entry in env_infos must be a numpy array, but got '
+                    'key {} with value type {} instead.'
+                    'instead'.format(key, type(val)))
+
+        # agent_infos
+        for key, val in agent_infos.items():
+            if not isinstance(val, np.ndarray):
+                raise ValueError(
+                    'Each entry in agent_infos must be a numpy array, but got '
+                    'key {} with value type {} instead.'
+                    'instead'.format(key, type(val)))
+
+        return super().__new__(SamplesBatch, env_spec, observations,
+                               actions, rewards, next_observations, terminals,
+                               env_infos, agent_infos)
+
+
+class Sample(
+        collections.namedtuple('Sample', [
+            'env_spec',
+            'observation',
+            'action',
+            'reward',
+            'next_observation',
+            'terminal',
+            'env_info',
+            'agent_info',
+        ])):
+    # pylint: disable=missing-return-doc, missing-return-type-doc, missing-param-doc, missing-type-doc  # noqa: E501
+    r"""A tuple representing a batch of whole trajectories.
+
+    A :class:`Sample` represents single sample sampled from the environment.
+
+    Attributes:
+        env_spec (garage.envs.EnvSpec): Specification for the environment from
+            which this data was sampled.
+        observation (float): Observation at the current time step in the
+            environment.
+        action (float): Action sampled from the policy given the observation
+            at the current time step.
+        reward (float): The reward for taking the action at the current state.
+        terminals (boolean): A boolean representing the termination signal for
+            the current time step.
+        env_info (dict): A dict containing the env_info returned from the
+            environment. For example, this may contain whether the termination
+            of the environment is due to the time step limit expiration or a
+            terminal state.
+        agent_info (dict): A dict of containing the agent_info returned by the
+            agent/policy. For example, this may contain the hidden states from 
+            an RNN policy.
+
+
+    Raises:
+        ValueError: If any of the above attributes do not conform to their
+            prescribed types and shapes.
+
+    """
+    __slots__ = ()
+
+    def __new__(cls, env_spec, observation, action, reward, next_observation,
+                terminal, env_info, agent_info):  # noqa: D102
+        # pylint: disable=too-many-branches
+
+        if type(observation) != float:
+            raise ValueError(
+                'observation must be type {}, but got type {} '
+                'instead.'.format(float, type(observation)))
+        # observation
+        if not env_spec.observation_space.contains(observation):
+            raise ValueError(
+                'observation must conform to observation_space {}, but got '
+                'data with shape {} instead.'.format(
+                    env_spec.observation_space, observation))
+
+        if type(next_observation) != float:
+            raise ValueError(
+                'next_observation must be type {}, but got type {} '
+                'instead.'.format(float, type(next_observation)))
+        # next_observation
+        if not env_spec.observation_space.contains(next_observation):
+            raise ValueError(
+                'next_observation must conform to observation_space {}, but got '
+                'data with shape {} instead.'.format(
+                    env_spec.observation_space, next_observation))
+
+        # action
+        # TODO: Shape check can be removed for gym>=0.15.4 # pylint: disable=fixme # noqa: E501
+        if not (env_spec.action_space.contains(action)
+                and action.shape == env_spec.action_space.shape):
+            raise ValueError(
+                'action must conform to action_space {}, but got data with '
+                'shape {} instead.'.format(env_spec.action_space,
+                                           action))
+
+        # rewards
+        if type(reward) != float:
+            raise ValueError(
+                'Rewards must be type {}, but got type {} '
+                'instead.'.format(float, type(reward)))
+
+        if type(terminal) != bool:
+            raise ValueError(
+                'Terminal must be dtype bool, but  got dtype {} instead.'.format(type(terminal)))
+
+        return super().__new__(Sample, env_spec, observation,
+                               action, reward, terminal, next_observation,
+                               env_info, agent_info)
