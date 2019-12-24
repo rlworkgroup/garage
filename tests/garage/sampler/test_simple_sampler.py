eb094b7c0d88d93d5b307acaff2c71fa2ba818c5
Avnish Narayan <avnishna@usc.edu>
2019-12-24 08:56:48 -0800

    Tests for simple sampler

---
+++ tests/garage/sampler/test_simple_sampler.py
@@ -0,0 +1,66 @@
+from unittest.mock import Mock
+
+import numpy as np
+
+from garage.envs.grid_world_env import GridWorldEnv
+from garage.np.policies import ScriptedPolicy
+from garage.sampler import SimpleSampler
+from garage.tf.envs import TfEnv
+
+
+class TestSampler:
+    """
+    Uses mock policy for 4x4 gridworldenv
+    '4x4': [
+        'SFFF',
+        'FHFH',
+        'FFFH',
+        'HFFG'
+    ]
+    0: left
+    1: down
+    2: right
+    3: up
+    -1: no move
+    'S' : starting point
+    'F' or '.': free space
+    'W' or 'x': wall
+    'H' or 'o': hole (terminates episode)
+    'G' : goal
+    [2,2,1,0,3,1,1,1,2,2,1,1,1,2,2,1]
+    """
+
+    def setup_method(self):
+
+        self.env = TfEnv(GridWorldEnv(desc='4x4'))
+        self.policy = ScriptedPolicy(
+            scripted_actions=[2, 2, 1, 0, 3, 1, 1, 1, 2, 2, 1, 1, 1, 2, 2, 1])
+        self.algo = Mock(env_spec=self.env.spec,
+                         policy=self.policy,
+                         max_path_length=16)
+
+    def teardown_method(self):
+        self.env.close()
+
+    def test_ray_batch_sampler(self):
+        sampler1 = SimpleSampler(self.)
+        sampler1.start_worker()
+        sampler2 = OnPolicyVectorizedSampler(self.algo, self.env)
+        sampler2.start_worker()
+        trajs1 = sampler1.obtain_samples(0, 16)
+        trajs2 = sampler2.obtain_samples(0, 1)
+        assert (trajs1[0]['observations'].shape == np.array(
+            trajs2[0]['observations']).shape == (6, ))
+        traj2_action_shape = np.array(trajs2[0]['actions']).shape
+        assert (trajs1[0]['actions'].shape == traj2_action_shape == (6, ))
+        assert (sum(trajs1[0]['rewards']) == sum(trajs2[0]['rewards']) == 1)
+
+        true_obs = np.array([0, 1, 2, 6, 10, 14])
+        true_actions = np.array([2, 2, 1, 1, 1, 2])
+        true_rewards = np.array([0, 0, 0, 0, 0, 1])
+        for trajectory in trajs1:
+            assert (np.array_equal(trajectory['observations'], true_obs))
+            assert (np.array_equal(trajectory['actions'], true_actions))
+            assert (np.array_equal(trajectory['rewards'], true_rewards))
+        sampler1.shutdown_worker()
+        sampler2.shutdown_worker()
