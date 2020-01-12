7d8f3264d6d801ee7d05c7674d667909e628f7c5
Anson Wong <ahtsans@gmail.com>
2020-01-11 22:02:38 -0800

    Address comments and add test

--- src/garage/sampler/rl2_sampler.py
+++ src/garage/sampler/rl2_sampler.py
@@ -1,6 +1,5 @@
-"""BatchSampler which uses VecEnvExecutor to run multiple environments."""
+"""RL2 Sampler which uses VecEnvExecutor to run multiple environments."""
 from collections import OrderedDict
-import itertools
 import pickle
 import time
 
@@ -10,60 +9,80 @@ import numpy as np
 from garage.experiment import deterministic
 from garage.misc import tensor_utils
 from garage.misc.prog_bar_counter import ProgBarCounter
-from garage.sampler.batch_sampler import BatchSampler
 from garage.sampler.stateful_pool import singleton_pool
 from garage.sampler.utils import truncate_paths
 from garage.sampler.vec_env_executor import VecEnvExecutor
 
 
-class RL2Sampler(BatchSampler):
-    """BatchSampler which uses VecEnvExecutor to run multiple environments.
+class RL2Sampler:
+    """Sampler which uses VecEnvExecutor to run multiple environments.
 
     This sampler is for RL^2. See https://arxiv.org/pdf/1611.02779.pdf.
 
     Args:
         algo (garage.np.algos.RLAlgorithm): An algorithm instance.
-        env (garage.envs.GarageEnv): An environement instance.
-        meta_batch_size (int): Number of meta batch for sampling.
-        episode_per_task (int): Number of episode for each task.
-        n_envs (int): Number of environment instances to setup.
-            This parameter has effect on sampling performance.
+        env (List[garage.envs.GarageEnv]): Environements to sample from.
+        meta_batch_size (int): Number of meta batch for sampling. If it is
+            larger than n_envs, it must be a multiple of n_envs so it can be
+            evenly distributed among environments.
+        n_envs (int): Number of environment instances for sampling. It it is
+            larger than meta_batch_size, it must be a multiple of
+            meta_batch_size so batch can be evenly distributed among
+            environments.
 
     """
 
-    def __init__(self,
-                 algo,
-                 env,
-                 meta_batch_size,
-                 episode_per_task,
-                 n_envs=None):
+    def __init__(self, algo, env, meta_batch_size, n_envs=None):
         if n_envs is None:
             n_envs = singleton_pool.n_parallel * 4
-        super().__init__(algo, env)
-        self._n_envs = n_envs
+        self.algo = algo
+        self.envs = env
 
+        self._n_envs = n_envs
         self._meta_batch_size = meta_batch_size
-        self._episode_per_task = episode_per_task
         self._vec_env = None
-        self._env_spec = self.env.spec
+        self._envs_per_worker = None
+        self._vec_envs_indices = None
 
     def start_worker(self):
-        """Start workers."""
-        n_envs = self._n_envs
-        assert n_envs >= self._meta_batch_size, (
-            'Number of vectorized environments '
-            'should be at least meta_batch_size.')
-        assert n_envs % self._meta_batch_size == 0, (
-            'Number of vectorized '
-            'environments should be a multiple of meta_batch_size.')
-        envs_per_worker = n_envs // self._meta_batch_size
+        """Start workers.
 
-        tasks = self.env.sample_tasks(self._meta_batch_size)
+        Raises:
+            ValueError: If meta_batch_size > n_envs and meta_batch_size is not
+                a multiple of n_envs, or if n_envs > meta_batch_size and n_envs
+                is not a multiple of meta_batch_size.
+
+        """
+        if self._meta_batch_size > self._n_envs:
+            if self._meta_batch_size % self._n_envs != 0:
+                raise ValueError(
+                    'meta_batch_size must be a multiple of n_envs')
+            self._envs_per_worker = 1
+            self._vec_envs_indices = np.split(np.arange(self._meta_batch_size),
+                                              self._n_envs)
+        if self._n_envs >= self._meta_batch_size:
+            if self._n_envs % self._meta_batch_size != 0:
+                raise ValueError(
+                    'n_envs must be a multiple of meta_batch_size')
+            self._envs_per_worker = self._n_envs // self._meta_batch_size
+            self._vec_envs_indices = [np.arange(self._meta_batch_size)]
+
+    def shutdown_worker(self):
+        """Shutdown workers."""
+        self._vec_env.close()
+
+    def setup_worker(self, env_indices):
+        """Setup workers.
+
+        Args:
+            env_indices (List[Int]): Indices of environments to be assigned
+                to workers for sampling.
+
+        """
         vec_envs = []
-        for task in tasks:
-            vec_env = pickle.loads(pickle.dumps(self.env))
-            vec_env.set_task(task)
-            vec_envs.extend([vec_env] * envs_per_worker)
+        for env_ind in env_indices:
+            vec_env = pickle.loads(pickle.dumps(self.envs[env_ind]))
+            vec_envs.extend([vec_env for env in range(self._envs_per_worker)])
         seed0 = deterministic.get_seed()
         if seed0 is not None:
             for (i, e) in enumerate(vec_envs):
@@ -72,13 +91,12 @@ class RL2Sampler(BatchSampler):
         self._vec_env = VecEnvExecutor(
             envs=vec_envs, max_path_length=self.algo.max_path_length)
 
-    def shutdown_worker(self):
-        """Shutdown workers."""
-        self._vec_env.close()
-
     # pylint: disable=too-many-statements
     def obtain_samples(self, itr, batch_size=None, whole_paths=True):
         """Sample the policy for new trajectories.
+
+        If batch size is not specified, episode per task by default is 1 so
+        batch size will be meta_batch_size * max_path_length.
 
         Args:
             itr (int): Iteration number.
@@ -97,6 +115,7 @@ class RL2Sampler(BatchSampler):
                 * observations: numpy.ndarray with shape :math:`[N, S^*]`
                 * actions: numpy.ndarray with shape :math:`[N, S^*]`
                 * rewards: numpy.ndarray with shape :math:`[N, S^*]`
+                * dones: numpy.ndarray with shape :math:`[N, S^*]`
                 * env_infos: A dictionary with each key representing one
                   environment info, value being a numpy.ndarray with shape
                   :math:`[N, S^*]`. One example is "ale.lives" for atari
@@ -111,80 +130,90 @@ class RL2Sampler(BatchSampler):
         logger.log('Obtaining samples for iteration %d...' % itr)
 
         if batch_size is None:
-            batch_size = (self._episode_per_task * self.algo.max_path_length *
-                          self._n_envs)
+            batch_size = self.algo.max_path_length * self._meta_batch_size
 
         paths = OrderedDict()
-        for i in range(self._n_envs):
+        for i in range(self._meta_batch_size):
             paths[i] = []
 
         n_samples = 0
-        obses = self._vec_env.reset()
-        dones = np.asarray([True] * self._vec_env.num_envs)
-        running_paths = [None] * self._vec_env.num_envs
 
-        pbar = ProgBarCounter(batch_size)
-        policy_time = 0
-        env_time = 0
-        process_time = 0
+        # Start main loop
+        batch_size_per_loop = batch_size // len(self._vec_envs_indices)
+        for vec_envs_indices in self._vec_envs_indices:
+            self.setup_worker(vec_envs_indices)
 
-        policy = self.algo.policy
-        # Only reset policies at the beginning of a meta batch
-        policy.reset(dones)
+            obses = self._vec_env.reset()
+            dones = np.asarray([True] * self._vec_env.num_envs)
+            running_paths = [None] * self._vec_env.num_envs
 
-        while n_samples < batch_size:
-            t = time.time()
+            pbar = ProgBarCounter(batch_size)
+            policy_time = 0
+            env_time = 0
+            process_time = 0
 
-            actions, agent_infos = policy.get_actions(obses)
+            policy = self.algo.policy
+            # Only reset policies at the beginning of a meta batch
+            policy.reset(dones)
 
-            policy_time += time.time() - t
-            t = time.time()
-            next_obses, rewards, dones, env_infos = self._vec_env.step(actions)
-            env_time += time.time() - t
-            t = time.time()
+            while n_samples < batch_size_per_loop:
+                t = time.time()
 
-            agent_infos = tensor_utils.split_tensor_dict_list(agent_infos)
-            env_infos = tensor_utils.split_tensor_dict_list(env_infos)
-            if env_infos is None:
-                env_infos = [dict() for _ in range(self._vec_env.num_envs)]
-            if agent_infos is None:
-                agent_infos = [dict() for _ in range(self._vec_env.num_envs)]
-            for idx, observation, action, reward, env_info, agent_info, done in zip(  # noqa: E501
-                    itertools.count(), obses, actions, rewards, env_infos,
-                    agent_infos, dones):
-                if running_paths[idx] is None:
-                    running_paths[idx] = dict(
-                        observations=[],
-                        actions=[],
-                        rewards=[],
-                        dones=[],
-                        env_infos=[],
-                        agent_infos=[],
-                    )
-                running_paths[idx]['observations'].append(observation)
-                running_paths[idx]['actions'].append(action)
-                running_paths[idx]['rewards'].append(reward)
-                running_paths[idx]['dones'].append(done)
-                running_paths[idx]['env_infos'].append(env_info)
-                running_paths[idx]['agent_infos'].append(agent_info)
-                if done:
-                    obs = np.asarray(running_paths[idx]['observations'])
-                    actions = np.asarray(running_paths[idx]['actions'])
-                    paths[idx].append(
-                        dict(observations=obs,
-                             actions=actions,
-                             rewards=np.asarray(running_paths[idx]['rewards']),
-                             dones=np.asarray(running_paths[idx]['dones']),
-                             env_infos=tensor_utils.stack_tensor_dict_list(
-                                 running_paths[idx]['env_infos']),
-                             agent_infos=tensor_utils.stack_tensor_dict_list(
-                                 running_paths[idx]['agent_infos'])))
-                    n_samples += len(running_paths[idx]['rewards'])
-                    running_paths[idx] = None
+                actions, agent_infos = policy.get_actions(obses)
 
-            process_time += time.time() - t
-            pbar.inc(len(obses))
-            obses = next_obses
+                policy_time += time.time() - t
+                t = time.time()
+                next_obses, rewards, dones, env_infos = self._vec_env.step(
+                    actions)
+                env_time += time.time() - t
+                t = time.time()
+
+                agent_infos = tensor_utils.split_tensor_dict_list(agent_infos)
+                env_infos = tensor_utils.split_tensor_dict_list(env_infos)
+                if env_infos is None:
+                    env_infos = [dict() for _ in range(self._vec_env.num_envs)]
+                if agent_infos is None:
+                    agent_infos = [
+                        dict() for _ in range(self._vec_env.num_envs)
+                    ]
+                for idx, observation, action, reward, env_info, agent_info, done in zip(  # noqa: E501
+                        vec_envs_indices, obses, actions, rewards, env_infos,
+                        agent_infos, dones):
+                    if running_paths[idx] is None:
+                        running_paths[idx] = dict(
+                            observations=[],
+                            actions=[],
+                            rewards=[],
+                            dones=[],
+                            env_infos=[],
+                            agent_infos=[],
+                        )
+                    running_paths[idx]['observations'].append(observation)
+                    running_paths[idx]['actions'].append(action)
+                    running_paths[idx]['rewards'].append(reward)
+                    running_paths[idx]['dones'].append(done)
+                    running_paths[idx]['env_infos'].append(env_info)
+                    running_paths[idx]['agent_infos'].append(agent_info)
+                    if done:
+                        obs = np.asarray(running_paths[idx]['observations'])
+                        actions = np.asarray(running_paths[idx]['actions'])
+                        paths[idx].append(
+                            dict(observations=obs,
+                                 actions=actions,
+                                 rewards=np.asarray(
+                                     running_paths[idx]['rewards']),
+                                 dones=np.asarray(running_paths[idx]['dones']),
+                                 env_infos=tensor_utils.stack_tensor_dict_list(
+                                     running_paths[idx]['env_infos']),
+                                 agent_infos=tensor_utils.
+                                 stack_tensor_dict_list(
+                                     running_paths[idx]['agent_infos'])))
+                        n_samples += len(running_paths[idx]['rewards'])
+                        running_paths[idx] = None
+
+                process_time += time.time() - t
+                pbar.inc(len(obses))
+                obses = next_obses
 
         pbar.stop()
 
