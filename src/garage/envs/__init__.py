2515e56330bb37fa3249dc091dd77f0bf1d9c131
Keren Zhu <naeioi@hotmail.com>
2020-01-16 02:06:16 -0800

    Add MAML
    Co-authored-by: Utkarsh Patel <utkarshjp7@gmail.com>

--- src/garage/envs/__init__.py
+++ src/garage/envs/__init__.py
@@ -4,14 +4,12 @@ from garage.envs.base import GarageEnv
 from garage.envs.base import Step
 from garage.envs.env_spec import EnvSpec
 from garage.envs.grid_world_env import GridWorldEnv
+from garage.envs.half_cheetah_dir_env import HalfCheetahDirEnv
+from garage.envs.half_cheetah_vel_env import HalfCheetahVelEnv
 from garage.envs.normalized_env import normalize
 from garage.envs.point_env import PointEnv
 
 __all__ = [
-    'GarageEnv',
-    'Step',
-    'EnvSpec',
-    'GridWorldEnv',
-    'normalize',
-    'PointEnv',
+    'GarageEnv', 'Step', 'EnvSpec', 'GridWorldEnv', 'HalfCheetahDirEnv',
+    'HalfCheetahVelEnv', 'normalize', 'PointEnv'
 ]
--- src/garage/torch/algos/__init__.py
+++ src/garage/torch/algos/__init__.py
@@ -5,8 +5,10 @@ from garage.torch.algos._utils import filter_valids  # noqa: F401
 from garage.torch.algos._utils import make_optimizer  # noqa: F401
 from garage.torch.algos._utils import pad_to_last  # noqa: F401
 from garage.torch.algos.ddpg import DDPG
+# VPG has to been import first because it is depended by MAML, PPO and TRPO.
 from garage.torch.algos.vpg import VPG
-from garage.torch.algos.ppo import PPO  # noqa: I100
+from garage.torch.algos.maml import MAML  # noqa: I100
+from garage.torch.algos.ppo import PPO
 from garage.torch.algos.trpo import TRPO
 
-__all__ = ['DDPG', 'VPG', 'PPO', 'TRPO']
+__all__ = ['DDPG', 'VPG', 'PPO', 'TRPO', 'MAML']
