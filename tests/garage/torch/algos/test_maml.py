f1cd7569be0d4a32af5ea497d42fd3966af26765
Keren Zhu <naeioi@hotmail.com>
2020-03-23 16:11:18 -0700

    Test without fixture

--- tests/garage/torch/algos/test_maml.py
+++ tests/garage/torch/algos/test_maml.py
@@ -12,30 +12,6 @@ from garage.torch.algos import MAMLPPO
 from garage.torch.policies import GaussianMLPPolicy
 
 
-@pytest.fixture
-def set_params():  # noqa: D202
-    """Set parameters of a module."""
-
-    def _set_params(v, m):
-        if isinstance(m, torch.nn.Linear):
-            m.weight.data.fill_(v)
-            m.bias.data.fill_(v)
-
-    return _set_params
-
-
-@pytest.fixture
-def test_params():  # noqa: D202
-    """Test parameters equal to a value."""
-
-    def _test_params(v, m):
-        if isinstance(m, torch.nn.Linear):
-            assert torch.all(torch.eq(m.weight.data, v))
-            assert torch.all(torch.eq(m.bias.data, v))
-
-    return _test_params
-
-
 class TestMAML:
     """Test class for MAML."""
 
@@ -64,29 +40,43 @@ class TestMAML:
         """Teardown method which is called after every test."""
         self.env.close()
 
-    def test_get_exploration_policy(self, set_params, test_params):
+    @staticmethod
+    def _set_params(v, m):
+        """Set the parameters of a module to a value."""
+        if isinstance(m, torch.nn.Linear):
+            m.weight.data.fill_(v)
+            m.bias.data.fill_(v)
+
+    @staticmethod
+    def _test_params(v, m):
+        """Test if all parameters of a module equal to a value."""
+        if isinstance(m, torch.nn.Linear):
+            assert torch.all(torch.eq(m.weight.data, v))
+            assert torch.all(torch.eq(m.bias.data, v))
+
+    def test_get_exploration_policy(self):
         """Test if an independent copy of policy is returned."""
-        self.policy.apply(partial(set_params, 0.1))
+        self.policy.apply(partial(self._set_params, 0.1))
         adapt_policy = self.algo.get_exploration_policy()
-        adapt_policy.apply(partial(set_params, 0.2))
+        adapt_policy.apply(partial(self._set_params, 0.2))
 
         # Old policy should remain untouched
-        self.policy.apply(partial(test_params, 0.1))
-        adapt_policy.apply(partial(test_params, 0.2))
+        self.policy.apply(partial(self._test_params, 0.1))
+        adapt_policy.apply(partial(self._test_params, 0.2))
 
-    def test_adapt_policy(self, set_params, test_params):
+    def test_adapt_policy(self):
         """Test if policy can adapt to samples."""
         worker = WorkerFactory(seed=100, max_path_length=100)
         sampler = LocalSampler.from_worker_factory(worker, self.policy,
                                                    self.env)
 
-        self.policy.apply(partial(set_params, 0.1))
+        self.policy.apply(partial(self._set_params, 0.1))
         adapt_policy = self.algo.get_exploration_policy()
         trajs = sampler.obtain_samples(0, 100, adapt_policy)
         self.algo.adapt_policy(adapt_policy, trajs)
 
         # Old policy should remain untouched
-        self.policy.apply(partial(test_params, 0.1))
+        self.policy.apply(partial(self._test_params, 0.1))
 
         # Adapted policy should not be identical to old policy
         for v1, v2 in zip(adapt_policy.parameters(), self.policy.parameters()):
