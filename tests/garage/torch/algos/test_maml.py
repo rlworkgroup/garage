8559497288c5301c0c2bee3185351c5f9de8cd83
Keren Zhu <naeioi@hotmail.com>
2020-03-25 15:19:56 -0700

    Fix import

--- tests/garage/torch/algos/test_maml.py
+++ tests/garage/torch/algos/test_maml.py
@@ -4,8 +4,9 @@ from functools import partial
 import pytest
 import torch
 
-from garage.envs import HalfCheetahDirEnv, normalize
+from garage.envs import normalize
 from garage.envs.base import GarageEnv
+from garage.envs.mujoco import HalfCheetahDirEnv
 from garage.np.baselines import LinearFeatureBaseline
 from garage.sampler import LocalSampler, WorkerFactory
 from garage.torch.algos import MAMLPPO
