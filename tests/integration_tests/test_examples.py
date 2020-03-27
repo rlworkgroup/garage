3bcdcae9b0f5f71cb6ef99926929bfd0890d59bd
tsan <ahtsans@gmail.com>
2020-03-27 12:20:21 -0700

    Fix formatting

--- tests/integration_tests/test_examples.py
+++ tests/integration_tests/test_examples.py
@@ -15,18 +15,22 @@ NON_ALGO_EXAMPLES = [
     EXAMPLES_ROOT_DIR / 'step_dm_control_env.py',
 ]
 
+# yapf: disable
 LONG_RUNNING_EXAMPLES = [
-    EXAMPLES_ROOT_DIR / 'tf/ppo_memorize_digits.py', EXAMPLES_ROOT_DIR /
-    'tf/dqn_pong.py', EXAMPLES_ROOT_DIR / 'tf/trpo_cubecrash.py',
+    EXAMPLES_ROOT_DIR / 'tf/ppo_memorize_digits.py',
+    EXAMPLES_ROOT_DIR / 'tf/dqn_pong.py',
+    EXAMPLES_ROOT_DIR / 'tf/trpo_cubecrash.py',
     EXAMPLES_ROOT_DIR / 'tf/trpo_swimmer_ray_sampler.py',
     EXAMPLES_ROOT_DIR / 'torch/maml_ppo_half_cheetah_dir.py',
     EXAMPLES_ROOT_DIR / 'torch/maml_trpo_half_cheetah_dir.py',
     EXAMPLES_ROOT_DIR / 'torch/maml_vpg_half_cheetah_dir.py',
-    EXAMPLES_ROOT_DIR / 'torch/maml_trpo_ml10.py', EXAMPLES_ROOT_DIR /
-    'torch/pearl_ml1_push.py', EXAMPLES_ROOT_DIR / 'tf/rl2_ppo_halfcheetah.py',
+    EXAMPLES_ROOT_DIR / 'torch/maml_trpo_ml10.py',
+    EXAMPLES_ROOT_DIR / 'torch/pearl_ml1_push.py',
+    EXAMPLES_ROOT_DIR / 'tf/rl2_ppo_halfcheetah.py',
     EXAMPLES_ROOT_DIR / 'tf/rl2_trpo_halfcheetah.py',
     EXAMPLES_ROOT_DIR / 'tf/rl2_ppo_halfcheetah_meta_test.py'
 ]
+# yapf: enable
 
 
 def enumerate_algo_examples():
