335808acfc2db5bd86991fab603458dcb41799c3
Keren Zhu <naeioi@hotmail.com>
2020-04-15 15:47:05 -0700

    Add coverage

--- tests/garage/envs/test_point_env.py
+++ tests/garage/envs/test_point_env.py
@@ -1,10 +1,13 @@
 import pickle
 
+import numpy as np
+
 from garage.envs.point_env import PointEnv
 from tests.helpers import step_env
 
 
 class TestPointEnv:
+
     def test_pickleable(self):
         env = PointEnv()
         round_trip = pickle.loads(pickle.dumps(env))
@@ -21,3 +24,39 @@ class TestPointEnv:
         env.step(a)
         assert a.all() == a_copy.all()
         env.close()
+
+    def test_observation_space(self):
+        env = PointEnv()
+        obs_space = env.observation_space
+        a = env.action_space.sample()
+        obs, _, _, _ = env.step(a)
+        assert obs_space.contains(obs)
+
+    def test_reset(self):
+        env = PointEnv()
+
+        assert (env._point == np.array([0, 0])).all()
+
+        a = env.action_space.sample()
+        _ = env.step(a)
+        env.reset()
+
+        assert (env._point == np.array([0, 0])).all()
+
+    def test_task(self):
+        env = PointEnv()
+        tasks = env.sample_tasks(5)
+        assert len(tasks) == 5
+
+        for task in tasks:
+            env.set_task(task)
+            assert (env._goal == task['goal']).all()
+
+    def test_done(self):
+        env = PointEnv()
+        for _ in range(1000):
+            _, _, done, _ = env.step(env._goal)
+            if done:
+                break
+        else:
+            assert False, 'Should report done'
