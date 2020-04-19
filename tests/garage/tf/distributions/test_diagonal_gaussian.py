8755b10e411cb5eac19fc9a6cf1a49efac4c472f
Keren Zhu <naeioi@hotmail.com>
2020-04-19 02:42:08 -0700

    Improve coverage

--- tests/garage/tf/distributions/test_diagonal_gaussian.py
+++ tests/garage/tf/distributions/test_diagonal_gaussian.py
@@ -22,3 +22,12 @@ def test_sample():
     samples = [gaussian.sample(dist) for _ in range(10000)]
     assert np.isclose(np.mean(samples), 1, atol=0.1)
     assert np.isclose(np.var(samples), 1, atol=0.1)
+
+
+def test_sample_sym():
+    gaussian = DiagonalGaussian(dim=2)
+    dist = dict(mean=np.array([1., 1.], dtype=np.float32),
+                log_std=np.array([0., 0.], dtype=np.float32))
+    samples = [gaussian.sample_sym(dist).numpy() for _ in range(10000)]
+    assert np.isclose(np.mean(samples), 1, atol=0.1)
+    assert np.isclose(np.var(samples), 1, atol=0.1)
