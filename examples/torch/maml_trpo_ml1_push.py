d92ae774fed09ee9fecb55c4e23c1fdc1c6b908d
Keren Zhu <naeioi@hotmail.com>
2020-04-27 16:38:32 -0700

    Add ML1 and ML45 examples to MAML

---
+++ examples/torch/maml_trpo_ml1_push.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""This is an example to train MAML-TRPO on ML1 Push environment."""
+# pylint: disable=no-value-for-parameter
+import click
+from metaworld.benchmarks import ML1
+import torch
+
+from garage import wrap_experiment
+from garage.envs import normalize
+from garage.envs.base import GarageEnv
+from garage.experiment import LocalRunner, MetaEvaluator
+from garage.experiment.deterministic import set_seed
+from garage.experiment.task_sampler import SetTaskSampler
+from garage.np.baselines import LinearFeatureBaseline
+from garage.torch.algos import MAMLTRPO
+from garage.torch.policies import GaussianMLPPolicy
+
+
+@click.command()
+@click.option('--seed', default=1)
+@click.option('--epochs', default=300)
+@click.option('--rollouts_per_task', default=10)
+@click.option('--meta_batch_size', default=20)
+@wrap_experiment(snapshot_mode='all')
+def maml_trpo(ctxt, seed, epochs, rollouts_per_task, meta_batch_size):
+    """Set up environment and algorithm and run the task.
+
+    Args:
+        ctxt (garage.experiment.ExperimentContext): The experiment
+            configuration used by LocalRunner to create the snapshotter.
+        seed (int): Used to seed the random number generator to produce
+            determinism.
+        epochs (int): Number of training epochs.
+        rollouts_per_task (int): Number of rollouts per epoch per task
+            for training.
+        meta_batch_size (int): Number of tasks sampled per batch.
+
+    """
+    set_seed(seed)
+    env = GarageEnv(
+        normalize(ML1.get_train_tasks('push-v1'), expected_action_scale=10.))
+
+    policy = GaussianMLPPolicy(
+        env_spec=env.spec,
+        hidden_sizes=(100, 100),
+        hidden_nonlinearity=torch.tanh,
+        output_nonlinearity=None,
+    )
+
+    value_function = LinearFeatureBaseline(env_spec=env.spec)
+
+    max_path_length = 100
+
+    test_sampler = SetTaskSampler(lambda: GarageEnv(
+        normalize(ML1.get_test_tasks('push-v1'))))
+
+    meta_evaluator = MetaEvaluator(test_task_sampler=test_sampler,
+                                   max_path_length=max_path_length)
+
+    runner = LocalRunner(ctxt)
+    algo = MAMLTRPO(env=env,
+                    policy=policy,
+                    value_function=value_function,
+                    max_path_length=max_path_length,
+                    meta_batch_size=meta_batch_size,
+                    discount=0.99,
+                    gae_lambda=1.,
+                    inner_lr=0.1,
+                    num_grad_updates=1,
+                    meta_evaluator=meta_evaluator)
+
+    runner.setup(algo, env)
+    runner.train(n_epochs=epochs,
+                 batch_size=rollouts_per_task * max_path_length)
+
+
+maml_trpo()
