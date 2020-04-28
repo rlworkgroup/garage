a32f0546c55bc163b28cf8758ed83486e69d89ca
tsan <ahtsans@gmail.com>
2020-04-27 22:25:44 -0700

    Fix set model param

--- src/garage/tf/models/base.py
+++ src/garage/tf/models/base.py
@@ -339,9 +339,15 @@ class Model(BaseModel):
         """
         variables = self._get_variables()
         for name, var in variables.items():
-            if name in parameters:
-                var.load(parameters[name])
-            else:
+            found = False
+            # param name without model name
+            param_name = name[name.find(self.name) + len(self.name) + 1:]
+            for k, v in parameters.items():
+                if param_name in k:
+                    var.load(v)
+                    found = True
+                    continue
+            if not found:
                 warnings.warn('No value provided for variable {}'.format(name))
 
     @property
