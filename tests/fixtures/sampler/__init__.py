4f8ed2cff8c4e72e56d9cf55ca5f83db3df61386
Avnish Narayan <38871737+avnishn@users.noreply.github.com>
2020-04-30 14:33:23 -0700

    Remove conftest.py (#1354)
    solves #1259

---
+++ tests/fixtures/sampler/__init__.py
@@ -0,0 +1,6 @@
+"""Fixtures for testing samplers."""
+
+from tests.fixtures.sampler.ray_fixtures import (ray_local_session_fixture,
+                                                 ray_session_fixture)
+
+__all__ = ['ray_local_session_fixture', 'ray_session_fixture']
