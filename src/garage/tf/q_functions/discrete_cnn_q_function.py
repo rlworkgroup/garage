Fix pixel normalization in CNN Q functions (#1419)

src/garage/tf/q_functions/discrete_cnn_q_function.py
@@ -165,13 +165,22 @@ class DiscreteCNNQFunction(QFunction):
 
     def _initialize(self):
         """Initialize QFunction."""
-        obs_ph = tf.compat.v1.placeholder(tf.float32, (None, ) + self.obs_dim,
-                                          name='obs')
         if isinstance(self._env_spec.observation_space, akro.Image):
-            obs_ph = tf.cast(obs_ph, tf.float32) / 255.0
+            obs_ph = tf.compat.v1.placeholder(tf.uint8,
+                                              (None, ) + self.obs_dim,
+                                              name='obs')
+            augmented_obs_ph = tf.cast(obs_ph, tf.float32) / 255.0
+        else:
+            obs_ph = tf.compat.v1.placeholder(tf.float32,
+                                              (None, ) + self.obs_dim,
+                                              name='obs')
+            augmented_obs_ph = obs_ph
+
         with tf.compat.v1.variable_scope(self.name) as vs:
             self._variable_scope = vs
-            self.model.build(obs_ph)
+            self.model.build(augmented_obs_ph)
+
+        self._obs_input = obs_ph
 
     @property
     def q_vals(self):
@@ -191,7 +200,7 @@ class DiscreteCNNQFunction(QFunction):
             tf.Tensor: QFunction Input.
 
         """
-        return self.model.networks['default'].input
+        return self._obs_input
 
     # pylint: disable=arguments-differ
     def get_qval_sym(self, state_input, name):
@@ -206,9 +215,11 @@ class DiscreteCNNQFunction(QFunction):
 
         """
         with tf.compat.v1.variable_scope(self._variable_scope):
+            augmented_state_input = state_input
             if isinstance(self._env_spec.observation_space, akro.Image):
-                state_input = tf.cast(state_input, tf.float32) / 255.0
-            return self.model.build(state_input, name=name)
+                augmented_state_input = tf.cast(state_input,
+                                                tf.float32) / 255.0
+            return self.model.build(augmented_state_input, name=name)
 
     def clone(self, name):
         """Return a clone of the Q-function.
@@ -251,3 +262,14 @@ class DiscreteCNNQFunction(QFunction):
         """
         self.__dict__.update(state)
         self._initialize()
+
+    def __getstate__(self):
+        """Object.__getstate__.
+
+        Returns:
+            dict: The state.
+
+        """
+        new_dict = self.__dict__.copy()
+        del new_dict['_obs_input']
+        return new_dict

tests/garage/tf/q_functions/test_continuous_cnn_q_function.py
@@ -56,6 +56,19 @@ class TestContinuousCNNQFunction(TfGraphTestCase):
         for output in outputs:
             assert np.array_equal(output, expected_output)
 
+        # make sure observations are unflattened
+
+        obs = env.observation_space.flatten(obs)
+        qf._f_qval = mock.MagicMock()
+
+        qf.get_qval([obs], [act])
+        unflattened_obs = qf._f_qval.call_args_list[0][0][0]
+        assert unflattened_obs.shape[1:] == env.spec.observation_space.shape
+
+        qf.get_qval([obs, obs], [act, act])
+        unflattened_obs = qf._f_qval.call_args_list[1][0][0]
+        assert unflattened_obs.shape[1:] == env.spec.observation_space.shape
+
     # yapf: disable
     @pytest.mark.parametrize('filter_dims, num_filters, strides, '
                              'pool_strides, pool_shapes', [
@@ -138,37 +151,17 @@ class TestContinuousCNNQFunction(TfGraphTestCase):
                                                 num_filters=(5, ),
                                                 strides=(1, ))
 
-                    # make sure image obses are normalized in get_qval()
-
-                    qf._f_qval = mock.MagicMock()
-                    fake_obs = np.full(image_env.spec.observation_space.shape,
-                                       255)
-                    fake_act = np.full(image_env.spec.action_space.shape, 1)
-
-                    qf.get_qval(fake_obs, fake_act)
-                    normalized_obs = qf._f_qval.call_args_list[0][0][0]
-                    assert (normalized_obs == 1.).all()
-
-                    # make sure image obses are unflattened if necessary
-
-                    flattened_obs = image_env.spec.observation_space.flatten(
-                        fake_obs)
-                    qf.get_qval(flattened_obs, fake_act)
-                    normalized_obs = qf._f_qval.call_args_list[1][0][0]
-                    assert (normalized_obs.shape ==
-                            image_env.observation_space.shape
-                            and (normalized_obs == 1.).all())
+                    fake_obs = [
+                        np.full(image_env.spec.observation_space.shape, 255)
+                    ]
 
                     # make sure image obses are normalized in _initialize()
-
+                    # and get_qval
                     normalized_obs = build.call_args_list[0][0][1]
+                    assert normalized_obs != qf.inputs[0]
 
-                    input_ph = tf.compat.v1.get_default_graph(
-                    ).get_tensor_by_name('state:0')
-
-                    fake_obs = [fake_obs]
                     assert (self.sess.run(normalized_obs,
-                                          feed_dict={input_ph:
+                                          feed_dict={qf.inputs[0]:
                                                      fake_obs}) == 1.).all()
 
                     # make sure image obses are normalized in get_qval_sim()
@@ -186,14 +179,8 @@ class TestContinuousCNNQFunction(TfGraphTestCase):
                     qf.get_qval_sym(state_input, action_input, name='another')
                     normalized_obs = build.call_args_list[1][0][1]
 
-                    input_ph = tf.compat.v1.get_default_graph(
-                    ).get_tensor_by_name('Placeholder:0')
-
-                    fake_obs = [
-                        np.full(image_env.spec.observation_space.shape, 255)
-                    ]
                     assert (self.sess.run(normalized_obs,
-                                          feed_dict={input_ph:
+                                          feed_dict={state_input:
                                                      fake_obs}) == 1.).all()
 
     def test_obs_not_image(self):
@@ -215,27 +202,17 @@ class TestContinuousCNNQFunction(TfGraphTestCase):
                                                 strides=(1, ))
 
                     # ensure non-image obses are not normalized
-                    # in get_qval()
-
-                    qf._f_qval = mock.MagicMock()
-                    fake_obs = np.full(env.spec.observation_space.shape, 255)
-                    fake_act = np.full(env.spec.action_space.shape, 1)
-
-                    qf.get_qval(fake_obs, fake_act)
-                    normalized_obs = qf._f_qval.call_args_list[0][0][0]
-                    assert (normalized_obs == 255.).all()
-
-                    # ensure non-image obses are not normalized
-                    # in _initialize()
+                    # in _initialize() and get_qval()
 
                     normalized_obs = build.call_args_list[0][0][1]
+                    assert normalized_obs == qf.inputs[0]
 
-                    input_ph = tf.compat.v1.get_default_graph(
-                    ).get_tensor_by_name('state:0')
+                    fake_obs = [
+                        np.full(env.spec.observation_space.shape, 255.)
+                    ]
 
-                    fake_obs = [np.full(env.spec.observation_space.shape, 255)]
                     assert (self.sess.run(normalized_obs,
-                                          feed_dict={input_ph:
+                                          feed_dict={qf.inputs[0]:
                                                      fake_obs}) == 255.).all()
 
                     # ensure non-image obses are not normalized
@@ -254,12 +231,8 @@ class TestContinuousCNNQFunction(TfGraphTestCase):
                     qf.get_qval_sym(state_input, action_input, name='another')
                     normalized_obs = build.call_args_list[1][0][1]
 
-                    input_ph = tf.compat.v1.get_default_graph(
-                    ).get_tensor_by_name('Placeholder:0')
-
-                    fake_obs = [np.full(env.spec.observation_space.shape, 255)]
                     assert (self.sess.run(normalized_obs,
-                                          feed_dict={input_ph:
+                                          feed_dict={state_input:
                                                      fake_obs}) == 255.).all()
 
     # yapf: disable

tests/garage/tf/q_functions/test_discrete_cnn_q_function.py
@@ -76,26 +76,24 @@ class TestDiscreteCNNQFunction(TfGraphTestCase):
                                       dueling=False)
             normalized_obs = build.call_args_list[0][0][1]
 
-            input_ph = tf.compat.v1.get_default_graph().get_tensor_by_name(
-                'obs:0')
+            input_ph = qf.input
+            assert input_ph != normalized_obs
 
             fake_obs = [np.full(image_env.spec.observation_space.shape, 255)]
             assert (self.sess.run(normalized_obs,
                                   feed_dict={input_ph: fake_obs}) == 1.).all()
 
             obs_dim = image_env.spec.observation_space.shape
-            state_input = tf.compat.v1.placeholder(tf.float32,
+            state_input = tf.compat.v1.placeholder(tf.uint8,
                                                    shape=(None, ) + obs_dim)
 
             qf.get_qval_sym(state_input, name='another')
             normalized_obs = build.call_args_list[1][0][1]
 
-            input_ph = tf.compat.v1.get_default_graph().get_tensor_by_name(
-                'Placeholder:0')
-
             fake_obs = [np.full(image_env.spec.observation_space.shape, 255)]
             assert (self.sess.run(normalized_obs,
-                                  feed_dict={input_ph: fake_obs}) == 1.).all()
+                                  feed_dict={state_input:
+                                             fake_obs}) == 1.).all()
 
     def test_obs_not_image(self):
         env = self.env
@@ -111,8 +109,8 @@ class TestDiscreteCNNQFunction(TfGraphTestCase):
                                       dueling=False)
             normalized_obs = build.call_args_list[0][0][1]
 
-            input_ph = tf.compat.v1.get_default_graph().get_tensor_by_name(
-                'obs:0')
+            input_ph = qf.input
+            assert input_ph == normalized_obs
 
             fake_obs = [np.full(env.spec.observation_space.shape, 255)]
             assert (self.sess.run(normalized_obs,
@@ -126,12 +124,9 @@ class TestDiscreteCNNQFunction(TfGraphTestCase):
             qf.get_qval_sym(state_input, name='another')
             normalized_obs = build.call_args_list[1][0][1]
 
-            input_ph = tf.compat.v1.get_default_graph().get_tensor_by_name(
-                'Placeholder:0')
-
             fake_obs = [np.full(env.spec.observation_space.shape, 255)]
             assert (self.sess.run(normalized_obs,
-                                  feed_dict={input_ph:
+                                  feed_dict={state_input:
                                              fake_obs}) == 255).all()
 
     # yapf: disable