4f139d60fd6df20170df26f3c6e964e2985586de
Ryan Julian <ryanjulian@users.noreply.github.com>
2020-06-02 00:16:00 -0700

    Delete np/batch_polopt.py
    I think this was forgotten in #1486

--- src/garage/np/algos/__init__.py
+++ src/garage/np/algos/__init__.py
@@ -1,5 +1,4 @@
 """Reinforcement learning algorithms which use NumPy as a numerical backend."""
-from garage.np.algos.batch_polopt import BatchPolopt
 from garage.np.algos.cem import CEM
 from garage.np.algos.cma_es import CMAES
 from garage.np.algos.meta_rl_algorithm import MetaRLAlgorithm
@@ -8,6 +7,6 @@ from garage.np.algos.off_policy_rl_algorithm import OffPolicyRLAlgorithm
 from garage.np.algos.rl_algorithm import RLAlgorithm
 
 __all__ = [
-    'RLAlgorithm', 'BatchPolopt', 'CEM', 'CMAES', 'MetaRLAlgorithm', 'NOP',
+    'RLAlgorithm', 'CEM', 'CMAES', 'MetaRLAlgorithm', 'NOP',
     'OffPolicyRLAlgorithm'
 ]
--- tests/fixtures/algos/dummy_algo.py
+++ tests/fixtures/algos/dummy_algo.py
@@ -1,8 +1,8 @@
 """A dummy algorithm fixture."""
-from garage.np.algos import BatchPolopt
+from garage.np.algos import RLAlgorithm
 
 
-class DummyAlgo(BatchPolopt):
+class DummyAlgo(RLAlgorithm):  # pylint: disable=too-few-public-methods
     """Dummy algo for test.
 
     Args:
@@ -13,12 +13,12 @@ class DummyAlgo(BatchPolopt):
     """
 
     def __init__(self, env_spec, policy, baseline):
-        super().__init__(env_spec=env_spec,
-                         policy=policy,
-                         baseline=baseline,
-                         discount=0.1,
-                         max_path_length=1,
-                         n_samples=10)
+        self.env_spec = env_spec
+        self.policy = policy
+        self.baseline = baseline
+        self.discount = 0.9
+        self.max_path_length = 1
+        self.n_samples = 10
 
     def train(self, runner):
         """Obtain samplers and start actual training for each epoch.
@@ -29,4 +29,5 @@ class DummyAlgo(BatchPolopt):
             runner (LocalRunner): LocalRunner is passed to give algorithm
                 the access to runner.step_epochs(), which provides services
                 such as snapshotting and sampler control.
+
         """
