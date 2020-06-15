91df3fb67524bf3ee2a017f4bef3e9bdb8ee7c97
Ryan Julian <ryanjulian@users.noreply.github.com>
2020-06-15 12:12:32 -0700

    Pin TensorFlow to >=1.14

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ REQUIRED = [
     'scikit-image',
     'scipy',
     'setproctitle>=1.0',
-    'tensorflow',
+    'tensorflow>=1.14',
     'tensorflow-probability',
     'torch>=1.0.0,<1.5.0',
     'torchvision>=0.2.1,<0.6.0',
