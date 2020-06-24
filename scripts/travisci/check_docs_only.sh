0c5314e5072de1526528db5470a7df50490d7621
Ryan Julian <rjulian@usc.edu>
2020-06-24 13:42:48 -0700

    Skip length build steps for docs-only PRs

---
+++ scripts/travisci/check_docs_only.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+status=0
+
+echo "Checking commit range ${TRAVIS_COMMIT_RANGE}"
+SOURCE="${TRAVIS_COMMIT_RANGE%...*}"
+ORIGIN="${TRAVIS_COMMIT_RANGE#*...}"
+status="$((${status} | ${?}))"
+
+while read commit; do
+  echo "Checking for docs only in ${commit}"
+  not_docs="$(git show --name-only --oneline ${commit} \
+    | tail -n +2 \
+    | awk -F . '{print $NF}' \
+    | uniq \
+    | grep -v 'md\|rst')"
+  test -z "${not_docs}"
+  pass=$?
+  status="$((${status} | ${pass}))"
+
+  # Print message if it fails
+  if [[ "${pass}" -ne 0 ]]; then
+    echo "Found non-documentation changes in commit ${commit}"
+  fi
+
+done < <(git log --cherry-pick --left-only --pretty="%H" \
+                 "${ORIGIN}...${SOURCE}")
+
+exit "${status}"
