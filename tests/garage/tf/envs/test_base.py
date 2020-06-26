3b21b42125410fa5d96a5c64b0164daf68b109c6
Eric Yihan Chen <ericyihc@gmail.com>
2020-06-26 13:02:46 -0700

    Skip bullet envs in test_base

--- tests/garage/tf/envs/test_base.py
+++ tests/garage/tf/envs/test_base.py
@@ -1,6 +1,7 @@
 import pickle
 
 import gym
+import pybullet_envs
 import pytest
 
 from garage.envs import GarageEnv
@@ -17,18 +18,30 @@ class TestGarageEnv:
     @pytest.mark.nightly
     @pytest.mark.parametrize('spec', list(gym.envs.registry.all()))
     def test_all_gym_envs(self, spec):
+        bullet_envs = [
+            env.replace('- ', '') for env in pybullet_envs.getList()
+        ]
         if spec._env_name.startswith('Defender'):
             pytest.skip(
                 'Defender-* envs bundled in atari-py 0.2.x don\'t load')
+        elif spec._env_name in bullet_envs:
+            pytest.skip('Bullet environments will be loaded differently via '
+                        'BulletEnv()')
         env = GarageEnv(spec.make())
         step_env_with_gym_quirks(env, spec)
 
     @pytest.mark.nightly
     @pytest.mark.parametrize('spec', list(gym.envs.registry.all()))
     def test_all_gym_envs_pickleable(self, spec):
+        bullet_envs = [
+            env.replace('- ', '') for env in pybullet_envs.getList()
+        ]
         if spec._env_name.startswith('Defender'):
             pytest.skip(
                 'Defender-* envs bundled in atari-py 0.2.x don\'t load')
+        elif spec._env_name in bullet_envs:
+            pytest.skip('Bullet environments will be loaded differently via '
+                        'BulletEnv()')
         env = GarageEnv(env_name=spec.id)
         step_env_with_gym_quirks(env,
                                  spec,
