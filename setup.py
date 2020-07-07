a53708e8337b875bcb6c533934c60b27786401eb
Ryan Julian <ryanjulian@users.noreply.github.com>
2020-07-07 12:22:21 -0700

    Fix pipenv dependency resolution (#1721)

--- setup.py
+++ setup.py
@@ -1,31 +1,37 @@
 """setuptools based setup module."""
 import os
 
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
 GARAGE_GH_TOKEN = os.environ.get('GARAGE_GH_TOKEN') or 'git'
-GYM_VERSION = '0.17.2'
+GYM_VERSION = '==0.15.4'
 
 # Required dependencies
 REQUIRED = [
     # Please keep alphabetized
     'akro',
     'click>=2.0',
-    'cloudpickle==1.3',
+    'cloudpickle',
     'cma==2.7.0',
     'dowel==0.0.3',
-    f'gym[atari,box2d,classic_control]=={GYM_VERSION}',
+    'gym[atari,box2d,classic_control]' + GYM_VERSION,
+    'joblib<0.13,>=0.12',
     'numpy>=1.14.5',
     'psutil',
+    # Pyglet 1.4.0 introduces some api change which breaks some
+    # gym environments
+    # See: https://github.com/openai/gym/issues/1588
+    'pyglet<1.4.0,>=1.3.0',
     'python-dateutil',
     'ray',
     'scikit-image',
     'scipy',
     'setproctitle>=1.0',
     'tensorflow>=1.14',
-    'tensorflow-probability>=0.11.0',
-    'torch>=1.0.0,!=1.5.0,<1.6.0',
-    'torchvision>=0.2.1,<0.7.0',
+    'tensorflow-probability',
+    'torch>=1.0.0,<1.5.0',
+    'torchvision>=0.2.1,<0.6.0',
 ]
 
 # Dependencies for optional features
@@ -33,7 +39,7 @@ EXTRAS = {}
 
 EXTRAS['mujoco'] = [
     'mujoco-py<2.1,>=2.0',
-    f'gym[all]=={GYM_VERSION}',
+    'gym[all]' + GYM_VERSION,
 ]
 
 EXTRAS['dm_control'] = [
@@ -41,10 +47,8 @@ EXTRAS['dm_control'] = [
     # find a build dependency (absl-py). Later pip executes the `install`
     # command again and the install succeeds because absl-py has been
     # installed. This is stupid, but harmless.
-    'dm_control',
+    'dm_control==0.0.300771433',
 ]
-
-EXTRAS['bullet'] = ['mpi4py', 'pybullet>=2.8.7']
 
 EXTRAS['all'] = list(set(sum(EXTRAS.values(), [])))
 
@@ -54,23 +58,20 @@ EXTRAS['dev'] = [
     'flake8',
     'flake8-docstrings>=1.5.0',
     'flake8-import-order',
-    f'metaworld @ https://{GARAGE_GH_TOKEN}@api.github.com/repos/rlworkgroup/metaworld/tarball/861ae8d8c4bef80a7ed86f47f47acaa494d4ab77',  # noqa: E501
-    'isort>=4.3.21,<5.0.0',
+    'metaworld @ https://{}@api.github.com/repos/rlworkgroup/metaworld/tarball/861ae8d8c4bef80a7ed86f47f47acaa494d4ab77'.format(GARAGE_GH_TOKEN),  # noqa: E501
     'pep8-naming==0.7.0',
     'pre-commit',
     'pycodestyle>=2.5.0',
     'pydocstyle>=4.0.0',
-    'pylint>=2.5.3',
+    'pylint>=2.4.3',
     'pytest>=4.5.0',  # Required for strict-markers
     'pytest-cov',
     'pytest-timeout',
     'pytest-xdist',
     'recommonmark',
     'sphinx',
-    'sphinx-autoapi>=1.4.0',
     'sphinx_rtd_theme',
-    'sphinxcontrib-bibtex',
-    'yapf==0.30.0',
+    'yapf==0.28.0',
 ]  # yapf: disable
 
 with open('README.md') as f:
@@ -89,7 +90,7 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     scripts=['scripts/garage'],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     license='MIT',
@@ -101,6 +102,7 @@ setup(
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
