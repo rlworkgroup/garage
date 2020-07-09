e67e070f6866f44eb33be6dd881c3f9f3a9575af
Ryan Julian <ryanjulian@users.noreply.github.com>
2020-07-09 09:17:05 -0700

    Relax DQN test last_avg_reg (#1744)

--- tests/garage/tf/algos/test_dqn.py
+++ tests/garage/tf/algos/test_dqn.py
@@ -4,18 +4,18 @@ too low.
 """
 import pickle
 
+import gym
 import numpy as np
 import pytest
 import tensorflow as tf
 
-from garage.envs import GymEnv
-from garage.experiment import deterministic, LocalTFRunner
+from garage.envs import GarageEnv
+from garage.experiment import LocalTFRunner
 from garage.np.exploration_policies import EpsilonGreedyPolicy
 from garage.replay_buffer import PathBuffer
 from garage.tf.algos import DQN
 from garage.tf.policies import DiscreteQfDerivedPolicy
 from garage.tf.q_functions import DiscreteMLPQFunction
-
 from tests.fixtures import snapshot_config, TfGraphTestCase
 
 
@@ -24,13 +24,12 @@ class TestDQN(TfGraphTestCase):
     @pytest.mark.large
     def test_dqn_cartpole(self):
         """Test DQN with CartPole environment."""
-        deterministic.set_seed(100)
         with LocalTFRunner(snapshot_config, sess=self.sess) as runner:
             n_epochs = 10
             steps_per_epoch = 10
             sampler_batch_size = 500
             num_timesteps = n_epochs * steps_per_epoch * sampler_batch_size
-            env = GymEnv('CartPole-v0')
+            env = GarageEnv(gym.make('CartPole-v0'))
             replay_buffer = PathBuffer(capacity_in_transitions=int(1e4))
             qf = DiscreteMLPQFunction(env_spec=env.spec, hidden_sizes=(64, 64))
             policy = DiscreteQfDerivedPolicy(env_spec=env.spec, qf=qf)
@@ -46,7 +45,6 @@ class TestDQN(TfGraphTestCase):
                        qf=qf,
                        exploration_policy=epilson_greedy_policy,
                        replay_buffer=replay_buffer,
-                       max_episode_length=100,
                        qf_lr=1e-4,
                        discount=1.0,
                        min_buffer_size=int(1e3),
@@ -59,20 +57,19 @@ class TestDQN(TfGraphTestCase):
             runner.setup(algo, env)
             last_avg_ret = runner.train(n_epochs=n_epochs,
                                         batch_size=sampler_batch_size)
-            assert last_avg_ret > 8.8
+            assert last_avg_ret > 15
 
             env.close()
 
     @pytest.mark.large
     def test_dqn_cartpole_double_q(self):
         """Test DQN with CartPole environment."""
-        deterministic.set_seed(100)
         with LocalTFRunner(snapshot_config, sess=self.sess) as runner:
             n_epochs = 10
             steps_per_epoch = 10
             sampler_batch_size = 500
             num_timesteps = n_epochs * steps_per_epoch * sampler_batch_size
-            env = GymEnv('CartPole-v0')
+            env = GarageEnv(gym.make('CartPole-v0'))
             replay_buffer = PathBuffer(capacity_in_transitions=int(1e4))
             qf = DiscreteMLPQFunction(env_spec=env.spec, hidden_sizes=(64, 64))
             policy = DiscreteQfDerivedPolicy(env_spec=env.spec, qf=qf)
@@ -88,7 +85,6 @@ class TestDQN(TfGraphTestCase):
                        qf=qf,
                        exploration_policy=epilson_greedy_policy,
                        replay_buffer=replay_buffer,
-                       max_episode_length=100,
                        qf_lr=1e-4,
                        discount=1.0,
                        min_buffer_size=int(1e3),
@@ -101,20 +97,19 @@ class TestDQN(TfGraphTestCase):
             runner.setup(algo, env)
             last_avg_ret = runner.train(n_epochs=n_epochs,
                                         batch_size=sampler_batch_size)
-            assert last_avg_ret > 8.8
+            assert last_avg_ret > 15
 
             env.close()
 
     @pytest.mark.large
     def test_dqn_cartpole_grad_clip(self):
         """Test DQN with CartPole environment."""
-        deterministic.set_seed(100)
         with LocalTFRunner(snapshot_config, sess=self.sess) as runner:
             n_epochs = 10
             steps_per_epoch = 10
             sampler_batch_size = 500
             num_timesteps = n_epochs * steps_per_epoch * sampler_batch_size
-            env = GymEnv('CartPole-v0')
+            env = GarageEnv(gym.make('CartPole-v0'))
             replay_buffer = PathBuffer(capacity_in_transitions=int(1e4))
             qf = DiscreteMLPQFunction(env_spec=env.spec, hidden_sizes=(64, 64))
             policy = DiscreteQfDerivedPolicy(env_spec=env.spec, qf=qf)
@@ -130,7 +125,6 @@ class TestDQN(TfGraphTestCase):
                        qf=qf,
                        exploration_policy=epilson_greedy_policy,
                        replay_buffer=replay_buffer,
-                       max_episode_length=100,
                        qf_lr=1e-4,
                        discount=1.0,
                        min_buffer_size=int(1e3),
@@ -144,19 +138,18 @@ class TestDQN(TfGraphTestCase):
             runner.setup(algo, env)
             last_avg_ret = runner.train(n_epochs=n_epochs,
                                         batch_size=sampler_batch_size)
-            assert last_avg_ret > 8.8
+            assert last_avg_ret > 15
 
             env.close()
 
     def test_dqn_cartpole_pickle(self):
         """Test DQN with CartPole environment."""
-        deterministic.set_seed(100)
         with LocalTFRunner(snapshot_config, sess=self.sess) as runner:
             n_epochs = 10
             steps_per_epoch = 10
             sampler_batch_size = 500
             num_timesteps = n_epochs * steps_per_epoch * sampler_batch_size
-            env = GymEnv('CartPole-v0')
+            env = GarageEnv(gym.make('CartPole-v0'))
             replay_buffer = PathBuffer(capacity_in_transitions=int(1e4))
             qf = DiscreteMLPQFunction(env_spec=env.spec, hidden_sizes=(64, 64))
             policy = DiscreteQfDerivedPolicy(env_spec=env.spec, qf=qf)
@@ -172,7 +165,6 @@ class TestDQN(TfGraphTestCase):
                        qf=qf,
                        exploration_policy=epilson_greedy_policy,
                        replay_buffer=replay_buffer,
-                       max_episode_length=100,
                        qf_lr=1e-4,
                        discount=1.0,
                        min_buffer_size=int(1e3),
@@ -184,7 +176,7 @@ class TestDQN(TfGraphTestCase):
                        buffer_batch_size=32)
             runner.setup(algo, env)
             with tf.compat.v1.variable_scope(
-                    'DiscreteMLPQFunction/mlp/hidden_0', reuse=True):
+                    'DiscreteMLPQFunction/MLPModel/mlp/hidden_0', reuse=True):
                 bias = tf.compat.v1.get_variable('bias')
                 # assign it to all one
                 old_bias = tf.ones_like(bias).eval()
@@ -194,7 +186,8 @@ class TestDQN(TfGraphTestCase):
             with tf.compat.v1.Session(graph=tf.Graph()):
                 pickle.loads(h)
                 with tf.compat.v1.variable_scope(
-                        'DiscreteMLPQFunction/mlp/hidden_0', reuse=True):
+                        'DiscreteMLPQFunction/MLPModel/mlp/hidden_0',
+                        reuse=True):
                     new_bias = tf.compat.v1.get_variable('bias')
                     new_bias = new_bias.eval()
                     assert np.array_equal(old_bias, new_bias)
