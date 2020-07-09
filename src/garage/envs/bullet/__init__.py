69c3ee15c858679b65b3ca206eab534a21c60408
Eric Yihan Chen <ericyihc@gmail.com>
2020-07-08 17:12:44 -0700

    Remove hardcoded bullet env list (#1737)

--- src/garage/envs/bullet/__init__.py
+++ src/garage/envs/bullet/__init__.py
@@ -13,23 +13,6 @@ from garage.envs.bullet.bullet_env import BulletEnv
 __all__ = ['BulletEnv']
 
 
-def _get_bullet_env_list():
-    """Return a complete list of Bullet Gym environments.
-
-    Returns:
-        list: a list of bullet environment id (str)
-    """
-    envs = [env.replace('- ', '') for env in pybullet_envs.getList()]
-    # Hardcoded missing environment names from pybullet_envs.getList()
-    envs.extend([
-        'MinitaurExtendedEnv-v0', 'MinitaurReactiveEnv-v0',
-        'MinitaurBallGymEnv-v0', 'MinitaurTrottingEnv-v0',
-        'MinitaurStandGymEnv-v0', 'MinitaurAlternatingLegsEnv-v0',
-        'MinitaurFourLegStandEnv-v0', 'KukaDiverseObjectGrasping-v0'
-    ])
-    return envs
-
-
 def _get_unsupported_env_list():
     """Return a list of unsupported Bullet Gym environments.
 
