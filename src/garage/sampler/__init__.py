689f0081d7e5422e8d4b070e87522cb980f4eae6
Ryan Julian <ryanjulian@users.noreply.github.com>
2020-07-15 21:02:30 -0700

    Export EnvUpdates in garage.sampler
    This will ensure they appear in the API docs

--- src/garage/sampler/__init__.py
+++ src/garage/sampler/__init__.py
@@ -1,6 +1,10 @@
 """Samplers which run agents in environments."""
-
+# yapf: disable
 from garage.sampler.default_worker import DefaultWorker
+from garage.sampler.env_update import (EnvUpdate,
+                                       ExistingEnvUpdate,
+                                       NewEnvUpdate,
+                                       SetTaskUpdate)
 from garage.sampler.local_sampler import LocalSampler
 from garage.sampler.multiprocessing_sampler import MultiprocessingSampler
 from garage.sampler.ray_sampler import RaySampler
@@ -8,6 +12,8 @@ from garage.sampler.sampler import Sampler
 from garage.sampler.vec_worker import VecWorker
 from garage.sampler.worker import Worker
 from garage.sampler.worker_factory import WorkerFactory
+
+# yapf: enable
 
 __all__ = [
     'Sampler',
@@ -18,4 +24,8 @@ __all__ = [
     'WorkerFactory',
     'Worker',
     'DefaultWorker',
+    'EnvUpdate',
+    'NewEnvUpdate',
+    'SetTaskUpdate',
+    'ExistingEnvUpdate',
 ]
