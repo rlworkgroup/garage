e6344677a04511a8286491a4caec0625c0e45872
ruofu <ruofuwan@usc.edu>
2020-07-26 18:17:54 -0700

    Fix gathering env_infos

--- src/garage/sampler/vec_worker.py
+++ src/garage/sampler/vec_worker.py
@@ -124,6 +124,14 @@ class VecWorker(DefaultWorker):
     def _gather_rollout(self, rollout_number, last_observation):
         assert 0 < self._path_lengths[
             rollout_number] <= self._max_episode_length
+        env_infos = self._env_infos[rollout_number]
+        self._env_infos[rollout_number] = collections.defaultdict(list)
+        agent_infos = self._agent_infos[rollout_number]
+        self._agent_infos[rollout_number] = collections.defaultdict(list)
+        for k, v in env_infos.items():
+            env_infos[k] = np.asarray(v)
+        for k, v in agent_infos.items():
+            agent_infos[k] = np.asarray(v)
         traj = TrajectoryBatch(
             env_spec=self._envs[rollout_number].spec,
             observations=np.asarray(self._observations[rollout_number]),
@@ -132,8 +140,8 @@ class VecWorker(DefaultWorker):
             rewards=np.asarray(self._rewards[rollout_number]),
             step_types=np.asarray(self._step_types[rollout_number],
                                   dtype=StepType),
-            env_infos=self._env_infos[rollout_number],
-            agent_infos=self._agent_infos[rollout_number],
+            env_infos=dict(env_infos),
+            agent_infos=dict(agent_infos),
             lengths=np.asarray([self._path_lengths[rollout_number]],
                                dtype='l'))
         self._completed_rollouts.append(traj)
