4fe761de292dd768329e06c2336c630aed424e53
ruofu <ruofuwan@usc.edu>
2020-08-04 15:31:45 -0700

    Use float32

--- src/garage/tf/baselines/gaussian_cnn_baseline.py
+++ src/garage/tf/baselines/gaussian_cnn_baseline.py
@@ -198,14 +198,11 @@ class GaussianCNNBaseline(GaussianCNNBaselineModel, Baseline):
         self._initialize()
 
     def _initialize(self):
-        if isinstance(self._env_spec.observation_space, akro.Image):
-            input_var = tf.compat.v1.placeholder(tf.uint8,
-                                                 shape=(None, ) +
-                                                 self._input_shape)
-        else:
-            input_var = tf.compat.v1.placeholder(tf.float32,
-                                                 shape=(None, ) +
-                                                 self._input_shape)
+        input_var = tf.compat.v1.placeholder(tf.float32,
+                                             shape=(None, ) +
+                                             self._input_shape)
+        if isinstance(self.env_spec.observation_space, akro.Image):
+            input_var = tf.cast(input_var, tf.float32) / 255.0
 
         ys_var = tf.compat.v1.placeholder(dtype=tf.float32,
                                           name='ys',
