cdb3bb6fc576317c9d9d249f9a67063d1a52ca7d
Mishari Aliesa <aliesa@usc.edu>
2020-09-01 11:55:01 -0700

    Add torch DQN

---
+++ examples/torch/dqn_cartpole.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""An example to train a task with DQN algorithm.
+
+Here it creates a gym environment CartPole, and trains a DQN with 50k steps.
+"""
+from garage import wrap_experiment
+from garage.envs import GymEnv
+from garage.experiment import LocalRunner
+from garage.experiment.deterministic import set_seed
+from garage.np.exploration_policies import EpsilonGreedyPolicy
+from garage.replay_buffer import PathBuffer
+from garage.torch.algos import DQN
+from garage.torch.policies import DiscreteQFDerivedPolicy
+from garage.torch.q_functions import DiscreteMLPQFunction
+
+hyperparams = dict(
+    n_epochs=100,
+    steps_per_epoch=10,
+    sampler_batch_size=512,
+    lr=5e-5,
+    discount=0.9,
+    min_buffer_size=int(1e4),
+    n_train_steps=500,
+    target_update_freq=30,
+    buffer_batch_size=64,
+    max_epsilon=1.0,
+    min_epsilon=0.01,
+    decay_ratio=0.4,
+    buffer_size=int(1e6),
+    hidden_sizes=(8, 5),
+)
+
+
+@wrap_experiment(snapshot_mode='none')
+def dqn_cartpole(ctxt=None, seed=24, **kwargs):
+    """Train DQN with CartPole-v0 environment.
+
+    Args:
+        ctxt (garage.experiment.ExperimentContext): The experiment
+            configuration used by LocalRunner to create the snapshotter.
+        seed (int): Used to seed the random number generator to produce
+            determinism.
+
+    """
+
+    set_seed(seed)
+    runner = LocalRunner(ctxt)
+
+    n_epochs = hyperparams['n_epochs']
+    steps_per_epoch = hyperparams['steps_per_epoch']
+    sampler_batch_size = hyperparams['sampler_batch_size']
+    num_timesteps = n_epochs * steps_per_epoch * sampler_batch_size
+    env = GymEnv('CartPole-v0')
+    replay_buffer = PathBuffer(
+        capacity_in_transitions=hyperparams['buffer_size'])
+    qf = DiscreteMLPQFunction(env_spec=env.spec,
+                              hidden_sizes=hyperparams['hidden_sizes'])
+    policy = DiscreteQFDerivedPolicy(env_spec=env.spec, qf=qf)
+    exploration_policy = EpsilonGreedyPolicy(
+        env_spec=env.spec,
+        policy=policy,
+        total_timesteps=num_timesteps,
+        max_epsilon=hyperparams['max_epsilon'],
+        min_epsilon=hyperparams['min_epsilon'],
+        decay_ratio=hyperparams['decay_ratio'])
+    algo = DQN(env_spec=env.spec,
+               policy=policy,
+               qf=qf,
+               exploration_policy=exploration_policy,
+               replay_buffer=replay_buffer,
+               steps_per_epoch=steps_per_epoch,
+               qf_lr=hyperparams['lr'],
+               discount=hyperparams['discount'],
+               min_buffer_size=hyperparams['min_buffer_size'],
+               n_train_steps=hyperparams['n_train_steps'],
+               target_update_freq=hyperparams['target_update_freq'],
+               buffer_batch_size=hyperparams['buffer_batch_size'])
+
+    runner.setup(algo, env)
+    runner.train(n_epochs=n_epochs, batch_size=sampler_batch_size)
+
+
+dqn_cartpole(**hyperparams)
---
+++ src/garage/torch/algos/dqn.py
@@ -0,0 +1,241 @@
+"""This modules creates a DDPG model in PyTorch."""
+import copy
+
+from dowel import logger, tabular
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from garage import _Default, log_performance, make_optimizer
+from garage.np import obtain_evaluation_episodes
+from garage.np.algos import RLAlgorithm
+from garage.sampler import DefaultWorker, LocalSampler
+from garage.torch import dict_np_to_torch, torch_to_np
+
+
+class DQN(RLAlgorithm):
+    """A DDPG model implemented with PyTorch.
+
+    DDPG, also known as Deep Deterministic Policy Gradient, uses actor-critic
+    method to optimize the policy and Q-function prediction. It uses a
+    supervised method to update the critic network and policy gradient to
+    update the actor network. And there are exploration strategy, replay
+    buffer and target networks involved to stabilize the training process.
+
+    Args:
+        env_spec (EnvSpec): Environment specification.
+        policy (garage.torch.policies.Policy): Policy. For DQN, this is a
+            policy that performs the action that yields the highest Q value.
+        qf (object): Q-value network.
+        replay_buffer (ReplayBuffer): Replay buffer.
+        steps_per_epoch (int): Number of train_once calls per epoch.
+        n_train_steps (int): Training steps.
+        max_episode_length_eval (int or None): Maximum length of episodes used
+            for off-policy evaluation. If `None`, defaults to
+            `env_spec.max_episode_length`.
+        buffer_batch_size (int): Batch size of replay buffer.
+        min_buffer_size (int): The minimum buffer size for replay buffer.
+        exploration_policy (ExplorationPolicy): Exploration strategy, typically
+            epsilon-greedy.
+        target_update_freq (int): Number of optimization steps between each
+            update to the target Q network.
+        discount(float): Discount factor for the cumulative return.
+        qf_optimizer (Union[type, tuple[type, dict]]): Type of optimizer
+            for training Q-value network. This can be an optimizer type such
+            as `torch.optim.Adam` or a tuple of type and dictionary, where
+            dictionary contains arguments to initialize the optimizer
+            e.g. `(torch.optim.Adam, {'lr' : 1e-3})`.
+        qf_lr (float): Learning rate for Q-value network parameters.
+        clip_rewards (float): Clip reward to be in [-clip_rewards,
+            clip_rewards]. If None, rewards are not clipped.
+        clip_gradient (float): Clip gradients to be in
+            [-clip_gradient, clip_gradient]. If None, gradients are not
+            clipped. Defaults to 1.0.
+        reward_scale (float): Reward scale.
+    """
+
+    def __init__(
+            self,
+            env_spec,
+            policy,
+            qf,
+            replay_buffer,
+            exploration_policy=None,
+            qf_optimizer=torch.optim.Adam,
+            *,  # Everything after this is numbers.
+            steps_per_epoch=20,
+            n_train_steps=50,
+            max_episode_length_eval=None,
+            buffer_batch_size=64,
+            min_buffer_size=int(1e4),
+            discount=0.99,
+            qf_lr=_Default(1e-3),
+            clip_rewards=None,
+            clip_gradient=1.0,
+            target_update_freq=5,
+            reward_scale=1.):
+        self._clip_reward = clip_rewards
+        self._clip_grad = clip_gradient
+
+        self._steps_per_epoch = steps_per_epoch
+        self._target_update_freq = target_update_freq
+        self._episode_qf_losses = []
+        self._epoch_ys = []
+        self._epoch_qs = []
+
+        self._policy = policy
+        self._qf = qf
+        self._n_train_steps = n_train_steps
+
+        self._min_buffer_size = min_buffer_size
+        self._qf = qf
+        self._steps_per_epoch = steps_per_epoch
+        self._n_train_steps = n_train_steps
+        self._buffer_batch_size = buffer_batch_size
+        self._discount = discount
+        self._reward_scale = reward_scale
+        self.max_episode_length = env_spec.max_episode_length
+        self._max_episode_length_eval = (max_episode_length_eval
+                                         or self.max_episode_length)
+
+        self.env_spec = env_spec
+        self.replay_buffer = replay_buffer
+        self.policy = policy
+        self.exploration_policy = exploration_policy
+
+        self._target_qf = copy.deepcopy(self._qf)
+        self._qf_optimizer = make_optimizer(qf_optimizer,
+                                            module=self._qf,
+                                            lr=qf_lr)
+        self._eval_env = None
+        self.sampler_cls = LocalSampler
+        self.worker_cls = DefaultWorker
+
+    def train(self, runner):
+        """Obtain samplers and start actual training for each epoch.
+
+        Args:
+            runner (LocalRunner): Experiment runner.
+
+        Returns:
+            float: The average return in last epoch cycle.
+
+        """
+        if not self._eval_env:
+            self._eval_env = runner.get_env_copy()
+        last_returns = [float('nan')]
+        runner.enable_logging = False
+
+        for _ in runner.step_epochs():
+            for cycle in range(self._steps_per_epoch):
+                runner.step_path = runner.obtain_episodes(runner.step_itr)
+                self.train_once(runner.step_itr, runner.step_path)
+                if (cycle == 0 and self.replay_buffer.n_transitions_stored >=
+                        self._min_buffer_size):
+                    runner.enable_logging = True
+                    eval_eps = obtain_evaluation_episodes(
+                        self.policy, self._eval_env)
+                    last_returns = log_performance(runner.step_itr,
+                                                   eval_eps,
+                                                   discount=self._discount)
+                runner.step_itr += 1
+
+        return np.mean(last_returns)
+
+    def train_once(self, itr, episodes):
+        """Perform one iteration of training.
+
+        Args:
+            itr (int): Iteration number.
+            episodes (EpisodeBatch): Batch of episodes.
+
+        """
+        self.replay_buffer.add_episode_batch(episodes)
+
+        epoch = itr / self._steps_per_epoch
+
+        for _ in range(self._n_train_steps):
+            if (self.replay_buffer.n_transitions_stored >=
+                    self._min_buffer_size):
+                samples = self.replay_buffer.sample_transitions(
+                    self._buffer_batch_size)
+                samples['rewards'] *= self._reward_scale
+                qf_loss, y, q = torch_to_np(self.optimize_qf(samples))
+
+                self._episode_qf_losses.append(qf_loss)
+                self._epoch_ys.append(y)
+                self._epoch_qs.append(q)
+
+        if itr % self._steps_per_epoch == 0:
+            logger.log('Training finished')
+
+            if (self.replay_buffer.n_transitions_stored >=
+                    self._min_buffer_size):
+                tabular.record('Epoch', epoch)
+                tabular.record('QFunction/AverageQFunctionLoss',
+                               np.mean(self._episode_qf_losses))
+                tabular.record('QFunction/AverageQ', np.mean(self._epoch_qs))
+                tabular.record('QFunction/MaxQ', np.max(self._epoch_qs))
+                tabular.record('QFunction/AverageAbsQ',
+                               np.mean(np.abs(self._epoch_qs)))
+                tabular.record('QFunction/AverageY', np.mean(self._epoch_ys))
+                tabular.record('QFunction/MaxY', np.max(self._epoch_ys))
+                tabular.record('QFunction/AverageAbsY',
+                               np.mean(np.abs(self._epoch_ys)))
+
+        if itr % self._target_update_freq == 0:
+            self._target_qf = copy.deepcopy(self._qf)
+
+    def optimize_qf(self, samples_data):
+        """Perform algorithm optimizing.
+
+        Args:
+            samples_data (dict): Processed batch data.
+
+        Returns:
+            qval_loss: Loss of Q-value predicted by the Q-network.
+            ys: y_s.
+            qval: Q-value predicted by the Q-network.
+
+        """
+        transitions = dict_np_to_torch(samples_data)
+
+        observations = transitions['observations']
+        rewards = transitions['rewards'].reshape(-1, 1)
+        actions = transitions['actions']
+        next_observations = transitions['next_observations']
+        terminals = transitions['terminals'].reshape(-1, 1)
+
+        next_inputs = next_observations
+        inputs = observations
+        with torch.no_grad():
+            # discrete, outputs Qs for all possible actions
+            target_qvals = self._target_qf(next_inputs)
+            best_qvals, _ = torch.max(target_qvals, 1)
+            best_qvals = best_qvals.unsqueeze(1)
+
+        rewards_clipped = rewards
+        if self._clip_reward is not None:
+            rewards_clipped = torch.clamp(rewards, -1 * self._clip_reward,
+                                          self._clip_reward)
+        y_target = (rewards_clipped +
+                    (1.0 - terminals) * self._discount * best_qvals)
+        y_target = y_target.squeeze(1)
+
+        # optimize qf
+        qvals = self._qf(inputs)
+        selected_qs = torch.stack([
+            qs[action.argmax(0)] for qs, action in zip(qvals, actions)
+        ])  # TODO: verify this
+        qval_loss = F.smooth_l1_loss(selected_qs, y_target)
+
+        self._qf_optimizer.zero_grad()
+        qval_loss.backward()
+
+        # optionally clip the gradients
+        if self._clip_grad is not None:
+            for param in self._qf.parameters():
+                param.grad.data.clamp_(-self._clip_grad, self._clip_grad)
+        self._qf_optimizer.step()
+
+        return (qval_loss.detach(), y_target, selected_qs.detach())
