7e50d9dbe538f1ac2dd91a60ae230730df6c9bd1
Gitanshu Sardana <gitanshusardana@gmail.com>
2020-09-04 12:15:27 -0700

    Remove dependency on openai/baselines

--- benchmarks/setup.py
+++ benchmarks/setup.py
@@ -10,9 +10,7 @@ GARAGE_GH_TOKEN = os.environ.get('GARAGE_GH_TOKEN') or 'git'
 
 REQUIRED = [
     # Please keep alphabetized
-    'baselines @ https://{}@api.github.com/repos/openai/baselines/tarball/ea25b9e8b234e6ee1bca43083f8f3cf974143998'.format(GARAGE_GH_TOKEN),  # noqa: E501
     'google-cloud-storage',
-    'gym==0.17.2',
     'matplotlib'
 ]  # yapf: disable
 
