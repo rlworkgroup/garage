780d8733e1564cac9c225ff93587fff9a2323bd7
Mishari Aliesa <aliesa@usc.edu>
2020-09-03 17:37:59 -0700

    Add torch DiscreteCNNModule & DiscreteCNNQFunction

--- src/garage/torch/modules/__init__.py
+++ src/garage/torch/modules/__init__.py
@@ -1,5 +1,6 @@
 """PyTorch Modules."""
 # yapf: disable
+# isort:skip_file
 from garage.torch.modules.categorical_cnn_module import CategoricalCNNModule
 from garage.torch.modules.cnn_module import CNNModule
 from garage.torch.modules.gaussian_mlp_module import (
@@ -9,12 +10,14 @@ from garage.torch.modules.gaussian_mlp_module import (
 from garage.torch.modules.gaussian_mlp_module import GaussianMLPModule
 from garage.torch.modules.mlp_module import MLPModule
 from garage.torch.modules.multi_headed_mlp_module import MultiHeadedMLPModule
-
+# DiscreteCNNModule must go after MLPModule
+from garage.torch.modules.discrete_cnn_module import DiscreteCNNModule
 # yapf: enable
 
 __all__ = [
     'CategoricalCNNModule',
     'CNNModule',
+    'DiscreteCNNModule',
     'MLPModule',
     'MultiHeadedMLPModule',
     'GaussianMLPModule',
