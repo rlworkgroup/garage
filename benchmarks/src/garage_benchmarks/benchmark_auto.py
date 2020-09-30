ae1b9f8cc1eeadf35743ea9db4ef6d2b0bd3fcd5
Gitanshu Sardana <gitanshusardana@gmail.com>
2020-09-30 16:09:12 -0700

    Make snapshot_config optional in benchmarks
    PR #2072 added an argument to functions decorated with @benchmark
    to pass snapshot_config to auto benchmarks but that broke other
    benchmarks that didn't need that argument.
    
    This commit cleans it up by passing snapshot_config directly to
    iterate_experiments, since trying to pass it from within decorator
    function makes for a confusing API

--- benchmarks/src/garage_benchmarks/benchmark_auto.py
+++ benchmarks/src/garage_benchmarks/benchmark_auto.py
@@ -15,26 +15,26 @@ from garage_benchmarks.parameters import MuJoCo1M_ENV_SET
 
 
 @benchmark(plot=False, auto=True)
-def auto_ddpg_benchmarks(snapshot_config):
+def auto_ddpg_benchmarks():
     """Run experiments for DDPG benchmarking."""
     iterate_experiments(ddpg_garage_tf,
                         MuJoCo1M_ENV_SET,
-                        snapshot_config=snapshot_config)
+                        snapshot_config={'snapshot_mode': 'none'})
 
 
 @benchmark(plot=False, auto=True)
-def auto_ppo_benchmarks(snapshot_config):
+def auto_ppo_benchmarks():
     """Run experiments for PPO benchmarking."""
     iterate_experiments(ppo_garage_pytorch,
                         MuJoCo1M_ENV_SET,
-                        snapshot_config=snapshot_config)
+                        snapshot_config={'snapshot_mode': 'none'})
     iterate_experiments(ppo_garage_tf,
                         MuJoCo1M_ENV_SET,
-                        snapshot_config=snapshot_config)
+                        snapshot_config={'snapshot_mode': 'none'})
 
 
 @benchmark(plot=False, auto=True)
-def auto_td3_benchmarks(snapshot_config):
+def auto_td3_benchmarks():
     """Run experiments for TD3 benchmarking."""
     td3_env_ids = [
         env_id for env_id in MuJoCo1M_ENV_SET if env_id != 'Reacher-v2'
@@ -42,26 +42,26 @@ def auto_td3_benchmarks(snapshot_config):
 
     iterate_experiments(td3_garage_tf,
                         td3_env_ids,
-                        snapshot_config=snapshot_config)
+                        snapshot_config={'snapshot_mode': 'none'})
 
 
 @benchmark(plot=False, auto=True)
-def auto_trpo_benchmarks(snapshot_config):
+def auto_trpo_benchmarks():
     """Run experiments for TRPO benchmarking."""
     iterate_experiments(trpo_garage_pytorch,
                         MuJoCo1M_ENV_SET,
-                        snapshot_config=snapshot_config)
+                        snapshot_config={'snapshot_mode': 'none'})
     iterate_experiments(trpo_garage_tf,
                         MuJoCo1M_ENV_SET,
-                        snapshot_config=snapshot_config)
+                        snapshot_config={'snapshot_mode': 'none'})
 
 
 @benchmark(plot=False, auto=True)
-def auto_vpg_benchmarks(snapshot_config):
+def auto_vpg_benchmarks():
     """Run experiments for VPG benchmarking."""
     iterate_experiments(vpg_garage_pytorch,
                         MuJoCo1M_ENV_SET,
-                        snapshot_config=snapshot_config)
+                        snapshot_config={'snapshot_mode': 'none'})
     iterate_experiments(vpg_garage_tf,
                         MuJoCo1M_ENV_SET,
-                        snapshot_config=snapshot_config)
+                        snapshot_config={'snapshot_mode': 'none'})
