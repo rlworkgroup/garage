43d7505062c2ea3f81e97b2b9cc84231fb6a345f
ruofu <ruofuwan@usc.edu>
2020-10-27 13:32:48 -0700

    Fix isort

--- src/garage/__init__.py
+++ src/garage/__init__.py
@@ -1,17 +1,11 @@
 """Garage Base."""
 # yapf: disable
-from garage._dtypes import (EpisodeBatch,
-                            InOutSpec,
-                            StepType,
-                            TimeStep,
+from garage._dtypes import (EpisodeBatch, InOutSpec, StepType, TimeStep,
                             TimeStepBatch)
 from garage._environment import Environment, EnvSpec, EnvStep, Wrapper
-from garage._functions import (_Default,
-                               log_multitask_performance,
-                               log_performance,
-                               make_optimizer,
-                               obtain_evaluation_episodes,
-                               rollout)
+from garage._functions import (_Default, log_multitask_performance,
+                               log_performance, make_optimizer,
+                               obtain_evaluation_episodes, rollout)
 from garage.experiment.experiment import wrap_experiment
 from garage.trainer import TFTrainer, Trainer
 
