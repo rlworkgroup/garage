6c94508dcaed0928d5651665ca893371eda28d48
Gitanshu Sardana <gitanshusardana@gmail.com>
2020-11-08 20:49:32 -0800

    Add python 3.8 in metadata (#2165)

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Mathematics',
