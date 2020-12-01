d45045000eefdeebe2a02294709b92906de47678
K.R. Zentner <krzentner@gmail.com>
2020-12-01 06:41:19 -0800

    Add garage.torch.ObservationBatch

--- src/garage/torch/__init__.py
+++ src/garage/torch/__init__.py
@@ -1,5 +1,8 @@
 """PyTorch-backed modules and algorithms."""
 # yapf: disable
+from garage.torch._dtypes import (ObservationBatch, ObservationOrder,
+                                  ShuffledOptimizationNotSupported,
+                                  observation_batch_to_packed_sequence)
 from garage.torch._functions import (as_torch_dict, compute_advantages,
                                      expand_var, filter_valids, flatten_batch,
                                      flatten_to_single_vector, global_device,
@@ -21,12 +24,16 @@ __all__ = [
     'flatten_to_single_vector',
     'global_device',
     'np_to_torch',
+    'ObservationBatch',
+    'observation_batch_to_packed_sequence',
+    'ObservationOrder',
     'output_height_2d',
     'output_width_2d',
     'pad_to_last',
     'prefer_gpu',
     'product_of_gaussians',
     'set_gpu_mode',
+    'ShuffledOptimizationNotSupported',
     'soft_update_model',
     'state_dict_to',
     'torch_to_np',
