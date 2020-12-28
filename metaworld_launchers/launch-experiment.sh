f049f7eade221a9499526934d2a6b3516b6a4e09
Avnish Narayan <avnishna@usc.edu>
2020-12-27 21:26:02 -0800

    Working launcher

--- metaworld_launchers/launch-experiment.sh
+++ metaworld_launchers/launch-experiment.sh
@@ -2,13 +2,12 @@
 cd /home/avnishnarayan/
 rm -rf garage/
 rm -rf metaworld-runs-v2
-git clone https://github.com/rlworkgroup/garage
-cd garage/
-git checkout run-maml-mt10
-mkdir data/
-cd ../
-mkdir -p metaworld-runs-v2/local/experiment/
+runuser -l avnishnarayan -c "git clone https://github.com/rlworkgroup/garage && cd garage/ && git checkout run-maml-mt10 && mkdir data/"
+runuser -l avnishnarayan -c "mkdir -p metaworld-runs-v2/local/experiment/"
 runuser -l avnishnarayan -c "make run-headless -C ~/garage/"
-runuser -l avnishnarayan -c "cd garage && python docker_metaworld_run_cpu.py metaworld_launchers/ml1/maml_trpo_metaworld_ml1_push.py"
-# runuser -l avnishnarayan -c "cd garage/metaworld_launchers && python upload_folders.py -s ml1-push -t 1200"
+runuser -l avnishnarayan -c "echo HERE1"
+runuser -l avnishnarayan -c "ls"
+runuser -l avnishnarayan -c "echo HERE2"
+runuser -l avnishnarayan -c "cd garage && python docker_metaworld_run_cpu.py metaworld_launchers/ml1/maml_trpo_metaworld_ml1_pick_place.py && ls"
+runuser -l avnishnarayan -c "cd garage/metaworld_launchers && python upload_folders.py ml1-pick-place 1200"
 
