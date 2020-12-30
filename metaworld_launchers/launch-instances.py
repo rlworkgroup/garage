fd1278bcdb91436c1a82cb8e0d4f201f192f97f5
Avnish Narayan <avnishna@usc.edu>
2020-12-29 16:12:20 -0800

    Add utilities for launching instances

--- metaworld_launchers/launch-instances.py
+++ metaworld_launchers/launch-instances.py
@@ -1,9 +1,9 @@
 import subprocess
 
 num_experiments = 3
-zone = 'us-west1-a'
+zone = 'us-west2-a'
 machine_type = 'c2-standard-8'
 source_machine_image = 'metaworld-ml10-mt10-cpu'
 
 for i in range(num_experiments):
-    subprocess.run([f"gcloud beta compute instances create rl2-ml1-pick-place-{i} --metadata-from-file startup-script=launch-experiment.sh --zone {zone} --source-machine-image {source_machine_image} --machine-type {machine_type}"], shell=True)
+    subprocess.run([f"gcloud beta compute instances create rl2-ml1-push-{i} --metadata-from-file startup-script=launch-experiment.sh --zone {zone} --source-machine-image {source_machine_image} --machine-type {machine_type}"], shell=True)
