ddaa41834832609c53f95b7e9f6c10326bf85f68
Hayden Shively <haydenshively@gmail.com>
2021-01-05 00:51:27 -0600

    Experiment #1

--- metaworld_launchers/launch-experiment.sh
+++ metaworld_launchers/launch-experiment.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
-cd /home/avnishnarayan/
+#!/bin/bash
+cd /home/haydenshively/
 rm -rf garage/
 rm -rf metaworld-runs-v2
-runuser -l avnishnarayan -c "git clone https://github.com/rlworkgroup/garage && cd garage/ && git checkout run-rl2-ml1 && mkdir data/"
-runuser -l avnishnarayan -c "mkdir -p metaworld-runs-v2/local/experiment/"
-runuser -l avnishnarayan -c "make run-headless -C ~/garage/"
-runuser -l avnishnarayan -c "cd garage && python docker_metaworld_run_cpu.py 'metaworld_launchers/ml1/rl2_ppo_metaworld_ml1.py --env-name push-v2'"
-runuser -l avnishnarayan -c "cd garage/metaworld_launchers && python upload_folders.py ml1-push 1200"
+runuser -l haydenshively -c "git clone https://github.com/rlworkgroup/garage && cd garage/ && git checkout run-ml1 && mkdir data/"
+runuser -l haydenshively -c "mkdir -p metaworld-runs-v2/local/experiment/"
+runuser -l haydenshively -c "make run-headless -C ~/garage/"
+runuser -l haydenshively -c "cd garage && python docker_metaworld_run_cpu.py 'metaworld_launchers/single_task_launchers/ppo_metaworld.py --env-name door-unlock-v2'"
+runuser -l haydenshively -c "cd garage/metaworld_launchers && python upload_folders.py ppo-door-unlock 1200"
 
--- metaworld_launchers/launch-instances.py
+++ metaworld_launchers/launch-instances.py
@@ -6,4 +6,4 @@ machine_type = 'c2-standard-8'
 source_machine_image = 'metaworld-ml10-mt10-cpu'
 
 for i in range(num_experiments):
-    subprocess.run([f"gcloud beta compute instances create rl2-ml1-push-{i} --metadata-from-file startup-script=launch-experiment.sh --zone {zone} --source-machine-image {source_machine_image} --machine-type {machine_type}"], shell=True)
+    subprocess.run([f"gcloud beta compute instances create ppo-door-unlock-{i} --metadata-from-file startup-script=launch-experiment.sh --zone {zone} --source-machine-image {source_machine_image} --machine-type {machine_type}"], shell=True)
