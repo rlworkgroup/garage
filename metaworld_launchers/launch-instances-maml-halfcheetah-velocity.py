7573afa12bf01d877134f78584f2b12ca86b9b97
Avnish Narayan <avnishna@usc.edu>
2021-01-20 21:44:17 -0800

    Modify launchers for multiple zones

--- metaworld_launchers/launch-instances-maml-halfcheetah-velocity.py
+++ metaworld_launchers/launch-instances-maml-halfcheetah-velocity.py
@@ -3,19 +3,19 @@ import os
 
 algorithm ='maml-trpo'
 sub_experiment_type = 'mamled-baseline'
-experiment_type = f'mt1-pick-place-tuning-{sub_experiment_type}'
-num_experiments = 3
-zones =  ['us-east1-b',]
-machine_types = ['n2-standard-8', 'c2-standard-8']
+experiment_type = f'half-cheetah-dir-{sub_experiment_type}'
+zones =  ['us-west1-a',]
+machine_types = ['n1-standard-8']
 source_machine_image = 'metaworld-v2-cpu-instance'
-bucket = f'mt1-pick-place-maml-tuning/{sub_experiment_type}'
+bucket = f'maml-half-cheetah/{sub_experiment_type}'
 branch='avnish-maml-mlp-baseline'
 
-lr = [1e-3, 5e-3, 1e-2, 1e-1, 2e-1, 5e-1]
+lr = [1e-2, 1e-1, 2e-1, 5e-1]
+lr = [1e-3, 5e-3]
 for i, zone in enumerate(zones):
     for j, machine_type in enumerate(machine_types):
-        for k in range(3):
-            script_number = (i  * len(machine_types) * 3) + j * 3 + k
+        for k in range(2):
+            script_number = (i  * len(machine_types) * 2) + j * 2 + k
             script = (
 f'''#!/bin/bash
 cd /home/avnishnarayan/
@@ -24,10 +24,10 @@ rm -rf metaworld-runs-v2
 runuser -l avnishnarayan -c "git clone https://github.com/rlworkgroup/garage && cd garage/ && git checkout {branch} && mkdir data/"
 runuser -l avnishnarayan -c "mkdir -p metaworld-runs-v2/local/experiment/"
 runuser -l avnishnarayan -c "make run-headless -C ~/garage/"
-runuser -l avnishnarayan -c "cd garage && python docker_metaworld_run_cpu.py 'metaworld_launchers/mt1/maml_trpo_metaworld_mt1.py --env-name pick-place-v2 --inner_lr {lr[script_number]}'"
+runuser -l avnishnarayan -c "cd garage && python docker_metaworld_run_cpu.py 'examples/torch/maml_trpo_half_cheetah_dir.py --inner_lr {lr[script_number]}'"
 runuser -l avnishnarayan -c "cd garage/metaworld_launchers && python upload_folders.py {bucket} 1200"''')
-            with open(f'launchers/launch-experiment-maml-mt1-{script_number}.sh', mode='w') as f:
+            with open(f'launchers/launch-experiment-maml-half-cheetah-{script_number}.sh', mode='w') as f:
                 f.write(script)
             instance_h_params = str(lr[script_number]).replace('.', '-')
             instance_name = f'{algorithm}-{experiment_type}-inner-lr-{instance_h_params}'
-            subprocess.Popen([f"gcloud beta compute instances create {instance_name} --metadata-from-file startup-script=launchers/launch-experiment-maml-mt1-{script_number}.sh --zone {zone} --source-machine-image {source_machine_image} --machine-type {machine_type}"], shell=True)
+            subprocess.Popen([f"gcloud beta compute instances create {instance_name} --metadata-from-file startup-script=launchers/launch-experiment-maml-half-cheetah-{script_number}.sh --zone {zone} --source-machine-image {source_machine_image} --machine-type {machine_type}"], shell=True)
