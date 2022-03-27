a27beb8ac575184585660275a6a28a1c782767da
Yulun Zhang <yulunzha@usc.edu>
2022-03-26 23:48:46 -0700

    ignore import/value error in close visualize window

--- src/garage/envs/gym_env.py
+++ src/garage/envs/gym_env.py
@@ -325,11 +325,18 @@ class GymEnv(Environment):
             elif any(package in getattr(self._env.spec, 'entry_point', '')
                      for package in KNOWN_GYM_NOT_CLOSE_VIEWER):
                 if hasattr(self._env, 'viewer'):
-                    from gym.envs.classic_control.rendering import (
-                        Viewer, SimpleImageViewer)
-                    if (isinstance(self._env.viewer,
-                                   (SimpleImageViewer, Viewer))):
-                        self._env.viewer.close()
+                    try:
+                        from gym.envs.classic_control.rendering import (
+                            Viewer, SimpleImageViewer)
+                        if (isinstance(self._env.viewer,
+                                    (SimpleImageViewer, Viewer))):
+                            self._env.viewer.close()
+                    except ImportError as e:
+                        print(f"Ignored ImportError: {e}")
+                        return
+                    except ValueError as e:
+                        print(f"Ignored ValueError: {e}")
+                        return
 
     def __getstate__(self):
         """See `Object.__getstate__.
